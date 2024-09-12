5e5867693c50b6bd1df0c6eec5a0479bdf90b998
1ndoryu <andoryyu@gmail.com>
2024-09-12 15:40:20 -0400

    *

--- js/colab.js
+++ js/colab.js
@@ -1,7 +1,8 @@
 function empezarcolab() {
     document.querySelectorAll('.ZYSVVV').forEach(button => {
         button.addEventListener('click', async event => {
-            if (confirm('¿Estás seguro de que quieres empezar la colaboración?')) {
+            const userConfirmed = await confirm('¿Estás seguro de que quieres empezar la colaboración?');
+            if (userConfirmed) {
                 const postId = event.target.dataset.postId;
                 const data = await enviarAjax('empezarColab', postId);
                 alert(data.success ? 'Colaboración iniciada con éxito' : 'Error al iniciar la colaboración');
