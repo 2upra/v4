2fbb9572910be43b0ea23b348addcb6a8135ca83
1ndoryu <andoryyu@gmail.com>
2024-09-12 19:41:55 -0400

    *

--- js/colab.js
+++ js/colab.js
@@ -1,4 +1,4 @@
-async function empezarcolab() {
+function empezarcolab() {
     const buttons = document.querySelectorAll('.ZYSVVV');
     
     if (buttons.length === 0) {
@@ -8,31 +8,23 @@ async function empezarcolab() {
     
     buttons.forEach(button => {
         button.addEventListener('click', async event => {
-            try {
-                console.log('Button clicked:', event.currentTarget);
-                const postId = event.currentTarget?.dataset.postId;
+            console.log('Button clicked:', event.currentTarget);
+            const postId = event.currentTarget?.dataset.postId;
 
-                if (!postId) {
-                    console.error('El post ID no se encontró en el botón.');
-                    return;
-                }
+            if (!postId) {
+                console.error('El post ID no se encontró en el botón.');
+                return;
+            }
 
-                console.log('Post ID:', postId);
-                
-                const userConfirmed = confirm('¿Estás seguro de que quieres empezar la colaboración?');
-                if (userConfirmed) {
-                    const data = await enviarAjax('empezarColab', postId);
-                    if (data.success) {
-                        alert('Colaboración iniciada con éxito');
-                    } else {
-                        alert(`Error al iniciar la colaboración: ${data.message || 'Desconocido'}`);
-                    }
-                } else {
-                    alert('Inicio de colaboración cancelado');
-                }
-            } catch (error) {
-                console.error('Error inesperado:', error);
-                alert('Ocurrió un error inesperado.');
+            console.log('Post ID:', postId);
+            
+            const userConfirmed = confirm('¿Estás seguro de que quieres empezar la colaboración?');
+            // Solo enviamos la solicitud AJAX si el usuario confirma
+            if (userConfirmed) {
+                const data = await enviarAjax('empezarColab', postId);
+                alert(data.success ? 'Colaboración iniciada con éxito' : `Error al iniciar la colaboración: ${data.message || 'Desconocido'}`);
+            } else {
+                alert('Inicio de colaboración cancelado');
             }
         });
     });
