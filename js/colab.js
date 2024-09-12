dea9eb8e06d9b3e58b4e1caf163bfa4e0c438d8e
1ndoryu <andoryyu@gmail.com>
2024-09-12 19:23:04 -0400

    *

--- js/colab.js
+++ js/colab.js
@@ -2,11 +2,17 @@ function empezarcolab() {
     document.querySelectorAll('.ZYSVVV').forEach(button => {
         button.addEventListener('click', async event => {
             console.log('Button clicked:', event.currentTarget);
-            console.log('Post ID:', event.currentTarget.dataset.postId);
+            const postId = event.currentTarget?.dataset.postId;
+
+            if (!postId) {
+                console.error('El post ID no se encontró en el botón.');
+                return;
+            }
+
+            console.log('Post ID:', postId);
             
             const userConfirmed = await confirm('¿Estás seguro de que quieres empezar la colaboración?');
             if (userConfirmed) {
-                const postId = event.currentTarget.dataset.postId;
                 const data = await enviarAjax('empezarColab', postId);
                 alert(data.success ? 'Colaboración iniciada con éxito' : 'Error al iniciar la colaboración');
             } else {
