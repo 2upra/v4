6b12f1cfa2303badc67e93399fe2a26b4f4743bb
1ndoryu <andoryyu@gmail.com>
2024-09-13 15:20:20 -0400

    *

--- TemplatePerfil.php
+++ TemplatePerfil.php
@@ -4,27 +4,20 @@ Template Name: Perfil
 */
 
 get_header();
-$user_id = get_current_user_id();
-$nologin_class = !is_user_logged_in() ? ' nologin' : '';
+
 ?>
 
 <div id="main">
-    <div id="content" class="<?php echo esc_attr($nologin_class); ?>">
+    <div id="content">
         <input type="hidden" id="pagina_actual" name="pagina_actual" value="<?php echo esc_attr(get_the_title()); ?>">
-        <?php if (!is_user_logged_in()):
-            // Aqui hace falta una pagina
-        ?>
-        <?php else: ?>
 
-            <div id="menuData" style="display:none;" pestanaActual="">
-                <div data-tab="perfil"></div>
-                <?php if (current_user_can('administrator')) : ?>
-                <?php endif; ?>
-            </div>
+        <div id="menuData" style="display:none;" pestanaActual="">
+            <div data-tab="perfil"></div>
+            <?php if (current_user_can('administrator')) : ?>
+            <?php endif; ?>
+        </div>
 
-            <?php echo perfil() ?>
-
-        <?php endif; ?>
+        <?php echo perfil() ?>
     </div>
 </div>
 
