656ee4652556cea4162f7fd55b39ec746a1df2b8
1ndoryu <andoryyu@gmail.com>
2024-09-14 14:27:31 -0400

    *

--- app/Form/formRS.php
+++ app/Form/formRS.php
@@ -10,57 +10,60 @@ function formRs()
 ?>
     <div class="bloque" id="formRs">
 
-            <div class="W8DK25">
-                <img id="perfil-imagen" src="<?php echo esc_url($urlImagenperfil); ?>" alt="Perfil"
-                    style="max-width: 50px; max-height: 50px; border-radius: 50%;">
-                <p><?php echo $nombreUsuario ?></p>
-            </div>
+        <div class="W8DK25">
+            <img id="perfil-imagen" src="<?php echo esc_url($urlImagenperfil); ?>" alt="Perfil"
+                style="max-width: 50px; max-height: 50px; border-radius: 50%;">
+            <p><?php echo $nombreUsuario ?></p>
+        </div>
 
-            <div>
-                <div class="postTags DABVYT" id="textoRs" contenteditable="true" data-placeholder="Puedes agregar tags usando #"></div>
-            </div>
+        <div>
+            <div class="postTags DABVYT" id="textoRs" contenteditable="true" data-placeholder="Puedes agregar tags usando #"></div>
+        </div>
 
-            <div class="previewsForm NGEESM">
-                <div class="previewAreaArchivos" id="previewImagen" style="display: none;">
-                    <label></label>
-                </div>
-                <div class="previewAreaArchivos" id="previewAudio" style="display: none;">
-                    <label></label>
-                </div>
-                <div class="previewAreaArchivos" id="previewArchivo" style="display: none;">
-                    <label>Archivo adicional para colab (flp, zip, rar, midi, etc)</label>
-                </div>
+        <div class="previewsForm NGEESM">
+            <div class="previewAreaArchivos" id="previewImagen" style="display: none;">
+                <label></label>
             </div>
+            <div class="previewAreaArchivos" id="previewAudio" style="display: none;">
+                <label></label>
+            </div>
+            <div class="previewAreaArchivos" id="previewArchivo" style="display: none;">
+                <label>Archivo adicional para colab (flp, zip, rar, midi, etc)</label>
+            </div>
+        </div>
 
-            <div class="bloque" id="opciones" style="display: none;">
+        <div class="bloque flex-row"" id=" opciones" style="display: none">
+            <p>Opciones de post</p>
+            <div class="flex flex-row">
                 <label class="custom-checkbox">
                     <input type="checkbox" id="allowDownload" name="allow_download" value="1">
                     <span class="checkmark"></span>
-                    Permitir descargas
+                    <?php echo $GLOBALS['descargaicono']; ?>
                 </label>
                 <label class="custom-checkbox">
                     <input type="checkbox" id="content-block" name="content-block" value="1">
                     <span class="checkmark"></span>
-                    Para suscriptores
+                    <?php echo $GLOBALS['estrella']; ?>
                 </label>
                 <label class="custom-checkbox">
                     <input type="checkbox" id="para_colab" name="para_colab" value="1">
                     <span class="checkmark"></span>
-                    Permitir colabs
+                    <?php echo $GLOBALS['iconocolab']; ?>
                 </label>
-                <label class="custom-checkbox">
+                <!--<label class="custom-checkbox">
                     <input type="checkbox" id="momento" name="momento" value="1">
                     <span class="checkmark"></span>
                     Momento
-                </label>
+                </label> -->
             </div>
+        </div>
 
-            <div class="botonesForm R0A915">
-                <button id="botonAudio">Audio</button>
-                <button id="botonImagen">Imagen</button>
-                <button id="botonArchivo">Archivo</button>
-                <button id="enviarRs">Publicar</button>
-            </div>
+        <div class="botonesForm R0A915">
+            <button id="botonAudio"><?php echo $GLOBALS['subiraudio']; ?></button>
+            <button id="botonImagen"><?php echo $GLOBALS['subirimagen']; ?></button>
+            <button id="botonArchivo"><?php echo $GLOBALS['subirArchivo']; ?></button>
+            <button id="enviarRs">Publicar</button>
+        </div>
     </div>
 
 <?php
