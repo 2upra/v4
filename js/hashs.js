54c546a18ab82acf42f15063f5d1602bfa50eb91
1ndoryu <andoryyu@gmail.com>
2024-09-14 12:56:42 -0400

    *

--- js/hashs.js
+++ js/hashs.js
@@ -1,26 +1,28 @@
 // Logs
 const enableLogs = true;
-const logHast = enableLogs ? console.log : function () {};
+const logHash = enableLogs ? console.log : function () {};
+
+// Uncaught SyntaxError: Identifier 'enableLogs' has already been declared (at Hast.js?ver=1.0.1.804423676:1:1)
 
 // Genera un hash SHA-256 del archivo
 window.generateFileHash = async function(file) {
-    logHast("Iniciando generación de hash para el archivo:", file.name);
+    logHash("Iniciando generación de hash para el archivo:", file.name);
     
     // Convertir el archivo a un ArrayBuffer
     const buffer = await file.arrayBuffer();
-    logHast("ArrayBuffer generado:", buffer);
+    logHash("ArrayBuffer generado:", buffer);
     
     // Generar hash con SHA-256
     const hashBuffer = await crypto.subtle.digest('SHA-256', buffer);
-    logHast("Hash buffer generado (SHA-256):", hashBuffer);
+    logHash("Hash buffer generado (SHA-256):", hashBuffer);
     
     // Convertir el buffer a una matriz de bytes y luego a un string hexadecimal
     const hashArray = Array.from(new Uint8Array(hashBuffer));
-    logHast("Array convertido a Uint8Array:", hashArray);
+    logHash("Array convertido a Uint8Array:", hashArray);
     
     // Convertir el array de bytes en un string hexadecimal
     const hash = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    logHast("Hash final generado:", hash);
+    logHash("Hash final generado:", hash);
     
     return hash;
 };
