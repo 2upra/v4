a6d9bacdb07692ec5f41ea12327676e3efa96c62
1ndoryu <andoryyu@gmail.com>
2024-09-24 17:29:01 -0400

    *

--- app/Posts/Filtros.php
+++ app/Posts/Filtros.php
@@ -7,23 +7,18 @@ function aplicarFiltros($query_args, $args, $user_id, $current_user_id)
     $rolaEstado = function($status) use (&$query_args) {
         $query_args += [
             'author' => get_current_user_id(),
-            'post_status' => [$status]
+            'post_status' => $status
         ];
         $query_args['meta_query'][] = ['key' => 'rola', 'value' => '1', 'compare' => '='];
     };
 
     $meta_query_conditions = [
-
-        /*
-        FILTROS PARA PAGINA SELLO
-        */
+        // FILTROS PARA PAGINA SELLO
         'rolasEliminadas'  => fn() => $rolaEstado('pending_deletion'),
         'rolasRechazadas'  => fn() => $rolaEstado('rejected'),
         'rolasPendiente'   => fn() => $rolaEstado('pending'),
 
-        /*
-        FILTROS PAGINA DE MUSICA
-        */
+        // FILTROS PAGINA DE MUSICA
         'rola' => fn() => $query_args['meta_query'][] = [
             ['key' => 'rola', 'value' => '1', 'compare' => '='],
             ['key' => 'post_audio', 'compare' => 'EXISTS']
@@ -33,9 +28,7 @@ function aplicarFiltros($query_args, $args, $user_id, $current_user_id)
             ? $query_args['post__in'] = $user_liked_post_ids 
             : $query_args['posts_per_page'] = 0,
 
-        /*
-        FILTROS INICIO
-        */
+        // FILTROS INICIO
         'nada'     => fn() => $query_args['post_status'] = 'publish',
         'colabs'   => ['key' => 'paraColab', 'value' => '1', 'compare' => '='],
         'libres'   => [
@@ -48,28 +41,25 @@ function aplicarFiltros($query_args, $args, $user_id, $current_user_id)
             ['key' => '_thumbnail_id', 'compare' => 'EXISTS']
         ],
 
-        /*
-        FILTROS SAMPLES
-        */
+        // FILTROS SAMPLES
         'sample'   => ['key' => 'paraDescarga', 'value' => '1', 'compare' => '='],
 
-        /*
-        FILTROS COLAB
-        */
-
+        // FILTROS COLAB
         'colab' => fn() => $query_args['post_status'] = 'pending',
-        'colabPendientes' => fn() => $query_args['meta_query'][] = [
+        'colabPendientes' => fn() => $query_args += [
             'author' => get_current_user_id(),
-            'post_status' => ['pending']
+            'post_status' => 'pending'
         ],
-
     ];
 
     // Aplica el filtro si existe
     if (isset($meta_query_conditions[$filtro])) {
-        $query_args['meta_query'][] = is_callable($meta_query_conditions[$filtro]) 
-            ? $meta_query_conditions[$filtro]() 
-            : $meta_query_conditions[$filtro];
+        $result = $meta_query_conditions[$filtro];
+        if (is_callable($result)) {
+            $result(); // Ejecutar función para modificar $query_args
+        } else {
+            $query_args['meta_query'][] = $result;
+        }
     }
 
     // Si se proporciona un user_id, se filtra por autor
