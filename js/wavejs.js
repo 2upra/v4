edba9b5892723984b9edf389e1a8076fb55acdab
1ndoryu <andoryyu@gmail.com>
2024-09-25 18:54:08 -0400

    *

--- js/wavejs.js
+++ js/wavejs.js
@@ -1,16 +1,5 @@
 function inicializarWaveforms() {
-    const maxRetries = 3; // Número máximo de reintentos
-    const retryDelay = 3000; // Retraso entre reintentos en milisegundos
-
-    const loadAndPlayAudio = (container, wavesurfer, src, attempt = 1) => {
-        if (attempt > maxRetries) {
-            // Mostrar mensaje de error al usuario
-            container.querySelector('.waveform-loading').style.display = 'none';
-            container.querySelector('.waveform-message').style.display = 'block';
-            console.error(`No se pudo cargar el audio después de ${maxRetries} intentos.`);
-            return;
-        }
-
+    const loadAndPlayAudio = (container, wavesurfer, src) => {
         window.audioLoading = true;
         container.querySelector('.waveform-loading').style.display = 'block';
         container.querySelector('.waveform-message').style.display = 'none';
@@ -23,58 +12,53 @@ function inicializarWaveforms() {
 
         wavesurfer.load(src);
 
-        const onReady = () => {
+        wavesurfer.on('ready', () => {
             window.audioLoading = false;
             container.dataset.audioLoaded = 'true';
             container.querySelector('.waveform-loading').style.display = 'none';
 
+            wavesurfer.setPlaybackRate(1.2); // Ajusta la velocidad a 1.2x
+
+            // Cambiar el tono utilizando el plugin pitchShift
+            if (wavesurfer.pitchShift) {
+                wavesurfer.pitchShift.setPitch(3); // Cambia el tono 3 semitonos hacia arriba
+            }
+        
+
             const waveCargada = container.getAttribute('data-wave-cargada') === 'true';
 
             if (!waveCargada) {
-                const image = generateWaveformImage(wavesurfer);
-                if (image) {
+                setTimeout(() => {
+                    const image = generateWaveformImage(wavesurfer);
                     const postId = container.getAttribute('postIDWave');
                     sendImageToServer(image, postId);
-                } else {
-                    console.error('No se pudo generar la imagen del waveform.');
-                }
+                }, 1);
             }
-
-            wavesurfer.un('ready', onReady);
-            wavesurfer.un('error', onError);
-        };
-
-        const onError = (e) => {
-            console.error(`Error al cargar el audio: ${e}`);
-            wavesurfer.un('ready', onReady);
-            wavesurfer.un('error', onError);
-
-            // Intentar recargar después de un retraso
-            setTimeout(() => {
-                loadAndPlayAudio(container, wavesurfer, src, attempt + 1);
-            }, retryDelay);
-        };
-
-        wavesurfer.on('ready', onReady);
-        wavesurfer.on('error', onError);
+        });
+        wavesurfer.on('error', () => {
+            setTimeout(() => loadAndPlayAudio(container, wavesurfer, src), 3000);
+        });
     };
 
-    const generateWaveformImage = (wavesurfer) => {
-        if (wavesurfer.drawer && wavesurfer.drawer.canvases && wavesurfer.drawer.canvases[0]) {
-            const canvas = wavesurfer.drawer.canvases[0].wave;
-            return canvas.toDataURL('image/png');
-        } else {
-            console.error('Cannot generate waveform image. Canvases are not properly initialized.');
-            return null;
+    function generateWaveformImage(wavesurfer) {
+        const canvas = wavesurfer.getWrapper().querySelector('canvas');
+        return canvas.toDataURL('image/png');
+    }
+
+    async function sendImageToServer(imageData, postId) {
+        if (imageData.length < 100) {
+            return;
         }
-    };
-
-    const sendImageToServer = async (imageData, postId) => {
-        if (imageData.length < 100) return;
 
         // Convertir la cadena base64 a Blob
-        const response = await fetch(imageData);
-        const blob = await response.blob();
+        const byteString = atob(imageData.split(',')[1]);
+        const mimeString = imageData.split(',')[0].split(':')[1].split(';')[0];
+        const ab = new ArrayBuffer(byteString.length);
+        const ia = new Uint8Array(ab);
+        for (let i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+        const blob = new Blob([ab], {type: mimeString});
 
         const formData = new FormData();
         formData.append('action', 'save_waveform_image');
@@ -82,12 +66,12 @@ function inicializarWaveforms() {
         formData.append('post_id', postId);
 
         try {
-            const res = await fetch(ajaxUrl, {
+            const response = await fetch(ajaxUrl, {
                 method: 'POST',
                 body: formData
             });
 
-            const data = await res.json();
+            const data = await response.json();
 
             if (!data.success) {
                 console.error('Error al guardar la imagen:', data.message);
@@ -95,30 +79,59 @@ function inicializarWaveforms() {
         } catch (error) {
             console.error('Error en la solicitud:', error);
         }
-    };
+    }
 
     const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
+        entries => {
+            entries.forEach(entry => {
                 if (entry.isIntersecting && entry.target.dataset.initialized !== 'true') {
                     const container = entry.target;
                     const audioSrc = container.getAttribute('data-audio-url');
                     const waveCargada = container.getAttribute('data-wave-cargada') === 'true';
 
+                    let wavesurfer;
+
+                    const initWavesurfer = () => {
+                        const containerHeight = container.classList.contains('waveform-container-venta') ? 60 : 102;
+                        const ctx = document.createElement('canvas').getContext('2d');
+                        const gradient = ctx.createLinearGradient(0, 0, 0, 500);
+                        const progressGradient = ctx.createLinearGradient(0, 0, 0, 500);
+                        [gradient, progressGradient].forEach(g => {
+                            ['0', '0.55', '0.551', '0.552', '0.553', '1'].forEach(stop => {
+                                g.addColorStop(parseFloat(stop), g === gradient ? '#FFFFFF' : '#d43333');
+                            });
+                        });
+
+                        return WaveSurfer.create({
+                            container: container,
+                            waveColor: gradient,
+                            progressColor: progressGradient,
+                            backend: 'WebAudio',
+                            interact: true,
+                            barWidth: 2,
+                            height: containerHeight,
+                            partialRender: true
+                        });
+                    };
+
                     if (audioSrc && !window.audioLoading) {
-                        const wavesurfer = initWavesurfer(container);
+                        wavesurfer = initWavesurfer();
                         container.dataset.audioLoaded = 'false';
 
                         if (waveCargada) {
+                            // Si la waveform ya está cargada, simplemente cargar el audio
                             wavesurfer.load(audioSrc);
                         } else {
-                            setTimeout(() => {
+                            // Si no está cargada, usar loadAndPlayAudio
+                            const loadTimer = setTimeout(() => {
                                 if (container.dataset.audioLoaded === 'false') {
                                     loadAndPlayAudio(container, wavesurfer, audioSrc);
                                 }
                             }, 5000);
                         }
+                    }
 
+                    if (wavesurfer) {
                         container.addEventListener('click', () => {
                             if (container.dataset.audioLoaded === 'false') {
                                 loadAndPlayAudio(container, wavesurfer, audioSrc);
@@ -133,40 +146,14 @@ function inicializarWaveforms() {
                 }
             });
         },
-        { rootMargin: '0px', threshold: 0.1 }
+        {rootMargin: '0px', threshold: 0.1}
     );
 
-    const initWavesurfer = (container) => {
-        const containerHeight = container.classList.contains('waveform-container-venta') ? 60 : 102;
-        const gradient = getGradient('#FFFFFF');
-        const progressGradient = getGradient('#d43333');
-
-        return WaveSurfer.create({
-            container: container,
-            waveColor: gradient,
-            progressColor: progressGradient,
-            backend: 'WebAudio',
-            interact: true,
-            barWidth: 2,
-            height: containerHeight,
-            partialRender: true
-        });
-    };
-
-    const getGradient = (color) => {
-        const canvas = document.createElement('canvas');
-        const ctx = canvas.getContext('2d');
-        const gradient = ctx.createLinearGradient(0, 0, 0, 500);
-
-        gradient.addColorStop(0, color);
-        gradient.addColorStop(1, color);
-
-        return gradient;
-    };
-
-    document.querySelectorAll('div[id^="waveform-"]').forEach((container) => {
+    document.querySelectorAll('div[id^="waveform-"]').forEach(container => {
         if (container.dataset.initialized !== 'true') {
             observer.observe(container);
         }
     });
 }
+
+
