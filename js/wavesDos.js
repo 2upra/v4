db902cb3f3c7c1467449997ec3f3492e09bb87fe
1ndoryu <andoryyu@gmail.com>
2024-10-01 21:07:23 -0400

    *

--- js/wavesDos.js
+++ js/wavesDos.js
@@ -1,12 +1,6 @@
 window.inicializarWaveform = function (containerId, audioSrc) {
     const container = document.getElementById(containerId);
     if (container && audioSrc) {
-        let loadingElement = container.querySelector('.waveform-loading');
-        let messageElement = container.querySelector('.waveform-message');
-        let backgroundElement = container.querySelector('.waveform-background');
-        messageElement.style.display = 'none';
-        loadingElement.style.display = 'block';
-        backgroundElement.style.display = 'block';
 
         const options = {
             container: container,
@@ -21,8 +15,6 @@ window.inicializarWaveform = function (containerId, audioSrc) {
         let wavesurfer = WaveSurfer.create(options);
         wavesurfer.load(audioSrc);
         wavesurfer.on('ready', function () {
-            loadingElement.style.display = 'none';
-            backgroundElement.style.display = 'none';
         });
         container.addEventListener('click', function () {
             wavesurfer.playPause();
