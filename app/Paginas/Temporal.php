bbc6188ad7963c558bacdc817c621ce687a2783b
1ndoryu <andoryyu@gmail.com>
2024-10-11 17:59:35 -0400

    *

--- app/Paginas/Temporal.php
+++ app/Paginas/Temporal.php
@@ -56,6 +56,136 @@ function papelera()
             </div>
         </div>
 
+        <div class="QYGNPB ASDASB" id="containerflux">
+            <div class="XXDD EZDNZE" id="stickyContainer">
+                <p class="MLZKPD" id="textflux"></p>
+                <p class="XXD2"></p>
+            </div>
+        </div>
+
+        <div class="MQGOCQ">
+            <div class="XX1 A1607241136 C2024715" id="contenedor1707">
+
+                <div class="X170724214 XX7 PROGRESO E17072412" id="ppp4">
+                    <div class="XXDD XX9">
+                        <div class="XX10">
+                            <h3 class="XXD11" id="startDate">05/01/2024</h3>
+                            <h3 class="XXD1">4. Rehacer y pulir </h3>
+                            <p class="XXD2">En esta etapa, muchas funcionalidades han sido refinadas, incluyendo el rediseño
+                                de la interfaz, mejoras en el rendimiento y una modernización significativa de las
+                                interfaces para artistas y seguidores. Lo más destacable de este periodo es la creación de
+                                funciones más claras y comprensibles para cada tipo de usuario.</p>
+
+                            <h3 class="XXD1 230624810">En progreso</h3>
+
+                            <div id="avancesContent" class="avances-content avancesContent">
+                                <ul>
+                                    <li>+ Se han rediseñado las interfaces para mejorar la experiencia del usuario.</li>
+                                    <li>+ Se ha realizado una separación de funcionalidades específicas para artistas y
+                                        seguidores. </li>
+                                    <li>+ Las interfaces ahora permiten un mejor entendimiento del propósito general de la
+                                        plataforma.</li>
+                                    <li>+ Se ha implementado un sistema óptimo para filtrar y encontrar recursos dirigidos a
+                                        los artistas / mejora en como se muestra el contenido para seguidores. </li>
+                                    <li>+ Se espera pulir las funcionalidades de interacción como las reacciones, chat,
+                                        subida contenido y la gestión de este mismo, así como seguir facilitando el
+                                        entendimiento para los proximos usuarios nuevos.</li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="X170724214 XX7 PROGRESO E17072412" id="ppp3">
+                    <div class="XXDD XX9">
+                        <div class="XX10">
+                            <h3 class="XXD11" id="startDate">04/01/2024</h3>
+                            <h3 class="XXD1">3. Complejidad</h3>
+                            <p class="XXD2">En este punto, se comprende que realizar un trabajo de alta calidad requiere una
+                                gran inversión de tiempo, esfuerzo y dedicación. Desarrollar la base para un chat en tiempo
+                                real desde cero es un logro significativo, especialmente considerando que es el primer
+                                acercamiento a la programación. Aunque hacerlo desde la base es complejo, es necesario
+                                debido a la naturaleza del resultado final que se desea alcanzar.</p>
+
+                            <h3 class="XXD1 230624810">Avances principales</h3>
+                            <div id="avancesContent" class="avances-content avancesContent">
+                                <ul>
+                                    <li>+ Se ha implementado un chat en tiempo real para los usuarios.</li>
+                                    <li>+ Se han implementado algoritmos básicos para mejorar la visualización del contenido
+                                        para los usuarios.</li>
+                                    <li>+ Se ha desarrollado un sistema justo para la descarga de contenido y la motivación
+                                        para publicar (monedas).</li>
+                                    <li>+ Se han realizado mejoras considerables en el rendimiento y el tiempo de carga,
+                                        incluyendo un sistema de caché para la música que permite cargar cada pista solo una
+                                        vez.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="X170724214 XX7 PROGRESO E17072412" id="ppp2">
+                    <div class="XXDD XX9">
+                        <div class="XX10">
+                            <h3 class="XXD11" id="startDate">03/01/2024</h3>
+                            <h3 class="XXD1">2. Prueba y errores</h3>
+                            <p class="XXD2">Se han desarrollado diversas funciones desde cero. Debido a la complejidad y los
+                                requisitos del proyecto, todas las funcionalidades se están programando desde la base. A
+                                pesar de que muchas de las funcionalidades complejas ya operan correctamente, aún requieren
+                                mejoras en cuanto a calidad, experiencia visual y otros aspectos para alcanzar el nivel
+                                deseado.</p>
+
+                            <h3 class="XXD1 230624810">Avances principales</h3>
+
+                            <div id="avancesContent" class="avances-content avancesContent">
+                                <ul>
+                                    <li>+ Se han implementado exitosamente todas las funcionalidades de interactividad,
+                                        tales como "Me gusta", comentarios, notificaciones, la opción de seguir a otros
+                                        usuarios, etc.</li>
+                                    <li>+ Se han desarrollado las funcionalidades necesarias para la monetización de
+                                        contenido, incluyendo la publicación de beats para la venta, suscripciones y
+                                        compras.</li>
+                                    <li>+ Se ha estructurado un modelo similar al de Spotify para la reproducción de música.
+                                    </li>
+                                    <li>+ La carga dinámica de páginas y todo tipo de contenido se realiza de manera
+                                        eficiente.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="X170724214 XX7 PROGRESO E17072412" id="ppp1">
+                    <div class="XXDD XX9">
+                        <div class="XX10">
+                            <h3 class="XXD11" id="startDate">01/01/2024</h3>
+                            <h3 class="XXD1">1. Comienzo</h3>
+                            <p class="XXD2">El planteamiento es claro: desarrollar una plataforma con funcionalidades
+                                innovadoras, que incluya un conjunto de herramientas para artistas y un espacio dedicado
+                                para sus seguidores. Inicialmente, se estimó que la realización de este proyecto no tomaría
+                                más de dos meses; sin embargo, la complejidad del mismo fue subestimada desde el principio.
+                            </p>
+
+                            <h3 class="XXD1 230624810">Avances principales</h3>
+
+                            <div id="avancesContent" class="avances-content avancesContent">
+                                <ul>
+                                    <li>+ Se plantea las funcionalidades principales.</li>
+                                    <li>+ Se consigue inversiones necesarias y recurrentes para el proyecto.</li>
+                                    <li>+ Se comienza a escribir las primeras lineas de codigo.</li>
+                                </ul>
+                            </div>
+                            <p id="timeAgo"></p>
+                        </div>
+                    </div>
+                </div>
+                <p class="textopeq">+</p>
+                <div id="barraProgreso1707"></div>
+            </div>
+        </div>
+
+
     </div>
 <?
 }
@@ -71,15 +201,12 @@ function dev()
             <div class="tab active GMXSUJ" id="inicio">
 
                 <div>
-                    
+
                     <div>
 
                     </div>
 
                 </div>
-
-
-
 
                 <div class="XX1 XX2">
                     <?
@@ -158,14 +285,6 @@ function dev()
                 </div>
 
                 <div class="DAEOXT">
-
-                    <div class="QYGNPB ASDASB" id="containerflux">
-                        <div class="XXDD EZDNZE" id="stickyContainer">
-                            <p class="MLZKPD" id="textflux"></p>
-                            <p class="XXD2"></p>
-                        </div>
-                    </div>
-
                     <div class="TTVMWQ">
                         <div class="XXDD IUNRBL">
                             <h3 class="XXD1"><strong>Conviértete en patrocinador:</strong> Si te gusta el proyecto, puedes colaborar obteniendo participación creativa, acceso anticipado, contenido exclusivo, reconocimiento y acciones mensuales del proyecto.</h3>
@@ -188,127 +307,7 @@ function dev()
 
 
                 </div>
-                <div class="MQGOCQ">
-                    <div class="XX1 A1607241136 C2024715" id="contenedor1707">
 
-                        <div class="X170724214 XX7 PROGRESO E17072412" id="ppp4">
-                            <div class="XXDD XX9">
-                                <div class="XX10">
-                                    <h3 class="XXD11" id="startDate">05/01/2024</h3>
-                                    <h3 class="XXD1">4. Rehacer y pulir </h3>
-                                    <p class="XXD2">En esta etapa, muchas funcionalidades han sido refinadas, incluyendo el rediseño
-                                        de la interfaz, mejoras en el rendimiento y una modernización significativa de las
-                                        interfaces para artistas y seguidores. Lo más destacable de este periodo es la creación de
-                                        funciones más claras y comprensibles para cada tipo de usuario.</p>
-
-                                    <h3 class="XXD1 230624810">En progreso</h3>
-
-                                    <div id="avancesContent" class="avances-content avancesContent">
-                                        <ul>
-                                            <li>+ Se han rediseñado las interfaces para mejorar la experiencia del usuario.</li>
-                                            <li>+ Se ha realizado una separación de funcionalidades específicas para artistas y
-                                                seguidores. </li>
-                                            <li>+ Las interfaces ahora permiten un mejor entendimiento del propósito general de la
-                                                plataforma.</li>
-                                            <li>+ Se ha implementado un sistema óptimo para filtrar y encontrar recursos dirigidos a
-                                                los artistas / mejora en como se muestra el contenido para seguidores. </li>
-                                            <li>+ Se espera pulir las funcionalidades de interacción como las reacciones, chat,
-                                                subida contenido y la gestión de este mismo, así como seguir facilitando el
-                                                entendimiento para los proximos usuarios nuevos.</li>
-                                        </ul>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="X170724214 XX7 PROGRESO E17072412" id="ppp3">
-                            <div class="XXDD XX9">
-                                <div class="XX10">
-                                    <h3 class="XXD11" id="startDate">04/01/2024</h3>
-                                    <h3 class="XXD1">3. Complejidad</h3>
-                                    <p class="XXD2">En este punto, se comprende que realizar un trabajo de alta calidad requiere una
-                                        gran inversión de tiempo, esfuerzo y dedicación. Desarrollar la base para un chat en tiempo
-                                        real desde cero es un logro significativo, especialmente considerando que es el primer
-                                        acercamiento a la programación. Aunque hacerlo desde la base es complejo, es necesario
-                                        debido a la naturaleza del resultado final que se desea alcanzar.</p>
-
-                                    <h3 class="XXD1 230624810">Avances principales</h3>
-                                    <div id="avancesContent" class="avances-content avancesContent">
-                                        <ul>
-                                            <li>+ Se ha implementado un chat en tiempo real para los usuarios.</li>
-                                            <li>+ Se han implementado algoritmos básicos para mejorar la visualización del contenido
-                                                para los usuarios.</li>
-                                            <li>+ Se ha desarrollado un sistema justo para la descarga de contenido y la motivación
-                                                para publicar (monedas).</li>
-                                            <li>+ Se han realizado mejoras considerables en el rendimiento y el tiempo de carga,
-                                                incluyendo un sistema de caché para la música que permite cargar cada pista solo una
-                                                vez.</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="X170724214 XX7 PROGRESO E17072412" id="ppp2">
-                            <div class="XXDD XX9">
-                                <div class="XX10">
-                                    <h3 class="XXD11" id="startDate">03/01/2024</h3>
-                                    <h3 class="XXD1">2. Prueba y errores</h3>
-                                    <p class="XXD2">Se han desarrollado diversas funciones desde cero. Debido a la complejidad y los
-                                        requisitos del proyecto, todas las funcionalidades se están programando desde la base. A
-                                        pesar de que muchas de las funcionalidades complejas ya operan correctamente, aún requieren
-                                        mejoras en cuanto a calidad, experiencia visual y otros aspectos para alcanzar el nivel
-                                        deseado.</p>
-
-                                    <h3 class="XXD1 230624810">Avances principales</h3>
-
-                                    <div id="avancesContent" class="avances-content avancesContent">
-                                        <ul>
-                                            <li>+ Se han implementado exitosamente todas las funcionalidades de interactividad,
-                                                tales como "Me gusta", comentarios, notificaciones, la opción de seguir a otros
-                                                usuarios, etc.</li>
-                                            <li>+ Se han desarrollado las funcionalidades necesarias para la monetización de
-                                                contenido, incluyendo la publicación de beats para la venta, suscripciones y
-                                                compras.</li>
-                                            <li>+ Se ha estructurado un modelo similar al de Spotify para la reproducción de música.
-                                            </li>
-                                            <li>+ La carga dinámica de páginas y todo tipo de contenido se realiza de manera
-                                                eficiente.</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="X170724214 XX7 PROGRESO E17072412" id="ppp1">
-                            <div class="XXDD XX9">
-                                <div class="XX10">
-                                    <h3 class="XXD11" id="startDate">01/01/2024</h3>
-                                    <h3 class="XXD1">1. Comienzo</h3>
-                                    <p class="XXD2">El planteamiento es claro: desarrollar una plataforma con funcionalidades
-                                        innovadoras, que incluya un conjunto de herramientas para artistas y un espacio dedicado
-                                        para sus seguidores. Inicialmente, se estimó que la realización de este proyecto no tomaría
-                                        más de dos meses; sin embargo, la complejidad del mismo fue subestimada desde el principio.
-                                    </p>
-
-                                    <h3 class="XXD1 230624810">Avances principales</h3>
-
-                                    <div id="avancesContent" class="avances-content avancesContent">
-                                        <ul>
-                                            <li>+ Se plantea las funcionalidades principales.</li>
-                                            <li>+ Se consigue inversiones necesarias y recurrentes para el proyecto.</li>
-                                            <li>+ Se comienza a escribir las primeras lineas de codigo.</li>
-                                        </ul>
-                                    </div>
-                                    <p id="timeAgo"></p>
-                                </div>
-                            </div>
-                        </div>
-                        <p class="textopeq">+</p>
-                        <div id="barraProgreso1707"></div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
