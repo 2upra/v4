c056cef1b3a191648f9f0180056eb85c09f78b0b
1ndoryu <andoryyu@gmail.com>
2024-10-14 17:55:46 -0400

    *

--- header.php
+++ header.php
@@ -178,7 +178,7 @@ if (!defined('ABSPATH')) {
 
                 <!-- Fondo oscuro para los submenus -->
                 <div id="modalBackground2" class="modal-background submenu modalBackground2" style="display: none;"></div>
-                >
+                
                 <div class="bloquesChatTest">
                     <div class="bloqueChatReiniciar">
                         <? echo conversacionesUsuario($user_id) ?>
