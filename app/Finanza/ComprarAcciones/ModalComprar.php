9d6c23fa2063da8e882fc32a1300e41b926f500b
1ndoryu <andoryyu@gmail.com>
2024-10-14 21:39:34 -0400

    *

--- app/Finanza/ComprarAcciones/ModalComprar.php
+++ app/Finanza/ComprarAcciones/ModalComprar.php
@@ -12,7 +12,7 @@ function modalComprarAcciones()
                 <input type="text" id="cantidadCompra" placeholder="$20">
                 <input type="hidden" id="cantidadReal">
                 <input type="hidden" id="userID" value="<? echo get_current_user_id(); ?>">
-                <p>"Al donar, parte de tu contribución se convierte en acciones de nuestra empresa a través de un fondo de inversión algorítmico que ajusta su valor automáticamente. Tu apoyo impulsa el proyecto y te hace parte de nuestro crecimiento, con la posibilidad de beneficiarte de nuestro frutos en el futuro"</p>
+                <p>"Al donar, parte de tu contribución se convierte en acciones de nuestra empresa a través de un fondo de inversión algorítmico que ajusta su valor automáticamente. Tu apoyo impulsa el proyecto y te hace parte de nuestro crecimiento con la autoridad de beneficiarte de nuestros frutos en el futuro"</p>
                 <div class="DZYSQD DZYSQF">
                     <button class="DZYBQD cerrardonar">Volver</button>
                     <button class="DZYBQD botonprincipal" id="botonComprar">Donar</button>
