920630f28959840017d1374c53701799d83a8eb8
1ndoryu <andoryyu@gmail.com>
2024-10-16 19:12:20 -0400

    *

--- single-colab.php
+++ single-colab.php
@@ -14,7 +14,7 @@ if (have_posts()) :
         <div id="main">
             <div id="content" class="<?php echo esc_attr($nologin_class); ?>">
                 <div class="single">
-                    <?php echo htmlPost($filtro); ?>
+                    <?php echo htmlColab($filtro); ?>
                 </div>
             </div>
         </div>
