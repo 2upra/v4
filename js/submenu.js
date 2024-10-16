cea31db14b2cdf6d81d6d509c93792eab7d647e1
1ndoryu <andoryyu@gmail.com>
2024-10-15 21:03:16 -0400

    *

--- js/submenu.js
+++ js/submenu.js
@@ -111,7 +111,7 @@ function initializeStaticMenus() {
     //console.log('[initializeStaticMenus] Inicializando menús estáticos');
     createSubmenu(".subiricono", "submenusubir", 0, 120);
     createSubmenu(".chatIcono", "bloqueConversaciones", 30, -270);
-    createSubmenu(".fotoperfilsub", "fotoperfilsub", 30, 150 )
+    createSubmenu(".fotoperfilsub", "fotoperfilsub", 30, 100);
 }
 
 // Esto se reinicia cada vez que cargan nuevos posts
