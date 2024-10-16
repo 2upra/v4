0e75312d003f906a5f7257c0938de237400c29ca
1ndoryu <andoryyu@gmail.com>
2024-10-15 21:38:58 -0400

    *

--- js/inversores.js
+++ js/inversores.js
@@ -1,6 +1,6 @@
 function meta() {
     const meta = 1000;
-    const recaudado = 632;
+    const recaudado = 657;
     const porcentaje = (recaudado / meta) * 100;
 
     const progressBar = document.querySelector('.progress-barA1');
