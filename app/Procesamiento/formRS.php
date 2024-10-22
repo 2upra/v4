f71e35bf769584cadb197afb4a2ba4b3deb9e1f3
1ndoryu <andoryyu@gmail.com>
2024-10-22 02:23:42 -0400

    *

--- app/Procesamiento/formRS.php
+++ app/Procesamiento/formRS.php
@@ -36,7 +36,7 @@ function formRs()
             </div>
         </div>
 
-        <div class="bloque flex-row"" id=" opciones" style="display: none">
+        <div class="bloque flex-row"" id="opciones" style="display: none">
             <p>Opciones de post</p>
             <div class="flex flex-row gap-2">
                 <label class="custom-checkbox">
