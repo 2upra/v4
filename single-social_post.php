50fbd7f6cc65d14735d8f62183590ab5222382ae
1ndoryu <andoryyu@gmail.com>
2024-10-24 14:10:47 -0400

    *

--- single-social_post.php
+++ single-social_post.php
@@ -1,33 +1,49 @@
 <?php get_header(); ?>
 <?php
-// single-social_post.php
-
 $user_id = get_current_user_id();
 $acciones = get_user_meta($user_id, 'acciones', true);
 $nologin_class = !is_user_logged_in() ? ' nologin' : '';
 
 if (have_posts()) :
     while (have_posts()) : the_post();
+        // Obtener el ID del post actual ANTES de usarlo
+        $current_post_id = get_the_ID();
         $filtro = 'single';
+        
+        // Obtener los datos del algoritmo
         $datosAlgoritmo = get_post_meta($current_post_id, 'datosAlgoritmo', true);
         $datos_decoded = json_decode($datosAlgoritmo, true);
-        $schema = array(
-            "@context" => "https://schema.org",
-            "@type" => "AudioObject",
-            "name" => get_the_title(),
-            "description" => $datos_decoded['descripcion_ia_pro']['es'],
-            "genre" => $datos_decoded['genero_posible']['es'],
-            "keywords" => implode(", ", $datos_decoded['tags_posibles']['es']),
-            "datePublished" => get_the_date('c'),
-            "author" => array(
-                "@type" => "Person",
-                "name" => get_the_author()
-            )
-        );
-        ob_start();
 
-        // Obtener el ID del post actual
-        $current_post_id = get_the_ID();
+        // Verificar si los datos existen y tienen el formato correcto
+        if ($datos_decoded && is_array($datos_decoded)) {
+            // Determinar qué clave usar para la descripción
+            $descripcion = isset($datos_decoded['descripcion_ia_pro']['es']) 
+                ? $datos_decoded['descripcion_ia_pro']['es'] 
+                : (isset($datos_decoded['descripcion_ia']['es']) 
+                    ? $datos_decoded['descripcion_ia']['es'] 
+                    : '');
+
+            // Verificar y procesar los tags
+            $tags = isset($datos_decoded['tags_posibles']['es']) && is_array($datos_decoded['tags_posibles']['es'])
+                ? implode(", ", $datos_decoded['tags_posibles']['es'])
+                : '';
+
+            $schema = array(
+                "@context" => "https://schema.org",
+                "@type" => "AudioObject",
+                "name" => get_the_title(),
+                "description" => $descripcion,
+                "genre" => isset($datos_decoded['genero_posible']['es']) ? $datos_decoded['genero_posible']['es'] : '',
+                "keywords" => $tags,
+                "datePublished" => get_the_date('c'),
+                "author" => array(
+                    "@type" => "Person",
+                    "name" => get_the_author()
+                )
+            );
+        }
+
+        ob_start();
 ?>
         <div id="main">
             <div id="content" class="<?php echo esc_attr($nologin_class); ?>">
@@ -50,8 +66,8 @@ if (have_posts()) :
             </div>
         </div>
 <?php
-        $contenido = ob_get_clean(); // Captura el contenido del buffer y lo limpia
-        echo $contenido; // Muestra el contenido capturado
+        $contenido = ob_get_clean();
+        echo $contenido;
     endwhile;
 else :
     echo '<p>No se encontró el contenido.</p>';
