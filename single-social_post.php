19d1701c4dd9389ded77837d0e7a598cfb41eb82
1ndoryu <andoryyu@gmail.com>
2024-10-24 14:11:59 -0400

    *

--- single-social_post.php
+++ single-social_post.php
@@ -6,42 +6,51 @@ $nologin_class = !is_user_logged_in() ? ' nologin' : '';
 
 if (have_posts()) :
     while (have_posts()) : the_post();
-        // Obtener el ID del post actual ANTES de usarlo
         $current_post_id = get_the_ID();
         $filtro = 'single';
         
-        // Obtener los datos del algoritmo
+        // Inicializar el schema con datos básicos que siempre estarán disponibles
+        $schema = array(
+            "@context" => "https://schema.org",
+            "@type" => "AudioObject",
+            "name" => get_the_title(),
+            "description" => get_the_excerpt() ?: get_the_title(), // Usar excerpt como fallback
+            "datePublished" => get_the_date('c'),
+            "author" => array(
+                "@type" => "Person",
+                "name" => get_the_author()
+            )
+        );
+
+        // Intentar obtener los datos del algoritmo si existen
         $datosAlgoritmo = get_post_meta($current_post_id, 'datosAlgoritmo', true);
-        $datos_decoded = json_decode($datosAlgoritmo, true);
+        if (!empty($datosAlgoritmo)) {
+            $datos_decoded = json_decode($datosAlgoritmo, true);
+            
+            if ($datos_decoded && is_array($datos_decoded)) {
+                // Actualizar la descripción si está disponible
+                if (isset($datos_decoded['descripcion_ia_pro']['es'])) {
+                    $schema['description'] = $datos_decoded['descripcion_ia_pro']['es'];
+                } elseif (isset($datos_decoded['descripcion_ia']['es'])) {
+                    $schema['description'] = $datos_decoded['descripcion_ia']['es'];
+                }
 
-        // Verificar si los datos existen y tienen el formato correcto
-        if ($datos_decoded && is_array($datos_decoded)) {
-            // Determinar qué clave usar para la descripción
-            $descripcion = isset($datos_decoded['descripcion_ia_pro']['es']) 
-                ? $datos_decoded['descripcion_ia_pro']['es'] 
-                : (isset($datos_decoded['descripcion_ia']['es']) 
-                    ? $datos_decoded['descripcion_ia']['es'] 
-                    : '');
+                // Añadir género si está disponible
+                if (isset($datos_decoded['genero_posible']['es'])) {
+                    $schema['genre'] = $datos_decoded['genero_posible']['es'];
+                }
 
-            // Verificar y procesar los tags
-            $tags = isset($datos_decoded['tags_posibles']['es']) && is_array($datos_decoded['tags_posibles']['es'])
-                ? implode(", ", $datos_decoded['tags_posibles']['es'])
-                : '';
-
-            $schema = array(
-                "@context" => "https://schema.org",
-                "@type" => "AudioObject",
-                "name" => get_the_title(),
-                "description" => $descripcion,
-                "genre" => isset($datos_decoded['genero_posible']['es']) ? $datos_decoded['genero_posible']['es'] : '',
-                "keywords" => $tags,
-                "datePublished" => get_the_date('c'),
-                "author" => array(
-                    "@type" => "Person",
-                    "name" => get_the_author()
-                )
-            );
+                // Añadir tags si están disponibles
+                if (isset($datos_decoded['tags_posibles']['es']) && is_array($datos_decoded['tags_posibles']['es'])) {
+                    $schema['keywords'] = implode(", ", $datos_decoded['tags_posibles']['es']);
+                }
+            }
         }
+
+        // Imprimir el schema en el head (puedes moverlo a donde sea necesario)
+        add_action('wp_head', function() use ($schema) {
+            echo '<script type="application/ld+json">' . wp_json_encode($schema) . '</script>';
+        });
 
         ob_start();
 ?>
@@ -54,7 +63,6 @@ if (have_posts()) :
                     <div class="publicaciones-similares">
                         <h3>Publicaciones Similares</h3>
                         <?php
-                        // Llamar a la función 'publicaciones' pasando el ID del post actual
                         echo publicaciones([
                             'filtro' => 'nada',
                             'posts' => 10,
