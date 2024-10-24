95d2840b86b5eb0d6a9b4ba88073c6b938badd5d
1ndoryu <andoryyu@gmail.com>
2024-10-24 15:33:57 -0400

    *

--- app/Funcionalidades/likes.php
+++ app/Funcionalidades/likes.php
@@ -2,75 +2,71 @@
 
 function manejarLike() {
     if (!is_user_logged_in()) {
-        guardarLog("Usuario no está logueado");
+        //guardarLog("Usuario no está logueado");
         echo 'not_logged_in';
         wp_die();
     }
 
-    $user_id = get_current_user_id();
-    $post_id = $_POST['post_id'] ?? '';
-    $like_state = $_POST['like_state'] ?? false;
+    $userId = get_current_user_id();
+    $postId = $_POST['post_id'] ?? '';
+    $likeEstado = $_POST['like_state'] ?? false;
 
-    guardarLog("Datos recibidos: user_id = $user_id, post_id = $post_id, like_state = $like_state");
+    //guardarLog("Datos recibidos: user_id = $userId, post_id = $postId, like_state = $likeEstado");
 
-    if (empty($post_id)) {
-        guardarLog("post_id está vacío");
+    if (empty($postId)) {
+        //guardarLog("post_id está vacío");
         echo 'error';
         wp_die();
     }
 
-    $action = $like_state ? 'like' : 'unlike';
-    guardarLog("Acción determinada: $action");
-
-    likeAccion($post_id, $user_id, $action);
-
-    // Contar los likes después de la acción
-    $like_count = contarLike($post_id);
-    guardarLog("Cantidad de likes después de la acción: $like_count");
-
-    echo $like_count;
+    $accion = $likeEstado ? 'like' : 'unlike';
+    //guardarLog("Acción determinada: $accion");
+    likeAccion($postId, $userId, $accion);
+    $contadorLike = contarLike($postId);
+    //guardarLog("Cantidad de likes después de la acción: $contadorLike")
+    echo $contadorLike;
     wp_die();
 }
 
 add_action('wp_ajax_like', 'manejarLike');
 
 
-
-function likeAccion($post_id, $user_id, $action) {
+//aqui esto no debería enviar la notificacion al usuario cuando esta dando like a su propia publicación, pero lo hace
+function likeAccion($postId, $userId, $accion) {
     global $wpdb;
     $table_name = $wpdb->prefix . 'post_likes';
 
-    if ($action === 'like') {
-        if (chequearLike($post_id, $user_id)) {
-            guardarLog("El usuario $user_id ya ha dado like al post $post_id.");
-            $action = 'unlike';  // Si ya le gustó, se cambia a "unlike"
+    if ($accion === 'like') {
+        if (chequearLike($postId, $userId)) {
+            //guardarLog("El usuario $userId ya ha dado like al post $postId.");
+            $accion = 'unlike';  // Si ya le gustó, se cambia a "unlike"
         } else {
-            $insert_result = $wpdb->insert($table_name, ['user_id' => $user_id, 'post_id' => $post_id]);
+            $insert_result = $wpdb->insert($table_name, ['user_id' => $userId, 'post_id' => $postId]);
             if ($insert_result === false) {
-                guardarLog("Error al insertar el like en la base de datos para el post $post_id y el usuario $user_id.");
+                //guardarLog("Error al insertar el like en la base de datos para el post $postId y el usuario $userId.");
             } else {
-                guardarLog("Like insertado correctamente en la base de datos.");
+                //guardarLog("Like insertado correctamente en la base de datos.");
                 // Enviar notificación al autor del post
-                $autor_id = get_post_field('post_author', $post_id);
-                $usuario = get_userdata($user_id);
-                insertar_notificacion($autor_id, "{$usuario->display_name} le gustó tu publicación.", get_permalink($post_id), $user_id);
+                $autor_id = get_post_field('post_author', $postId);
+                $usuario = get_userdata($userId);
+                insertar_notificacion($autor_id, "{$usuario->display_name} le gustó tu publicación.", get_permalink($postId), $userId);
             }
         }
     }
 
-    if ($action === 'unlike') {
-        $delete_result = $wpdb->delete($table_name, ['user_id' => $user_id, 'post_id' => $post_id]);
+    if ($accion === 'unlike') {
+        $delete_result = $wpdb->delete($table_name, ['user_id' => $userId, 'post_id' => $postId]);
         if ($delete_result === false) {
-            guardarLog("Error al eliminar el like en la base de datos para el post $post_id y el usuario $user_id.");
+            //guardarLog("Error al eliminar el like en la base de datos para el post $postId y el usuario $userId.");
         } else {
-            guardarLog("Like eliminado correctamente.");
+            //guardarLog("Like eliminado correctamente.");
         }
     }
 }
 
 
 
-function obtenerLikesDelUsuario($user_id, $limit = 500)
+function obtenerLikesDelUsuario($userId, $limit = 500)
 {
     global $wpdb;
     $table_name = $wpdb->prefix . 'post_likes';
@@ -78,7 +74,7 @@ function obtenerLikesDelUsuario($user_id, $limit = 500)
     // Prepara la consulta SQL con un límite opcional
     $query = $wpdb->prepare(
         "SELECT post_id FROM $table_name WHERE user_id = %d ORDER BY like_date DESC LIMIT %d",
-        $user_id, $limit
+        $userId, $limit
     );
     
     // Obtener los IDs de los posts con like
@@ -92,28 +88,28 @@ function obtenerLikesDelUsuario($user_id, $limit = 500)
     return $liked_posts;
 }
 
-function contarLike($post_id) {
+function contarLike($postId) {
     global $wpdb;
     $table_name = $wpdb->prefix . 'post_likes';
     
     // Contar el número de likes para el post
-    $like_count = $wpdb->get_var($wpdb->prepare(
+    $contadorLike = $wpdb->get_var($wpdb->prepare(
         "SELECT COUNT(*) FROM $table_name WHERE post_id = %d",
-        $post_id
+        $postId
     ));
 
-    return $like_count ? $like_count : 0;
+    return $contadorLike ? $contadorLike : 0;
 }
 
-function chequearLike($post_id, $user_id) {
+function chequearLike($postId, $userId) {
     global $wpdb;
     $table_name = $wpdb->prefix . 'post_likes';
 
     // Verificar si el usuario ya ha dado like al post
     $results = $wpdb->get_var($wpdb->prepare(
         "SELECT COUNT(1) FROM $table_name WHERE post_id = %d AND user_id = %d",
-        $post_id,
-        $user_id
+        $postId,
+        $userId
     ));
 
     return $results > 0;
@@ -122,20 +118,20 @@ function chequearLike($post_id, $user_id) {
 
 
 
-function like($post_id)
+function like($postId)
 {
-    $user_id = get_current_user_id();
-    $like_count = contarLike($post_id);
-    $user_has_liked = chequearLike($post_id, $user_id);
+    $userId = get_current_user_id();
+    $contadorLike = contarLike($postId);
+    $user_has_liked = chequearLike($postId, $userId);
     $liked_class = $user_has_liked ? 'liked' : 'not-liked';
 
     ob_start();
 ?>
     <div class="TJKQGJ botonlike">
-        <button class="post-like-button <?= esc_attr($liked_class) ?>" data-post_id="<?= esc_attr($post_id) ?>" data-nonce="<?= wp_create_nonce('like_post_nonce') ?>">
+        <button class="post-like-button <?= esc_attr($liked_class) ?>" data-post_id="<?= esc_attr($postId) ?>" data-nonce="<?= wp_create_nonce('like_post_nonce') ?>">
             <? echo $GLOBALS['iconoCorazon']; ?>
         </button>
-        <span class="like-count"><?= esc_html($like_count) ?></span>
+        <span class="like-count"><?= esc_html($contadorLike) ?></span>
     </div>
 <?
     $output = ob_get_clean();
