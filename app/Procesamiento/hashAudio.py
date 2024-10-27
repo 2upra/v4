91217796712f57a1136bb886ca0a1f46731adaa5
1ndoryu <andoryyu@gmail.com>
2024-10-27 17:43:19 -0400

    *

--- app/Procesamiento/hashAudio.py
+++ app/Procesamiento/hashAudio.py
@@ -1,25 +1,28 @@
+import os
 import librosa
 import numpy as np
 import hashlib
 
 def calcular_hash_audio(audio_path):
     try:
+        # Verificar si el archivo existe
+        if not os.path.exists(audio_path):
+            print(f"Archivo no encontrado: {audio_path}")
+            return None
+
         # Cargar el archivo de audio
         y, sr = librosa.load(audio_path, sr=None)
 
-        # Extraer el mel-spectrogram (representación de las características del audio)
+        # Extraer el mel-spectrogram
         mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
 
-        # Tomar el logaritmo del mel-spectrogram para hacerlo más manejable
+        # Tomar el logaritmo
         log_mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
 
-        # Convertir el espectrograma a una representación en bytes
+        # Convertir a bytes y calcular el hash
         mel_bytes = log_mel_spectrogram.tobytes()
-
-        # Crear un hash usando SHA-256 (resistente a cambios menores)
         hash_obj = hashlib.sha256(mel_bytes)
 
-        # Devolver el hash en formato hexadecimal
         return hash_obj.hexdigest()
     
     except Exception as e:
