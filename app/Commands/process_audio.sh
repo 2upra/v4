60bd7bc6042374e4f61ba7af2a2578370a1fe3fa
1ndoryu <andoryyu@gmail.com>
2024-10-29 05:08:45 +0100

    *

--- app/Commands/process_audio.sh
+++ app/Commands/process_audio.sh
@@ -5,4 +5,4 @@ export PYTHONPATH=/usr/local/lib/python3.9/dist-packages:/usr/lib/python3/dist-p
 export PATH=/usr/local/bin:/usr/bin:$PATH
 
 # Ejecutar el script Python
-/usr/bin/python3 /var/www/wordpress/wp-content/themes/2upra3v/app/Procesamiento/hashAudio.py "$1"
+/usr/bin/python3 /var/www/wordpress/wp-content/themes/2upra3v/app/python/hashAudio.py "$1"
