632370e72e5bd8273f46e557086da734b84bed29
1ndoryu <andoryyu@gmail.com>
2024-11-01 21:39:29 -0400

    *

--- app/Commands/permisos.sh
+++ app/Commands/permisos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/bin/chmod -R 770 /home/asley01/MEGA/Waw/Kits/
-/bin/chown -R asley01:www-data /home/asley01/MEGA/Waw/Kits/
+/bin/chmod -R 770 /home/asley01/MEGA/Waw/
+/bin/chown -R asley01:www-data /home/asley01/MEGA/Waw/
