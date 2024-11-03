a891cb3902c604d9c8533c48fa89c089ba533987
1ndoryu <andoryyu@gmail.com>
2024-11-02 21:29:08 -0400

    *

--- app/Functions/mostrarTags.php
+++ app/Functions/mostrarTags.php
@@ -1,7 +1,7 @@
 <?
 
 function tagsFrecuentes() {
-    $cache_key = 'tagsFrecuentes';
+    $cache_key = 'tagsFrecuentes2';
     $tags_frecuentes = get_transient($cache_key);
 
     // Si los tags ya están en caché, devolverlos
@@ -13,19 +13,26 @@ function tagsFrecuentes() {
     guardarLog("Iniciando recopilación de tags frecuentes.");
 
     $tags_conteo = array();
-    $posts_per_page = 100; // Número de posts por página
+    $posts_per_page = 50; // Número de posts por página
     $paged = 1;  // Empezamos en la página 1
     $total_posts = 0;
+    $max_num_pages = null;
 
     do {
         $args = array(
             'post_type' => 'social_post', // Modifica si usas un custom post type
             'posts_per_page' => $posts_per_page, // Obtener los posts en lotes
             'paged' => $paged, // Paginación
-            'meta_key' => 'datosAlgoritmo'
+            'meta_key' => 'datosAlgoritmo',
+            'no_found_rows' => false, // Necesitamos el total de posts para la paginación
         );
 
         $query = new WP_Query($args);
+
+        if ($max_num_pages === null) {
+            $max_num_pages = $query->max_num_pages;
+            guardarLog("Número máximo de páginas: $max_num_pages");
+        }
 
         guardarLog("Número de posts encontrados en la página $paged: " . $query->found_posts);
         $total_posts += $query->found_posts;
@@ -61,7 +68,7 @@ function tagsFrecuentes() {
 
         // Incrementar la página para la siguiente iteración
         $paged++;
-    } while ($query->have_posts());
+    } while ($paged <= $max_num_pages);
 
     guardarLog("Total de posts procesados: $total_posts");
 
@@ -78,7 +85,6 @@ function tagsFrecuentes() {
 
     return $tags_frecuentes;
 }
-
 function tagsPosts() {
     $tags_frecuentes = tagsFrecuentes();
 
