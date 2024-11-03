95ee5b006b90996635f67708d6fc87e6d9b52723
1ndoryu <andoryyu@gmail.com>
2024-11-03 00:41:07 -0400

    *

--- app/Auto/busquedaAudio.php
+++ app/Auto/busquedaAudio.php
@@ -3,8 +3,8 @@
 add_action('init', 'iniciar_cron_procesamiento_audios');
 function iniciar_cron_procesamiento_audios()
 {
-    if (!wp_next_scheduled('adfadsfsdf')) {
-        wp_schedule_event(time(), 'cadaDosMinutos', 'adfadsfsdf');
+    if (!wp_next_scheduled('58au')) {
+        wp_schedule_event(time(), 'cadaDosMinutos', '58au');
         autLog("Cron de procesamiento de audios programado para cada 2 minutos.");
     }
 }
@@ -14,14 +14,14 @@ function definir_cron_cada_dos_minutos($schedules)
 {
     if (!isset($schedules['cadaDosMinutos'])) {
         $schedules['cadaDosMinutos'] = array(
-            'interval' => 50,
+            'interval' => 58,
             'display'  => __('Cada 2 minutos')
         );
     }
     return $schedules;
 }
 
-add_action('adfadsfsdf', 'procesarAudios');
+add_action('58au', 'procesarAudios');
 
 
 function procesarAudios()
