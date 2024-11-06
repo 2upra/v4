0b8e5d9bbb6cf72d18dc6fa441e7cfedc7b28f00
1ndoryu <andoryyu@gmail.com>
2024-11-06 15:44:47 -0400

    *

--- js/genericAjax.js
+++ js/genericAjax.js
@@ -26,33 +26,97 @@ async function eliminarPost() {
     );
 }
 
+
 async function verificarPost() {
     await accionClick(
         '.verificarPost',
         'verificarPost',
         'Verificar este post tilin',
-        async (statusElement, data) => {
+        async (statusElement, data, post_id) => {
             actualizarElemento(statusElement, data.new_status);
             
-            // Seleccionar el div de verificarPost
-            const verificarPostDiv = document.querySelector('.verificarPost');
+            // Seleccionar el div específico de verificarPost usando el post_id
+            const verificarPostDiv = document.querySelector(`.verificarPost[data-post-id="${post_id}"]`);
             
-            // Limpiar el contenido actual
-            verificarPostDiv.innerHTML = '';
-            
-            // Agregar el nuevo SVG
-            const newSvg = `
-                <svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" style="color: currentcolor;">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 9.52717V4.057C3.69054 4.00405 3.8926 3.95131 4.10681 3.8954L4.10684 3.89539C4.25396 3.85699 4.40682 3.81709 4.5656 3.7746C5.15243 3.61758 5.79596 3.43066 6.38899 3.17017C6.97334 2.91351 7.55664 2.56529 8 2.05704C8.44336 2.56529 9.02666 2.91351 9.61101 3.17017C10.204 3.43066 10.8476 3.61758 11.4344 3.7746C11.5932 3.81709 11.746 3.85699 11.8932 3.89539C12.1074 3.9513 12.3094 4.00405 12.5 4.057V9.52717C12.5 10.9221 11.7257 12.2018 10.49 12.849L8 14.1533L5.50997 12.849C4.27429 12.2018 3.5 10.9221 3.5 9.52717ZM6.87802 1.06132C7.10537 0.796772 7.25 0.467199 7.25 0H8.75C8.75 0.467199 8.89463 0.796772 9.12198 1.06132C9.3643 1.34329 9.73045 1.58432 10.2142 1.79681C10.6962 2.00853 11.2465 2.17155 11.8221 2.32558C11.9557 2.36133 12.0926 2.39704 12.2305 2.43301L12.2307 2.43305C12.6631 2.54586 13.1054 2.66124 13.4872 2.78849L14 2.95943V3.5V9.52717C14 11.4801 12.916 13.2716 11.186 14.1778L8.34801 15.6644L8 15.8467L7.65199 15.6644L4.81396 14.1778C3.084 13.2716 2 11.4801 2 9.52717V3.5V2.95943L2.51283 2.78849C2.89458 2.66124 3.33687 2.54586 3.76932 2.43305L3.7694 2.43303C3.90732 2.39706 4.04424 2.36134 4.17787 2.32558C4.75351 2.17155 5.30375 2.00853 5.78576 1.79681C6.26955 1.58432 6.6357 1.34329 6.87802 1.06132ZM10.5303 7.53033L11.0607 7L10 5.93934L9.46967 6.46967L7 8.93934L6.53033 8.46967L6 7.93934L4.93934 9L5.46967 9.53033L6.46967 10.5303C6.76256 10.8232 7.23744 10.8232 7.53033 10.5303L10.5303 7.53033Z" fill="currentColor">
-                    </path>
-                </svg>
-            `;
-            verificarPostDiv.innerHTML = newSvg;
+            if (verificarPostDiv) {
+                // Limpiar el contenido actual
+                verificarPostDiv.innerHTML = '';
+                
+                // Agregar el nuevo SVG
+                const newSvg = `
+                    <svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" style="color: currentcolor;">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M3.5 9.52717V4.057C3.69054 4.00405 3.8926 3.95131 4.10681 3.8954L4.10684 3.89539C4.25396 3.85699 4.40682 3.81709 4.5656 3.7746C5.15243 3.61758 5.79596 3.43066 6.38899 3.17017C6.97334 2.91351 7.55664 2.56529 8 2.05704C8.44336 2.56529 9.02666 2.91351 9.61101 3.17017C10.204 3.43066 10.8476 3.61758 11.4344 3.7746C11.5932 3.81709 11.746 3.85699 11.8932 3.89539C12.1074 3.9513 12.3094 4.00405 12.5 4.057V9.52717C12.5 10.9221 11.7257 12.2018 10.49 12.849L8 14.1533L5.50997 12.849C4.27429 12.2018 3.5 10.9221 3.5 9.52717ZM6.87802 1.06132C7.10537 0.796772 7.25 0.467199 7.25 0H8.75C8.75 0.467199 8.89463 0.796772 9.12198 1.06132C9.3643 1.34329 9.73045 1.58432 10.2142 1.79681C10.6962 2.00853 11.2465 2.17155 11.8221 2.32558C11.9557 2.36133 12.0926 2.39704 12.2305 2.43301L12.2307 2.43305C12.6631 2.54586 13.1054 2.66124 13.4872 2.78849L14 2.95943V3.5V9.52717C14 11.4801 12.916 13.2716 11.186 14.1778L8.34801 15.6644L8 15.8467L7.65199 15.6644L4.81396 14.1778C3.084 13.2716 2 11.4801 2 9.52717V3.5V2.95943L2.51283 2.78849C2.89458 2.66124 3.33687 2.54586 3.76932 2.43305L3.7694 2.43303C3.90732 2.39706 4.04424 2.36134 4.17787 2.32558C4.75351 2.17155 5.30375 2.00853 5.78576 1.79681C6.26955 1.58432 6.6357 1.34329 6.87802 1.06132ZM10.5303 7.53033L11.0607 7L10 5.93934L9.46967 6.46967L7 8.93934L6.53033 8.46967L6 7.93934L4.93934 9L5.46967 9.53033L6.46967 10.5303C6.76256 10.8232 7.23744 10.8232 7.53033 10.5303L10.5303 7.53033Z" fill="currentColor">
+                        </path>
+                    </svg>
+                `;
+                verificarPostDiv.innerHTML = newSvg;
+            }
             
             await alert('Actualizado');
         },
         '.EDYQHV'
     );
+}
+// Función genérica para manejar acciones con confirmación y AJAX
+async function accionClick(selector, action, confirmMessage, successCallback, elementToRemoveSelector = null) {
+    const elements = document.querySelectorAll(selector); // Selecciona cualquier elemento que coincida con el selector
+
+    elements.forEach(element => {
+        // Verifica si el listener ya fue añadido
+        if (!element.dataset.listenerAdded) {
+            element.addEventListener('click', async event => {
+                // Previene comportamiento por defecto si es un botón
+                if (element.tagName.toLowerCase() === 'button') {
+                    event.preventDefault();
+                }
+
+                // Obtiene el post_id del elemento actual
+                const post_id = event.currentTarget.dataset.postId || 
+                              event.currentTarget.getAttribute('data-post-id');
+                              
+                const tipoContenido = event.currentTarget.dataset.tipoContenido;
+
+                if (!post_id) {
+                    console.error('No se encontró post_id en el elemento');
+                    return;
+                }
+
+                const confirmed = await confirm(confirmMessage);
+
+                if (confirmed) {
+                    const detalles = document.getElementById('mensajeError')?.value || '';
+                    const descripcion = document.getElementById('mensajeEdit')?.value || '';
+
+                    try {
+                        const data = await enviarAjax(action, {
+                            post_id,
+                            tipoContenido,
+                            detalles,
+                            descripcion
+                        });
+
+                        if (data.success) {
+                            successCallback(null, data, post_id);
+                        } else {
+                            console.error(`Error: ${data.message}`);
+                            alert('Error al enviar petición: ' + (data.message || 'Error desconocido'));
+                        }
+                    } catch (error) {
+                        console.error('Error al procesar la acción:', error);
+                        alert('Ocurrió un error al procesar la acción.');
+                    }
+                }
+            });
+            
+            // Añade cursor pointer si es un div u otro elemento que no sea botón
+            if (element.tagName.toLowerCase() !== 'button') {
+                element.style.cursor = 'pointer';
+            }
+            
+            // Marca el elemento para indicar que ya tiene un listener
+            element.dataset.listenerAdded = 'true';
+        }
+    });
 }
 
 async function permitirDescarga() {	
@@ -294,67 +358,7 @@ async function handleAllRequests() {
     }
 }
 
-// Función genérica para manejar acciones con confirmación y AJAX
-async function accionClick(selector, action, confirmMessage, successCallback, elementToRemoveSelector = null) {
-    const elements = document.querySelectorAll(selector); // Selecciona cualquier elemento que coincida con el selector
 
-    elements.forEach(element => {
-        // Verifica si el listener ya fue añadido
-        if (!element.dataset.listenerAdded) {
-            element.addEventListener('click', async event => {
-                // Previene comportamiento por defecto si es un botón
-                if (element.tagName.toLowerCase() === 'button') {
-                    event.preventDefault();
-                }
-
-                // Obtiene el post_id del elemento actual
-                const post_id = event.currentTarget.dataset.postId || 
-                              event.currentTarget.getAttribute('data-post-id');
-                              
-                const tipoContenido = event.currentTarget.dataset.tipoContenido;
-
-                if (!post_id) {
-                    console.error('No se encontró post_id en el elemento');
-                    return;
-                }
-
-                const confirmed = await confirm(confirmMessage);
-
-                if (confirmed) {
-                    const detalles = document.getElementById('mensajeError')?.value || '';
-                    const descripcion = document.getElementById('mensajeEdit')?.value || '';
-
-                    try {
-                        const data = await enviarAjax(action, {
-                            post_id,
-                            tipoContenido,
-                            detalles,
-                            descripcion
-                        });
-
-                        if (data.success) {
-                            successCallback(null, data, post_id);
-                        } else {
-                            console.error(`Error: ${data.message}`);
-                            alert('Error al enviar petición: ' + (data.message || 'Error desconocido'));
-                        }
-                    } catch (error) {
-                        console.error('Error al procesar la acción:', error);
-                        alert('Ocurrió un error al procesar la acción.');
-                    }
-                }
-            });
-            
-            // Añade cursor pointer si es un div u otro elemento que no sea botón
-            if (element.tagName.toLowerCase() !== 'button') {
-                element.style.cursor = 'pointer';
-            }
-            
-            // Marca el elemento para indicar que ya tiene un listener
-            element.dataset.listenerAdded = 'true';
-        }
-    });
-}
 
 async function requestDeletion() {
     await accionClick(
