60452509fd3fa0dd6d09bb429b0230fa32a5ee1a
1ndoryu <andoryyu@gmail.com>
2024-11-08 08:48:09 -0400

    *

--- header.php
+++ header.php
@@ -23,7 +23,6 @@ if (!defined('ABSPATH')) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; object-src 'none'; media-src 'self'">
     <!-- Título SEO -->
     <title><?php echo get_seo_title(); ?></title>
 
