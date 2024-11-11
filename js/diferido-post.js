b6f74702b0c2661f61ded9e4365c2c3f0b8459d6
1ndoryu <andoryyu@gmail.com>
2024-11-11 03:41:29 -0400

    *

--- js/diferido-post.js
+++ js/diferido-post.js
@@ -280,7 +280,7 @@
     }
 
     window.limpiarBusqueda = function() {
-        publicacionesCargadas.clear();
+        //publicacionesCargadas.clear();
         identificador = '';
         actualizarUIBusqueda('');
         resetearCarga();
