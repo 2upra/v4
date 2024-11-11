a1b2a97b4f7f0db384211cf23b4112dbeb84b77c
1ndoryu <andoryyu@gmail.com>
2024-11-10 20:03:52 -0400

    *

--- js/genericAjax.js
+++ js/genericAjax.js
@@ -565,6 +565,7 @@ async function enviarAjax(action, data = {}) {
 async function establecerFiltros() {
     try {
         const response = await enviarAjax('obtenerFiltrosTotal');
+        contadorDeSamples();
         if (response.success) {
             const {filtroPost, filtroTiempo} = response.data;
             const hayFiltrosActivados = filtroTiempo !== 0 || filtroPost !== 'a:0:{}';
@@ -599,6 +600,7 @@ async function establecerFiltros() {
         console.error('Error en la solicitud AJAX para obtener filtros:', error);
     }
 }
+
 const FLECHA_SVG = '<svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" style="color: currentcolor;"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.7071 2.39644C8.31658 2.00592 7.68341 2.00592 7.29289 2.39644L4.46966 5.21966L3.93933 5.74999L4.99999 6.81065L5.53032 6.28032L7.99999 3.81065L10.4697 6.28032L11 6.81065L12.0607 5.74999L11.5303 5.21966L8.7071 2.39644ZM5.53032 9.71966L4.99999 9.18933L3.93933 10.25L4.46966 10.7803L7.29289 13.6035C7.68341 13.9941 8.31658 13.9941 8.7071 13.6035L11.5303 10.7803L12.0607 10.25L11 9.18933L10.4697 9.71966L7.99999 12.1893L5.53032 9.71966Z" fill="currentColor"></path></svg>';
 
 // Función para obtener el nombre del filtro según el valor
