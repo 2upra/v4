dfb6e171a561b37fee7bf34721c8210881bc66c4
1ndoryu <andoryyu@gmail.com>
2024-11-18 20:54:40 -0400

    *

--- app/View/InicialModal.php
+++ app/View/InicialModal.php
@@ -8,10 +8,10 @@ function modalPreguntas()
 ?>
     <div class="modal">
         <div class="TIPEARTISTSF">
-            <div class="borde" id="productorDiv">
+            <div class="selectorUsuario borde" id="productorDiv">
                 <p>Productor</p>
             </div>
-            <div class="borde" id="artistaDiv">
+            <div class="selectorUsuario borde" id="artistaDiv">
                 <p>Artista</p>
             </div>
         </div>
