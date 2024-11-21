6af97d0fdf520bbff1b9dfdd7894bca858019b02
1ndoryu <andoryyu@gmail.com>
2024-11-21 16:33:35 -0400

    *

--- single.php
+++ single.php
@@ -1,33 +1,41 @@
-<?
+<?php
 if (! defined('ABSPATH')) {
     exit;
 }
-
+$current_post_id = get_the_ID();
 ?>
 
 <head>
-    <meta charset="<? bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="index, follow">
-    <? wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
 
-<body <? body_class(); ?>>
+<body <?php body_class(); ?>>
 
-    <? get_header(); ?>
+    <?php get_header(); ?>
 
     <main id="main">
-        <div id="content" class="<? echo esc_attr(! is_user_logged_in() ? 'nologin' : ''); ?>">
-            <? if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <article <? post_class(); ?>>
-                      
+        <div id="content" class="<?php echo esc_attr(! is_user_logged_in() ? 'nologin' : ''); ?>">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <article <?php post_class(); ?>>
+                        <h1 class="post-titleSingle"><?php the_title(); ?></h1>
+                        <p class="post-authorSingle">By: <?php the_author(); ?></p>
+                        <div class="post-contentSingle">
+                            <?php the_content(); ?>
+                        </div>
+                        <div class="botonesPost">
+                            <? echo like($post_id); ?>
+                            <? echo botonComentar($post_id, $colab); ?>
+                        </div>
                     </article>
-            <? endwhile;
+            <?php endwhile;
             endif; ?>
         </div>
     </main>
 
-    <? get_footer(); ?>
+    <?php get_footer(); ?>
 
 </body>
 
