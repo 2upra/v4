2c6d03d8518ac3ce55948f367f213f9bab252ac3
1ndoryu <andoryyu@gmail.com>
2024-11-21 18:18:04 -0400

    *

--- app/Content/Posts/htmlArticulo.php
+++ app/Content/Posts/htmlArticulo.php
@@ -39,22 +39,24 @@ function htmlArticulo($filtro)
 
     ob_start();
 ?>
-    <li class="POST-<? echo esc_attr($filtro); ?> EDYQHV"
-        filtro="<? echo esc_attr($filtro); ?> "
-        id-post="<? echo esc_attr($post_id); ?> "
-        autor="<? echo esc_attr($autorId); ?>"
-        style="background-image: url('<? echo esc_url($imagenUrl); ?>'); background-size: cover; background-position: center; position: relative;">
-        
-        <!-- Capa superpuesta para resaltar el texto -->
-        <div class="overlay"></div>
+    <a href="<? echo esc_url(get_permalink($post_id)); ?>" class="post-link">
+        <li class="POST-<? echo esc_attr($filtro); ?> EDYQHV"
+            filtro="<? echo esc_attr($filtro); ?> "
+            id-post="<? echo esc_attr($post_id); ?> "
+            autor="<? echo esc_attr($autorId); ?>"
+            style="background-image: url('<? echo esc_url($imagenUrl); ?>'); background-size: cover; background-position: center; position: relative;">
+            
+            <!-- Capa superpuesta para resaltar el texto -->
+            <div class="overlay"></div>
 
-        <!-- Contenido del post -->
-        <div class="post-content">
-            <h2 class="post-title" data-post-id="<? echo esc_attr($postId); ?>"><? echo get_the_title($postId); ?></h2>
-            <p class="post-author"><? echo get_the_author_meta('display_name', $autorId); ?></p>
-        </div>
+            <!-- Contenido del post -->
+            <div class="post-content">
+                <h2 class="post-title" data-post-id="<? echo esc_attr($postId); ?>"><? echo get_the_title($postId); ?></h2>
+                <p class="post-author"><? echo get_the_author_meta('display_name', $autorId); ?></p>
+            </div>
 
-    </li>
+        </li>
+    </a>
 
     <li class="comentariosPost"></li>
 <?php
