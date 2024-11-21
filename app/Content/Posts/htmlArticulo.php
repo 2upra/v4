f02175c1373d8f668dccd7df5453caec3bb838ef
1ndoryu <andoryyu@gmail.com>
2024-11-21 18:09:46 -0400

    *

--- app/Content/Posts/htmlArticulo.php
+++ app/Content/Posts/htmlArticulo.php
@@ -30,7 +30,6 @@ function imagenArticulo($postId)
     return esc_url($imagenProcesada);
 }
 
-
 function htmlArticulo($filtro)
 {
     $post_id = get_the_ID();
@@ -41,11 +40,15 @@ function htmlArticulo($filtro)
     ob_start();
 ?>
     <li class="POST-<? echo esc_attr($filtro); ?> EDYQHV"
-        filtro="<? echo esc_attr($filtro); ?>"
-        id-post="<? echo esc_attr($post_id); ?>"
+        filtro="<? echo esc_attr($filtro); ?> "
+        id-post="<? echo esc_attr($post_id); ?> "
         autor="<? echo esc_attr($autorId); ?>"
-        style="background-image: url('<? echo esc_url($imagenUrl); ?>'); background-size: cover; background-position: center;">
+        style="background-image: url('<? echo esc_url($imagenUrl); ?>'); background-size: cover; background-position: center; position: relative;">
         
+        <!-- Capa superpuesta para resaltar el texto -->
+        <div class="overlay"></div>
+
+        <!-- Contenido del post -->
         <div class="post-content">
             <h2 class="post-title" data-post-id="<? echo esc_attr($postId); ?>"><? echo get_the_title($postId); ?></h2>
             <p class="post-author"><? echo get_the_author_meta('display_name', $autorId); ?></p>
@@ -57,3 +60,4 @@ function htmlArticulo($filtro)
 <?php
     return ob_get_clean();
 }
+
