a5086b776a4f45ea9ce81aa9221d54584b35a018
1ndoryu <andoryyu@gmail.com>
2024-11-20 20:56:51 -0400

    *

--- TemplateInicio.php
+++ TemplateInicio.php
@@ -46,5 +46,22 @@ setcookie("page_description", $descripcion, time() + 3600, "/");
 
 <?
 get_header();
+$user_id = get_current_user_id();
+$acciones = get_user_meta($user_id, 'acciones', true);
+$nologin_class = !is_user_logged_in() ? ' nologin' : '';
+?>
 
+<div id="main">
+    <div id="content" class="<? echo esc_attr($nologin_class); ?>">
+        <input type="hidden" id="pagina_actual" name="pagina_actual" value="<? echo esc_attr(get_the_title()); ?>">
+        <? if (!is_user_logged_in()): ?>
+            <? echo dev(); ?>
+        <? else: ?>
+            <? echo socialTabs(); ?>
+        <? endif; ?>
+    </div>
+</div>
+
+<?
+get_footer();
 ?>
