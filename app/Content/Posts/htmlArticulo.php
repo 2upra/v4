b59b086f5161ab0feb9ed79c72e6bfaabe186c20
1ndoryu <andoryyu@gmail.com>
2024-11-21 18:33:19 -0400

    *

--- app/Content/Posts/htmlArticulo.php
+++ app/Content/Posts/htmlArticulo.php
@@ -32,24 +32,21 @@ function imagenArticulo($postId)
 
 function htmlArticulo($filtro)
 {
-    $post_id = get_the_ID();
-    $vars = variablesArticulo($post_id);
+    $postId = get_the_ID();
+    $vars = variablesArticulo($postId);
     extract($vars);
     $imagenUrl = imagenArticulo($postId);
 
     ob_start();
 ?>
-    <a href="<? echo esc_url(get_permalink($post_id)); ?>" class="post-link">
+    <a href="<? echo esc_url(get_permalink($postId)); ?>" class="post-link">
         <li class="POST-<? echo esc_attr($filtro); ?> EDYQHV"
             filtro="<? echo esc_attr($filtro); ?> "
-            id-post="<? echo esc_attr($post_id); ?> "
+            id-post="<? echo esc_attr($postId); ?> "
             autor="<? echo esc_attr($autorId); ?>"
             style="background-image: url('<? echo esc_url($imagenUrl); ?>'); background-size: cover; background-position: center; position: relative;">
             
-            <!-- Capa superpuesta para resaltar el texto -->
             <div class="overlay"></div>
-
-            <!-- Contenido del post -->
             <div class="post-content">
                 <h2 class="post-title" data-post-id="<? echo esc_attr($postId); ?>"><? echo get_the_title($postId); ?></h2>
                 <p class="post-author"><? echo get_the_author_meta('display_name', $autorId); ?></p>
