2bddfdba8ab5e3a64f1df1fe462c9a8e6e2b955b
1ndoryu <andoryyu@gmail.com>
2024-11-28 21:00:39 -0400

    *

--- app/Authentication/Iniciar.php
+++ app/Authentication/Iniciar.php
@@ -2,21 +2,11 @@
 
 function iniciar_sesion()
 {
-    /*
-        <label for="nombre_usuario">Nombre de Usuario</label>
-        <input type="text" id="nombre_usuario_login" name="nombre_usuario_login" required class="nombre_usuario"><br>
-        <label for="contrasena_usuario">Contraseña:</label>
-        <input type="password" id="contrasena_usuario_login" name="contrasena_usuario_login" required class="contrasena_usuario"><br>
-        <input class="R0A915 A1" type="submit" name="iniciar_sesion_submit" value="Iniciar sesión">
-    */
-    // Si el usuario ya está logueado, mostrar un mensaje
     if (is_user_logged_in()) {
         return '<div>Ya has iniciado sesión. ¿Quieres cerrar sesión? <a href="' . wp_logout_url(home_url()) . '">Cerrar sesión</a></div>';
     }
 
     $mensaje = '';
-
-    // Procesar el formulario de inicio de sesión
     if (isset($_POST['iniciar_sesion_submit'])) {
         $user = wp_signon(array(
             'user_login' => sanitize_user($_POST['nombre_usuario_login']),
@@ -27,14 +17,11 @@ function iniciar_sesion()
             wp_set_current_user($user->ID);
             wp_set_auth_cookie($user->ID);
 
-            // Redirigir al usuario
-            if (!headers_sent()) {
-                wp_safe_redirect('https://2upra.com');
-                exit;
-            } else {
-                echo "<script>window.location.href='https://2upra.com';</script>";
-                exit;
-            }
+            // Clean output buffer
+            ob_clean();
+
+            wp_safe_redirect('https://2upra.com');
+            exit;
         } else {
             $mensaje = '<div class="error-mensaje">Error al iniciar sesión. Por favor, verifica tus credenciales.</div>';
         }
@@ -42,11 +29,16 @@ function iniciar_sesion()
 
     ob_start();
 ?>
+
     <div class="PUWJVS">
         <form class="CXHMID" action="" method="post">
             <div class="XUSEOO">
+                <label for="nombre_usuario">Nombre de Usuario</label>
+                <input type="text" id="nombre_usuario_login" name="nombre_usuario_login" required class="nombre_usuario"><br>
+                <label for="contrasena_usuario">Contraseña:</label>
+                <input type="password" id="contrasena_usuario_login" name="contrasena_usuario_login" required class="contrasena_usuario"><br>
                 <div class="XYSRLL">
-
+                    <input class="R0A915 A1" type="submit" name="iniciar_sesion_submit" value="Iniciar sesión">
                     <button type="button" class="R0A915 botonprincipal A1 A2" id="google-login-btn"><?php echo $GLOBALS['Google']; ?>Iniciar sesión con Google</button>
 
                     <script>
@@ -69,21 +61,23 @@ function iniciar_sesion()
             <div class="HPUYVS" id="fondograno"><?php echo $GLOBALS['iconologo1']; ?></div>
         </div>
     </div>
+
 <?php
     return ob_get_clean();
 }
 
-// Manejo del callback de Google
-function handle_google_callback() {
-    // Desactiva la caché para esta acción
+
+function handle_google_callback()
+{
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
 
     if (isset($_GET['code'])) {
         $code = $_GET['code'];
         $client_id = '84327954353-lb14ubs4vj4q2q57pt3sdfmapfhdq7ef.apps.googleusercontent.com';
-        $client_secret = ($_ENV['GOOGLEAPI']); // Asegúrate de definir esto correctamente en tu entorno
+        $client_secret = ($_ENV['GOOGLEAPI']);
         $redirect_uri = 'https://2upra.com/google-callback';
-
-        // Solicitar el token de acceso a Google
         $response = wp_remote_post('https://oauth2.googleapis.com/token', array(
             'body' => array(
                 'code' => $code,
@@ -101,8 +95,6 @@ function handle_google_callback() {
 
         $token = json_decode($response['body']);
         $access_token = $token->access_token;
-
-        // Obtener la información del usuario desde Google
         $user_info_response = wp_remote_get('https://www.googleapis.com/oauth2/v1/userinfo?access_token=' . $access_token);
         $user_info = json_decode($user_info_response['body']);
 
@@ -110,76 +102,61 @@ function handle_google_callback() {
             $email = $user_info->email;
             $name = $user_info->name;
 
-            // Verificar si el usuario ya existe
             if ($user = get_user_by('email', $email)) {
                 wp_set_current_user($user->ID);
                 wp_set_auth_cookie($user->ID);
                 $token = generate_secure_token($user->ID);
-
-                if (!headers_sent()) {
-                    if (is_electron_app()) {
-                        wp_redirect('https://2upra.com/app?token=' . $token);
-                    } else {
-                        wp_redirect('https://2upra.com');
-                    }
-                    exit;
+                if (is_electron_app()) {
+                    wp_redirect('https://2upra.com/app?token=' . $token);
                 } else {
-                    if (is_electron_app()) {
-                        echo "<script>window.location.href='https://2upra.com/app?token=" . $token . "';</script>";
-                    } else {
-                        echo "<script>window.location.href='https://2upra.com';</script>";
-                    }
-                    exit;
+                    wp_redirect('https://2upra.com');
                 }
+                exit;
             } else {
-                // Crear un nuevo usuario en WordPress
                 $random_password = wp_generate_password();
                 $user_id = wp_create_user($name, $random_password, $email);
-
                 wp_set_current_user($user_id);
                 wp_set_auth_cookie($user_id);
-
-                if (!headers_sent()) {
-                    wp_redirect('https://2upra.com');
-                    exit;
-                } else {
-                    echo "<script>window.location.href='https://2upra.com';</script>";
-                    exit;
-                }
+                wp_redirect('https://2upra.com');
+                exit;
             }
         }
     }
 }
+
 add_action('init', 'handle_google_callback');
 
 function is_electron_app()
 {
     return isset($_SERVER['HTTP_X_ELECTRON_APP']) && $_SERVER['HTTP_X_ELECTRON_APP'] === 'true';
 }
-function generate_secure_token($user_id) {
+function generate_secure_token($user_id)
+{
     $token = bin2hex(random_bytes(32));
-    $expiration = time() + 36000; 
+    $expiration = time() + 36000;
     $result_token = update_user_meta($user_id, 'session_token', $token);
     $result_expiration = update_user_meta($user_id, 'session_token_expiration', $expiration);
-    
+
     error_log('generate_secure_token - user_id: ' . $user_id . ' token: ' . $token . ' expiration: ' . $expiration  . ' result_token: ' . ($result_token ? 'true' : 'false') . ' result_expiration: ' . ($result_expiration ? 'true' : 'false'));
-    
+
     return $token;
 }
 
-function verify_secure_token($token) {
+function verify_secure_token($token)
+{
     global $wpdb;
     $user_id = $wpdb->get_var($wpdb->prepare(
         "SELECT user_id FROM $wpdb->usermeta WHERE meta_key = 'session_token' AND meta_value = %s AND CAST((SELECT meta_value FROM $wpdb->usermeta WHERE user_id = user_id AND meta_key = 'session_token_expiration') AS UNSIGNED) > %d",
-        $token, time()
+        $token,
+        time()
     ));
-    
+
     error_log('verify_secure_token - token: ' . $token . ' user_id: ' . ($user_id ? $user_id : 'not found'));
 
     if ($user_id) {
         return $user_id;
     }
-    
+
     error_log('verify_secure_token - invalid token: ' . $token);
     return false;
 }
@@ -191,7 +168,8 @@ add_action('rest_api_init', function () {
     ));
 });
 
-function verify_token_endpoint(WP_REST_Request $request) {
+function verify_token_endpoint(WP_REST_Request $request)
+{
     $token = $request->get_param('token');
     $user_id = verify_secure_token($token);
 
@@ -199,7 +177,7 @@ function verify_token_endpoint(WP_REST_Request $request) {
         error_log('verify_token_endpoint - token: ' . $token . ' user_id: ' . $user_id);
         return new WP_REST_Response(array('user_id' => $user_id, 'status' => 'valid'), 200);
     } else {
-        error_log('verify_token_endpoint - invalid token: ' . $token );
+        error_log('verify_token_endpoint - invalid token: ' . $token);
         return new WP_REST_Response(array('message' => 'Token inválido', 'status' => 'invalid'), 401);
     }
 }
