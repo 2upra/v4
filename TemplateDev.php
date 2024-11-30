36f0ed6c55c0c5bde15d7b8d243a80163b4cba44
1ndoryu <andoryyu@gmail.com>
2024-11-29 22:17:08 -0400

    *

--- TemplateDev.php
+++ TemplateDev.php
@@ -1,4 +1,4 @@
-<?php
+<?
 /*
 Template Name: dev
 ya hay un h1 adentro de dev
@@ -8,13 +8,13 @@ ya hay un h1 adentro de dev
 add_action('wp_head', 'add_meta_tags');
 function add_meta_tags() {
     ?>
-    <meta name="description" content="<?php echo esc_attr(get_the_excerpt()); ?>">
-    <meta name="keywords" content="<?php echo esc_attr(get_post_meta(get_the_ID(), '_yoast_wpseo_focuskw', true)); ?>">
-    <meta property="og:title" content="<?php echo esc_attr(get_the_title()); ?>">
-    <meta property="og:description" content="<?php echo esc_attr(get_the_excerpt()); ?>">
-    <meta property="og:image" content="<?php echo esc_url(get_the_post_thumbnail_url()); ?>">
-    <meta property="og:url" content="<?php echo esc_url(get_permalink()); ?>">
-    <?php
+    <meta name="description" content="<? echo esc_attr(get_the_excerpt()); ?>">
+    <meta name="keywords" content="<? echo esc_attr(get_post_meta(get_the_ID(), '_yoast_wpseo_focuskw', true)); ?>">
+    <meta property="og:title" content="<? echo esc_attr(get_the_title()); ?>">
+    <meta property="og:description" content="<? echo esc_attr(get_the_excerpt()); ?>">
+    <meta property="og:image" content="<? echo esc_url(get_the_post_thumbnail_url()); ?>">
+    <meta property="og:url" content="<? echo esc_url(get_permalink()); ?>">
+    <?
 }
 
 get_header();
@@ -23,24 +23,24 @@ $nologin_class = !is_user_logged_in() ? ' nologin' : '';
 ?>
 
 <main id="main" role="main">
-    <article id="content" class="<?php echo esc_attr($nologin_class); ?>">
-        <input type="hidden" id="pagina_actual" name="pagina_actual" value="<?php echo esc_attr(get_the_title()); ?>">
+    <article id="content" class="<? echo esc_attr($nologin_class); ?>">
+        <input type="hidden" id="pagina_actual" name="pagina_actual" value="<? echo esc_attr(get_the_title()); ?>">
 
         <div id="menuData" style="display:none;" data-pestana-actual="">
             <div data-tab="2upra"></div>
-            <?php if (current_user_can('administrator')) : ?>
-            <?php endif; ?>
+            <? if (current_user_can('administrator')) : ?>
+            <? endif; ?>
         </div>
 
-        <?php if (is_user_logged_in()) : ?>
-            <?php echo devlogin(); ?>
-        <?php else : ?>
-            <?php echo dev(); ?>
-        <?php endif; ?>
+        <? if (is_user_logged_in()) : ?>
+            <? echo devlogin(); ?>
+        <? else : ?>
+            <? echo dev(); ?>
+        <? endif; ?>
 
     </article>
 </main>
 
-<?php
+<?
 get_footer();
 ?>
