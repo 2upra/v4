4c5a228f6e7c44b20681d11f1362cef509244285
1ndoryu <andoryyu@gmail.com>
2024-12-02 20:53:31 -0400

    *

--- app/Pages/ColabTabs.php
+++ app/Pages/ColabTabs.php
@@ -6,13 +6,13 @@ function colabTabs()
 
 ?>
     <div id="menuData" style="display:none;" pestanaActual="">
-        <div data-tab="Colab"></div>
+        <div data-tab="Colabs"></div>
     </div>
     
     <div class="tabs">
         <div class="tab-content">
 
-            <div class="tab INICIO S4K7I3" id="Colab">
+            <div class="tab INICIO S4K7I3" id="Colabs">
                 <div class="GSDKRA">
                     <? echo colab(); ?>
                 </div>
