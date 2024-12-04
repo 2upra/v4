74ab0b937acd30ac08957173cfefa5936f08fd42
1ndoryu <andoryyu@gmail.com>
2024-12-04 07:14:47 -0400

    }

--- app/Pages/socialTabs.php
+++ app/Pages/socialTabs.php
@@ -19,161 +19,81 @@ function socialTabs()
     <div class="tab S4K7I3" id="Proyecto">
         <? echo devlogin(); ?>
     </div>
-    <div data-tab="Feed"></div
-                <div class="tab INICIO S4K7I3" id="Feed">
-                <div class="OXMGLZ">
-                    <div class="OAXRVB">
+    <div data-tab="Feed"></div  
 
-                        <div class="FEDAG5">
-                            <? echo publicaciones(['filtro' => 'sample', 'posts' => 12]); ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            
-            <div class="tab INICIO S4K7I3" id="Colecciones">
-                <div class="BPLBDE">
-                    <? echo publicaciones(['post_type' => 'colecciones', 'filtro' => 'colecciones', 'posts' => 12]); ?>
-                </div>
-            </div>
     */
+
     ob_start();
+
+    // Obtener el tipo de usuario actual (por ejemplo, desde una función o meta del usuario).
+    $usuarioTipo = get_user_meta(get_current_user_id(), 'usuarioTipo', true); // Asumiendo que esto devuelve 'artista' o 'fan'.
+
 ?>
 
     <div id="menuData" style="display:none;" pestanaActual="">
-        <div data-tab="Samples"></div>
+        <? if ($usuarioTipo === 'Artista'): 
+        ?>
+            <div data-tab="Samples"></div>
+        <? endif; ?>
 
+        <? if ($usuarioTipo === 'Fan'):
+        ?>
+            <div data-tab="Feed"></div>
+        <? endif; ?>
     </div>
-
     <div class="tabs">
         <div class="tab-content">
-            <div class="BPLBDE UP">
-                <div class="DHRDTAG">
-                    <? echo tagsPosts() ?>
-                </div>
-                <div class="FDGEDF">
-                    <p id="resultadosPost-sampleList"></p>
-                    <div class="OPCDGED">
-
-                        <button class="restablecerBusqueda" style="display: none;">Restablecer filtros</button>
-                        <button class="filtrosboton">
-                            <?
-                            $user_id = get_current_user_id();
-                            $filtro_tiempo = get_user_meta($user_id, 'filtroTiempo', true);
-                            $filtro_tiempo = $filtro_tiempo === '' ? 0 : intval($filtro_tiempo);
-
-                            $nombres_filtros = array(
-                                0 => 'Feed',
-                                1 => 'Reciente',
-                                2 => 'Semanal',
-                                3 => 'Mensual'
-                            );
-
-                            $nombre_filtro = isset($nombres_filtros[$filtro_tiempo]) ? $nombres_filtros[$filtro_tiempo] : 'Feed';
-                            echo $nombre_filtro . ' ' . $GLOBALS['iconoflechaArriAba'];
-                            ?>
-                        </button>
-
-                        <?
-                        $filtroTiempo = get_user_meta(get_current_user_id(), 'filtroTiempo', true);
-                        ?>
-
-
-                        <div class="A1806241" id="filtrosMenu-default">
-                            <div class="A1806242">
-                                <button class="filtroFeed <? echo ($filtroTiempo == 0 || $filtroTiempo === '') ? 'filtroSelec' : ''; ?>">Para mí</button>
-                                <button class="filtroReciente <? echo ($filtroTiempo == 1) ? 'filtroSelec' : ''; ?>">Recientes</button>
-                                <button class="filtroSemanal <? echo ($filtroTiempo == 2) ? 'filtroSelec' : ''; ?>">Top Semanal</button>
-                                <button class="filtroMensual <? echo ($filtroTiempo == 3) ? 'filtroSelec' : ''; ?>">Top Mensual</button>
-                            </div>
-                        </div>
-                        <button class="ORDENPOSTSL" id="ORDENPOSTSL">Opciones<? echo $GLOBALS['flechaAbajo']; ?></button>
-                        <div class="opcionCheckBox modal" id="filtrosPost" style="display: none;">
-
-                            <div class="opcionCheck">
-                                <div>
-                                    <label>Ocultar descargados</label>
-                                    <p class="description">No verás samples ya descargados.</p>
-                                </div>
-                                <label class="switch">
-                                    <input type="checkbox" name="ocultarDescargados" id="ocultarDescargados">
-                                    <span class="slider"></span>
-                                </label>
-                            </div>
-
-                            <div class="opcionCheck">
-                                <div>
-                                    <label>Ocultar en colecciones</label>
-                                    <p class="description">Se excluyen samples en tus colecciones.</p>
-                                </div>
-                                <label class="switch">
-                                    <input type="checkbox" name="ocultarEnColeccion" id="ocultarEnColeccion">
-                                    <span class="slider"></span>
-                                </label>
-                            </div>
-
-                            <div class="opcionCheck">
-                                <div>
-                                    <label>Mostrar solo con mi like</label>
-                                    <p class="description">Verás solo samples que te gustaron.</p>
-                                </div>
-                                <label class="switch">
-                                    <input type="checkbox" name="mostrarMeGustan" id="mostrarMeGustan">
-                                    <span class="slider"></span>
-                                </label>
-                            </div>
-
-                            <div class="opcionCheck">
-                                <div>
-                                    <label>Mostrar solo mi contenido</label>
-                                    <p class="description">Solo se muestra lo que publicaste.</p>
-                                </div>
-                                <label class="switch">
-                                    <input type="checkbox" name="misColecciones" id="misColecciones">
-                                    <span class="slider"></span>
-                                </label>
-                            </div>
-
-                            <div class="XJAAHB">
-                                <button class="botonsecundario borde">Restablecer</button>
-                                <button class="botonprincipal">Guardar</button>
-                            </div>
-                        </div>
-
+            <? if ($usuarioTipo === 'Artista'): // Mostrar solo si el usuario es artista 
+            ?>
+                <div class="BPLBDE UP">
+                    <div class="DHRDTAG">
+                        <? echo tagsPosts(); ?>
+                    </div>
+                    <div class="FDGEDF">
+                        <p id="resultadosPost-sampleList"></p>
+                        <? echo renderFiltro(); ?>
                     </div>
                 </div>
-            </div>
 
-            <div class="tab INICIO S4K7I3" id="Samples">
-                <div class="BPLBDE">
-                    <div class="FOFDV5">
-                        <? echo publicaciones(['filtro' => 'sampleList', 'tab_id' => 'Samples', 'posts' => 12]); ?>
+                <div class="tab INICIO S4K7I3" id="Samples">
+                    <div class="BPLBDE">
+                        <div class="FOFDV5">
+                            <? echo publicaciones(['filtro' => 'sampleList', 'tab_id' => 'Samples', 'posts' => 12]); ?>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <? endif; ?>
 
-
-
-
+            <? if ($usuarioTipo === 'Fan'): // Mostrar solo si el usuario es fan 
+            ?>
+                <div class="tab INICIO S4K7I3" id="Feed">
+                    <div class="OXMGLZ">
+                        <div class="OAXRVB">
+                            <div class="FEDAG5">
+                                <? echo publicaciones(['filtro' => 'sample', 'tab_id' => 'Feed', 'posts' => 12]); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <? endif; ?>
         </div>
     </div>
 
-<?
+    <?
     return ob_get_clean();
-}
+    ?>
 
 
 
-function momentosfijos()
-{
+    function momentosfijos()
+    {
     ob_start();
 
     $imagenUno = "https://images.ctfassets.net/kftzwdyauwt9/2CPrXUZS0yLGo894hU24zv/b9e1759c6f213a8888e17852266c515b/apple-art-2a-3x4.jpg?w=640&q=90&fm=webp";
     $imagenDos = "https://images.ctfassets.net/kftzwdyauwt9/1ZTOGp7opuUflFmI2CsATh/df5da4be74f62c70d35e2f5518bf2660/ChatGPT_Carousel1.png?w=640&q=90&fm=webp";
     $imagenTres = "https://images.ctfassets.net/kftzwdyauwt9/3XDJfuQZLCKWAIOleFIFZn/14b93d23652347ee7706eca921e3a716/enterprise.png?w=640&q=90&fm=webp";
 
-?>
+    ?>
     <div class="ZCOPHT" style="background-image: url('<? echo esc_url($imagenUno); ?>');" onclick="window.location.href='https://2upra.com/quehacer';">
         <p>Que hacer en 2upra</p>
     </div>
