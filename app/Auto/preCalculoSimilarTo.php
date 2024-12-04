be5563aa08c5a31a9e5a31ec8213ee066553b2d1
1ndoryu <andoryyu@gmail.com>
2024-12-04 04:43:09 -0400

    *

--- app/Auto/preCalculoSimilarTo.php
+++ app/Auto/preCalculoSimilarTo.php
@@ -1,5 +1,5 @@
 <?
-
+/*
 // Asegúrate de definir estas constantes en tu código o en tu archivo de configuración.
 define('SIMILAR_TO_PROCESS_LOCK', 'similar_to_process_lock');
 define('SIMILAR_TO_MAX_LOCK_TIME', 300); // 5 minutos, ajusta según tus necesidades
@@ -116,9 +116,7 @@ function agregar_cron_30_segundos($schedules) {
 }
 add_filter('cron_schedules', 'agregar_cron_30_segundos');
 
-/**
- * Inicializar el evento cron si no está ya programado.
- */
+
 function inicializar_cron() {
     if (!wp_next_scheduled('recalcular_similar_to_feed_cron')) {
         wp_schedule_event(time(), 'every_30_seconds', 'recalcular_similar_to_feed_cron');
@@ -127,18 +125,15 @@ function inicializar_cron() {
 }
 add_action('init', 'inicializar_cron');
 
-/**
- * Asignar la función 'recalcularSimilarToFeed' al evento cron.
- */
+
 add_action('recalcular_similar_to_feed_cron', 'recalcularSimilarToFeed');
 
-/**
- * Función para limpiar el bloqueo manualmente.
- * Puedes llamar a esta función desde cualquier lugar (por ejemplo, un botón en el admin) para limpiar el bloqueo.
- */
+
 function limpiar_bloqueo_similar_to() {
     delete_transient(SIMILAR_TO_PROCESS_LOCK);
     //guardarLog("Bloqueo de proceso similar_to limpiado manualmente.");
 }
 // Puedes asignar esta función a una acción específica si lo deseas, por ejemplo:
 add_action('admin_post_limpia_bloqueo_similar_to', 'limpiar_bloqueo_similar_to');
+
+*/
