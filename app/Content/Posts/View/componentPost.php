32b942c4748b32aaa78a6bd30ee1629976bbaa2c
1ndoryu <andoryyu@gmail.com>
2024-12-05 00:03:25 -0400

    *

--- app/Content/Posts/View/componentPost.php
+++ app/Content/Posts/View/componentPost.php
@@ -431,19 +431,29 @@ function botonComentar($postId)
 
 function fondoPost($filtro, $block, $es_suscriptor, $postId)
 {
-
     if (!in_array($filtro, ['rolastatus1', 'rolasEliminadas1', 'rolasRechazadas1'])) {
+        // Verificar si el post tiene una foto de portada
+        $thumbnail_url = get_the_post_thumbnail_url($postId, 'full');
+        // Verificar si el post tiene la meta 'audio_post_id'
+        $audio_post_id = get_post_meta($postId, 'audio_post_id', true);
+
+        // Si tiene foto de portada pero no tiene 'audio_post_id', no colocar la imagen de fondo
+        if ($thumbnail_url && empty($audio_post_id)) {
+            return ''; // No mostrar el fondo
+        }
+
+        // Continuar con los ajustes del fondo
         $blurred_class = ($block && !$es_suscriptor) ? 'blurred' : '';
         $image_size = ($block && !$es_suscriptor) ? 'thumbnail' : 'large';
         $quality = ($block && !$es_suscriptor) ? 20 : 80;
-        $thumbnail_url = get_the_post_thumbnail_url($postId, 'full');
         $optimized_thumbnail_url = img($thumbnail_url, 40, 'all');
+
         ob_start();
         ?>
         <div class="post-background <?= $blurred_class ?>"
-            style="background-image: linear-gradient(to top, rgba(9, 9, 9, 10), rgba(0, 0, 0, 0) 100%), url(<? echo esc_url($optimized_thumbnail_url) ?>);">
+             style="background-image: linear-gradient(to top, rgba(9, 9, 9, 10), rgba(0, 0, 0, 0) 100%), url(<?php echo esc_url($optimized_thumbnail_url); ?>);">
         </div>
-    <?
+        <?php
         $output = ob_get_clean();
         return $output;
     }
