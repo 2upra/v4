55fc90bd5777a123eaf065e9d75b10f703b4eee1
1ndoryu <andoryyu@gmail.com>
2024-12-06 10:33:19 -0400

    *

--- js/RS.js
+++ js/RS.js
@@ -11,17 +11,17 @@ const nombreRolaData = {};
 
 let uploadInProgressCount = 0;
 // Logs
-let enablelogRS = true;
-const logRS = enablelogRS ? console.log : function () {};
+//let enablelogRS = true;
+//const //logRS = enablelogRS ? console.log : function () {};
 let waveSurferInstances = {};
 
 let rsIniciado = false;
 function iniciarRS() {
     if (rsIniciado) return;
     rsIniciado = true;
-    logRS('comienzoFormRS fue llamado');
+    //logRS('comienzoFormRS fue llamado');
     if (document.getElementById('formRs')) {
-        logRS('formRs existe');
+        //logRS('formRs existe');
         imagenUrl = null;
         imagenId = null;
         audioUrl = null;
@@ -45,7 +45,7 @@ function iniciarRS() {
         selectorformtipo();
         selectorFanArtista();
     } else {
-        logRS('formRs no existe');
+        //logRS('formRs no existe');
     }
 }
 function verificarCamposRs() {
@@ -242,7 +242,7 @@ async function envioRs() {
 }
 
 function subidaRs() {
-    const ids = ['formRs', 'botonAudio', 'botonImagen', 'previewAudio', 'previewArchivo', 'opciones', 'botonArchivo', 'previewImagen', 'enviarRs', 'ppp3'];
+    const ids = ['botonAudio', 'botonImagen', 'botonArchivo'];
     const elements = ids.reduce((acc, id) => {
         const el = document.getElementById(id);
         if (!el) console.warn(`Elemento con id="${id}" no encontrado en el DOM.`);
@@ -250,255 +250,49 @@ function subidaRs() {
         return acc;
     }, {});
 
-    const missingElements = Object.entries(elements)
-        .filter(([_, el]) => !el)
-        .map(([id]) => id);
-    if (missingElements.length) {
-        return;
-    }
+    const { botonAudio, botonImagen, botonArchivo } = elements;
 
-    const {formRs, botonAudio, botonImagen, previewAudio, previewArchivo, opciones, botonArchivo, previewImagen, enviarRs, ppp3} = elements;
+    // Si alguno de los botones no existe, salimos de la función
+    if (!botonAudio || !botonImagen || !botonArchivo) return;
 
-    const inicialSubida = event => {
-        event.preventDefault();
-        const file = event.dataTransfer?.files[0] || event.target.files[0];
-
-        if (!file) return;
-        if (file.size > 50 * 1024 * 1024) return alert('El archivo no puede superar los 50 MB.');
-
-        file.type.startsWith('audio/') ? subidaAudio(file) : file.type.startsWith('image/') ? subidaImagen(file) : subidaArchivo(file);
-    };
-
-    const actualizarFlexDirection = () => {
-        const previewsFormDiv = document.querySelector('.previewsForm.NGEESM.RS');
-
-        if (previewsFormDiv) {
-            if (audiosData.length > 1) {
-                previewsFormDiv.style.flexDirection = 'column';
-            } else {
-                previewsFormDiv.style.flexDirection = '';
-            }
-        }
-    };
-
-    const musicCheckbox = document.getElementById('musiccheck');
-    const actualizarCamposNombre = () => {
-        const cantidadAudios = audiosData.length;
-        const mostrarCampos = musicCheckbox.checked && cantidadAudios > 1;
-
-        audiosData.forEach(audio => {
-            const inputNombre = document.getElementById(`nombre-${audio.tempId}`);
-            if (inputNombre) {
-                inputNombre.style.display = mostrarCampos ? 'block' : 'none';
-            }
-        });
-    };
-
-    musicCheckbox.addEventListener('change', actualizarCamposNombre);
-
-    const subidaAudio = async file => {
-        try {
-            alert(`Audio subido: ${file.name}`);
-            previewAudio.style.display = 'block';
-            ppp3.style.display = 'flex';
-            opciones.style.display = 'flex';
-            const tempId = `temp-${Date.now()}`;
-            const progressBarId = waveAudio(file, tempId);
-            audiosData.push({tempId, audioUrl: null, audioId: null});
-            actualizarFlexDirection();
-            const {fileUrl, fileId} = await subidaRsBackend(file, progressBarId);
-            const index = audiosData.findIndex(audio => audio.tempId === tempId);
-            if (index !== -1) {
-                audiosData[index].audioUrl = fileUrl;
-                audiosData[index].audioId = fileId;
-                const waveformContainer = document.querySelector(`[data-temp-id="${tempId}"]`);
-                if (waveformContainer) {
-                    waveformContainer.setAttribute('data-audio-url', fileUrl);
-                }
-            }
-            if (audiosData.length > 30) {
-                alert('Ya has subido el límite máximo de 30 audios.');
-            }
-            actualizarCamposNombre();
-        } catch (error) {
-            alert('Hubo un problema al cargar el Audio. Inténtalo de nuevo.');
-        }
-    };
-
-    const waveAudio = (file, tempId) => {
-        const reader = new FileReader(),
-            audioContainerId = `waveform-container-${Date.now()}`,
-            progressBarId = `progress-${Date.now()}`;
-
-        reader.onload = e => {
-            const newWaveform = document.createElement('div');
-            newWaveform.innerHTML = `
-            <div id="${audioContainerId}" class="waveform-wrapper">
-                <div class="waveform-container without-image" data-temp-id="${tempId}">
-                    <div class="waveform-loading" style="display: none;">Cargando...</div>
-                    <audio controls style="width: 100%;"><source src="${e.target.result}" type="${file.type}"></audio>
-                    <div class="file-name">${file.name}</div>
-                    <button class="delete-waveform">Eliminar</button>
-                </div>
-                <div class="progress-bar" style="width: 100%; height: 2px; background-color: #ddd; margin-top: 10px;">
-                    <div id="${progressBarId}" class="progress" style="width: 0%; height: 100%; background-color: #4CAF50; transition: width 0.3s;"></div>
-                </div>
-                <!-- Campo de texto para el nombre del audio -->
-                <input type="text" id="nombre-${tempId}" class="nombreAudioRs" placeholder="Titulo" style="display: none; margin-top: 5px; width: 100%;">
-            </div>`;
-
-            previewAudio.appendChild(newWaveform);
-            inicializarWaveform(audioContainerId, e.target.result);
-            const deleteButton = newWaveform.querySelector('.delete-waveform');
-            deleteButton.addEventListener('click', event => {
-                event.stopPropagation();
-                eliminarWaveform(audioContainerId, tempId);
-            });
-
-            // Actualiza los campos de nombre después de agregar un nuevo audio
-            actualizarCamposNombre();
-        };
-
-        reader.readAsDataURL(file);
-        return progressBarId;
-    };
-
-    const eliminarWaveform = (containerId, tempId) => {
-        const wrapper = document.getElementById(containerId);
-
-        if (wrapper) {
-            wrapper.parentNode.removeChild(wrapper);
-            if (waveSurferInstances[containerId]) {
-                waveSurferInstances[containerId].unAll();
-                if (waveSurferInstances[containerId].isPlaying()) {
-                    waveSurferInstances[containerId].stop();
-                }
-                waveSurferInstances[containerId].destroy();
-                delete waveSurferInstances[containerId];
-            }
-        }
-
-        const index = audiosData.findIndex(audio => audio.tempId === tempId);
-        if (index !== -1) {
-            audiosData.splice(index, 1);
-        }
-
-        if (audiosData.length === 0) {
-            previewAudio.style.display = 'none';
-            ppp3.style.display = 'none';
-        }
-
-        actualizarCamposNombre();
-        actualizarFlexDirection();
-    };
-
-    const subidaArchivo = async file => {
-        subidaArchivoEnProgreso = true;
-        previewArchivo.style.display = 'block';
-        previewArchivo.innerHTML = `<div class="file-name">${file.name}</div><div id="barraProgresoFile" class="progress" style="width: 0%; height: 100%; background-color: #4CAF50; transition: width 0.3s;"></div>`;
-        try {
-            alert(`Archivo subido: ${file.name}`);
-            const {fileUrl, fileId} = await subidaRsBackend(file, 'barraProgresoFile');
-            archivoUrl = fileUrl;
-            archivoId = fileId;
-            subidaArchivoEnProgreso = false;
-        } catch {
-            alert('Hubo un problema al cargar el Archivo. Inténtalo de nuevo.');
-            subidaArchivoEnProgreso = false;
-        }
-    };
-
-    const subidaImagen = async file => {
-        subidaImagenEnProgreso = true;
-        opciones.style.display = 'flex';
-        updatePreviewImagen(file);
-        try {
-            alert(`Imagen subida: ${file.name}`);
-            const {fileUrl, fileId} = await subidaRsBackend(file, 'barraProgresoImagen');
-            imagenUrl = fileUrl;
-            imagenId = fileId;
-            subidaImagenEnProgreso = false;
-        } catch {
-            alert('Hubo un problema al cargar la Imagen. Inténtalo de nuevo.');
-            subidaImagenEnProgreso = false;
-        }
-    };
-
-    const updatePreviewImagen = file => {
-        const reader = new FileReader();
-        reader.onload = e => {
-            previewImagen.innerHTML = `<img src="${e.target.result}" alt="Preview" style="width: 100%; height: 100%; aspect-ratio: 1 / 1; object-fit: cover;">`;
-            previewImagen.style.display = 'block';
-        };
-        reader.readAsDataURL(file);
-    };
-
-    formRs.addEventListener('click', event => {
-        logRS('Evento click detectado en formRs.');
-
-        // Identificar el elemento clickeado que contenga las clases .previewAudio o .previewImagen
-        const clickedElement = event.target.closest('.previewAudio, .previewImagen');
-
-        if (clickedElement) {
-            logRS(`Elemento clickeado: ${clickedElement.tagName}, clases: ${clickedElement.className}`);
-
-            // Determinar el tipo de archivo basado en la clase del elemento clickeado
-            const tipoArchivo = clickedElement.classList.contains('previewAudio') ? 'audio/*' : 'image/*';
-            logRS(`Tipo de archivo determinado: ${tipoArchivo}`);
-
-            // Llamar a la función para abrir el selector de archivos
-            abrirSelectorArchivos(tipoArchivo);
-        } else {
-            logRS('No se encontró un elemento con las clases .previewAudio o .previewImagen en la jerarquía del clic.');
-        }
-    });
-
-    const abrirSelectorArchivos = tipoArchivo => {
-        logRS(`Abrir selector de archivos con tipo: ${tipoArchivo}`);
-
-        // Crear dinámicamente un input de tipo file
+    // Función para abrir el selector de archivos
+    const abrirSelectorArchivos = (tipoArchivo) => {
+        console.log(`Abriendo selector de archivos para tipo: ${tipoArchivo}`);
         const input = document.createElement('input');
         input.type = 'file';
         input.accept = tipoArchivo;
 
-        // Asociar la función de manejo de cambios (onchange)
-        input.onchange = event => {
-            logRS('Evento onchange detectado en el input de archivos.');
-            inicialSubida(event);
+        input.onchange = (event) => {
+            const archivo = event.target.files[0];
+            if (archivo) {
+                console.log(`Archivo seleccionado: ${archivo.name}`);
+            } else {
+                console.log('No se seleccionó ningún archivo.');
+            }
         };
 
-        // Simular el clic del usuario para abrir el selector de archivos
         input.click();
-        logRS('Selector de archivos abierto (input.click ejecutado).');
     };
 
-    // Asociar eventos a los botones para abrir el selector de archivos con diferentes tipos
+    // Asociar eventos a los botones
     botonArchivo.addEventListener('click', () => {
-        logRS('Botón "Subir Archivo" clickeado.');
+        console.log('Botón Archivo clickeado.');
         abrirSelectorArchivos('*');
     });
 
     botonAudio.addEventListener('click', () => {
-        logRS('Botón "Subir Audio" clickeado.');
+        console.log('Botón Audio clickeado.');
         abrirSelectorArchivos('audio/*');
     });
 
     botonImagen.addEventListener('click', () => {
-        logRS('Botón "Subir Imagen" clickeado.');
+        console.log('Botón Imagen clickeado.');
         abrirSelectorArchivos('image/*');
-    });
-
-    ['dragover', 'dragleave', 'drop'].forEach(eventName => {
-        formRs.addEventListener(eventName, e => {
-            e.preventDefault();
-            formRs.style.backgroundColor = eventName === 'dragover' ? '#e9e9e9' : '';
-            eventName === 'drop' && inicialSubida(e);
-        });
     });
 }
 
 async function subidaRsBackend(file, progressBarId) {
-    logRS('Iniciando subida de archivo', {fileName: file.name, fileSize: file.size});
+    //logRS('Iniciando subida de archivo', {fileName: file.name, fileSize: file.size});
 
     // Incrementar el contador de subidas en progreso
     uploadInProgressCount++;
@@ -512,7 +306,7 @@ async function subidaRsBackend(file, progressBarId) {
         const xhr = new XMLHttpRequest();
         xhr.open('POST', my_ajax_object.ajax_url, true);
 
-        logRS('Preparando solicitud AJAX', {url: my_ajax_object.ajax_url});
+        //logRS('Preparando solicitud AJAX', {url: my_ajax_object.ajax_url});
 
         xhr.upload.onprogress = e => {
             if (e.lengthComputable) {
@@ -520,12 +314,12 @@ async function subidaRsBackend(file, progressBarId) {
                 const progressPercent = (e.loaded / e.total) * 100;
                 if (progressBar) progressBar.style.width = `${progressPercent}%`;
 
-                logRS('Actualizando barra de progreso', {loaded: e.loaded, total: e.total, progressPercent});
+                //logRS('Actualizando barra de progreso', {loaded: e.loaded, total: e.total, progressPercent});
             }
         };
 
         xhr.onload = () => {
-            logRS('Respuesta recibida', {status: xhr.status, response: xhr.responseText});
+            //logRS('Respuesta recibida', {status: xhr.status, response: xhr.responseText});
 
             // Decrementar el contador al finalizar la subida
             uploadInProgressCount--;
@@ -534,33 +328,33 @@ async function subidaRsBackend(file, progressBarId) {
                 try {
                     const result = JSON.parse(xhr.responseText);
                     if (result.success) {
-                        logRS('Archivo subido exitosamente', {data: result.data});
+                        //logRS('Archivo subido exitosamente', {data: result.data});
                         resolve(result.data);
                     } else {
-                        logRS('Error en la respuesta del servidor (No éxito)', {response: result});
+                        //logRS('Error en la respuesta del servidor (No éxito)', {response: result});
                         reject(new Error('Error en la respuesta del servidor'));
                     }
                 } catch (error) {
-                    logRS('Error al parsear la respuesta', {errorMessage: error.message, response: xhr.responseText});
+                    //logRS('Error al parsear la respuesta', {errorMessage: error.message, response: xhr.responseText});
                     reject(error);
                 }
             } else {
-                logRS('Error en la carga del archivo', {status: xhr.status, response: xhr.responseText});
+                //logRS('Error en la carga del archivo', {status: xhr.status, response: xhr.responseText});
                 reject(new Error(`Error en la carga del archivo. Status: ${xhr.status}`));
             }
         };
 
         xhr.onerror = () => {
-            logRS('Error en la conexión con el servidor', {status: xhr.status});
+            //logRS('Error en la conexión con el servidor', {status: xhr.status});
             uploadInProgressCount--; // Decrementar el contador en caso de error
             reject(new Error('Error en la conexión con el servidor'));
         };
 
         try {
-            logRS('Enviando solicitud AJAX', {formData});
+            //logRS('Enviando solicitud AJAX', {formData});
             xhr.send(formData);
         } catch (error) {
-            logRS('Error al enviar la solicitud AJAX', {errorMessage: error.message});
+            //logRS('Error al enviar la solicitud AJAX', {errorMessage: error.message});
             uploadInProgressCount--; // Decrementar el contador en caso de error
             reject(new Error('Error al enviar la solicitud AJAX'));
         }
