e7ecfd8050a812fb8581d207cfb875e943ca62bd
1ndoryu <andoryyu@gmail.com>
2024-12-13 06:21:33 -0400

    *

--- functions.php
+++ functions.php
@@ -360,7 +360,7 @@ add_action('wp_enqueue_scripts', 'custom_deregister_jquery', 100);
 
 function scriptsOrdenados()
 {
-    $global_version = '3.0.82';
+    $global_version = '3.0.83';
     $dev_mode = defined('LOCAL') && LOCAL;
     //$error_log = [];
 
