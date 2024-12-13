d7edbf8e24e128123e6cdc1a40865fd86bf634fb
1ndoryu <andoryyu@gmail.com>
2024-12-13 10:23:42 -0400

    *

--- js/genericAjax.js
+++ js/genericAjax.js
@@ -782,9 +782,27 @@ async function establecerFiltros() {
                     let filtroPostObj;
                     if (Array.isArray(filtroPost)) {
                         filtroPostObj = filtroPost;
+                        console.log('establecerFiltros: filtroPost es array', filtroPostObj);
+                    } else if (typeof filtroPost === 'string' && filtroPost.startsWith('a:')) {
+                        // Deserializa la cadena de PHP
+                        try {
+                            const tempDiv = document.createElement('div');
+                            tempDiv.innerHTML = `<textarea>${filtroPost}</textarea>`;
+                            const textarea = tempDiv.querySelector('textarea');
+                            const valor = textarea.value;
+
+                            filtroPostObj = JSON.parse(JSON.stringify(phpUnserialize(valor)));
+
+                            console.log('establecerFiltros: filtroPost deserializado', filtroPostObj);
+                        } catch (error) {
+                            console.error('establecerFiltros: Error al deserializar filtroPost', error);
+                            filtroPostObj = {};
+                        }
                     } else {
-                        filtroPostObj = JSON.parse(filtroPost.replace(/s:(\d+):"(.*?)";/g, '"$2":'));
+                        filtroPostObj = {};
+                        console.log('establecerFiltros: filtroPost no es string ni array', filtroPostObj);
                     }
+
                     console.log('establecerFiltros: filtroPostObj', filtroPostObj);
 
                     const filtrosPost = ['misPost', 'mostrarMeGustan', 'ocultarEnColeccion', 'ocultarDescargados'];
@@ -854,6 +872,15 @@ async function establecerFiltros() {
         console.error('establecerFiltros: Error en AJAX:', error);
     }
     console.log('establecerFiltros: Fin');
+}
+
+function phpUnserialize(str) {
+    try {
+        return unserialize(str);
+    } catch (e) {
+        console.error('Error al unserializar con la libreria php-unserialize:', e);
+        return null;
+    }
 }
 
 const FLECHA_SVG = '<svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" style="color: currentcolor;"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.7071 2.39644C8.31658 2.00592 7.68341 2.00592 7.29289 2.39644L4.46966 5.21966L3.93933 5.74999L4.99999 6.81065L5.53032 6.28032L7.99999 3.81065L10.4697 6.28032L11 6.81065L12.0607 5.74999L11.5303 5.21966L8.7071 2.39644ZM5.53032 9.71966L4.99999 9.18933L3.93933 10.25L4.46966 10.7803L7.29289 13.6035C7.68341 13.9941 8.31658 13.9941 8.7071 13.6035L11.5303 10.7803L12.0607 10.25L11 9.18933L10.4697 9.71966L7.99999 12.1893L5.53032 9.71966Z" fill="currentColor"></path></svg>';
