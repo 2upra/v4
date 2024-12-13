8ea69015b05cc6a487f3ea35d0a3848b3ae3b0d7
1ndoryu <andoryyu@gmail.com>
2024-12-13 10:33:34 -0400

    *

--- js/genericAjax.js
+++ js/genericAjax.js
@@ -779,13 +779,27 @@ async function establecerFiltros() {
             if (hayFiltrosActivados) {
                 console.log('establecerFiltros: Hay filtros activos, procesando...');
                 try {
-                    const filtroPostObj = JSON.parse(filtroPost.replace(/s:(\d+):"(.*?)";/g, '"$2":'));
+                    let filtroPostObj;
+                    if (typeof filtroPost === 'string') {
+                        try {
+                            // Intentamos parsear el JSON directamente
+                            filtroPostObj = JSON.parse(filtroPost);
+                            console.log('establecerFiltros: filtroPost parseado como JSON', filtroPostObj);
+                        } catch (error) {
+                            console.error('establecerFiltros: Error al parsear filtroPost como JSON', error);
+                            filtroPostObj = {};
+                        }
+                    } else {
+                        filtroPostObj = {};
+                        console.log('establecerFiltros: filtroPost no es string', filtroPostObj);
+                    }
+
                     console.log('establecerFiltros: filtroPostObj', filtroPostObj);
 
                     const filtrosPost = ['misPost', 'mostrarMeGustan', 'ocultarEnColeccion', 'ocultarDescargados'];
-                    const hayFiltrosPost = Object.keys(filtroPostObj).some(filtro => filtrosPost.includes(filtro));
+                    const hayFiltrosPost = Array.isArray(filtroPostObj) ? filtroPostObj.some(filtro => filtrosPost.includes(filtro)) : Object.keys(filtroPostObj).some(filtro => filtrosPost.includes(filtro));
                     console.log('establecerFiltros: hayFiltrosPost', hayFiltrosPost);
-                    const hayFiltroColeccion = Object.keys(filtroPostObj).includes('misColecciones');
+                    const hayFiltroColeccion = Array.isArray(filtroPostObj) ? filtroPostObj.includes('misColecciones') : Object.keys(filtroPostObj).includes('misColecciones');
                     console.log('establecerFiltros: hayFiltroColeccion', hayFiltroColeccion);
 
                     // Mostrar el botón correspondiente si es necesario
@@ -849,6 +863,15 @@ async function establecerFiltros() {
         console.error('establecerFiltros: Error en AJAX:', error);
     }
     console.log('establecerFiltros: Fin');
+}
+
+function phpUnserialize(str) {
+    try {
+        return unserialize(str);
+    } catch (e) {
+        console.error('Error al unserializar con la libreria php-unserialize:', e);
+        return null;
+    }
 }
 
 const FLECHA_SVG = '<svg data-testid="geist-icon" height="16" stroke-linejoin="round" viewBox="0 0 16 16" width="16" style="color: currentcolor;"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.7071 2.39644C8.31658 2.00592 7.68341 2.00592 7.29289 2.39644L4.46966 5.21966L3.93933 5.74999L4.99999 6.81065L5.53032 6.28032L7.99999 3.81065L10.4697 6.28032L11 6.81065L12.0607 5.74999L11.5303 5.21966L8.7071 2.39644ZM5.53032 9.71966L4.99999 9.18933L3.93933 10.25L4.46966 10.7803L7.29289 13.6035C7.68341 13.9941 8.31658 13.9941 8.7071 13.6035L11.5303 10.7803L12.0607 10.25L11 9.18933L10.4697 9.71966L7.99999 12.1893L5.53032 9.71966Z" fill="currentColor"></path></svg>';
