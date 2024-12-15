d8bfb745fd3c85f15b507ff402574c72aa8e72ba
1ndoryu <andoryyu@gmail.com>
2024-12-15 04:13:16 -0400

    *

--- app/Pages/busquedaTabs.php
+++ app/Pages/busquedaTabs.php
@@ -6,13 +6,13 @@ function busquedaTabs()
 
 ?>
     <div id="menuData" style="display:none;" pestanaActual="">
-        <div data-tab="Busqueda"></div>
+        <div data-tab="Busquedatab"></div>
     </div>
     
     <div class="tabs">
         <div class="tab-content">
 
-            <div class="tab INICIO S4K7I3" id="Busqueda">
+            <div class="tab INICIO S4K7I3" id="Busquedatab">
                 <div class="GSDKRA DSOE4LS">
                     <? echo busqueda(); ?>
                 </div>
