c07d1c39d2b3c024da5405f1f5413e6f3eac9642
1ndoryu <andoryyu@gmail.com>
2024-12-17 17:29:57 -0400

    *

--- js/stripeCompra.js
+++ js/stripeCompra.js
@@ -1,15 +1,17 @@
 function stripecomprabeat() {
     var botonesComprar = document.querySelectorAll('.botonCompra'); // Selecciona los botones con la clase 'botonCompra'
 
-    if (botonesComprar.length > 0) {  // Verifica si hay botones con la clase
-        botonesComprar.forEach(function(boton) { // Itera sobre cada botón
+    if (botonesComprar.length > 0) {
+        // Verifica si hay botones con la clase
+        botonesComprar.forEach(function (boton) {
+            // Itera sobre cada botón
             boton.addEventListener('click', function (e) {
                 e.preventDefault();
-                
+
                 // Obtiene los valores de los atributos data-* del botón
-                const postId = this.dataset.post_id; 
-                const userId = this.dataset.user_id; 
-                const nonce = this.dataset.nonce; 
+                const postId = this.dataset.post_id;
+                const userId = this.dataset.user_id;
+                const nonce = this.dataset.nonce;
                 const precio = this.parentElement.querySelector('.precioCount').textContent;
 
                 console.log('postId:', postId);
@@ -24,10 +26,10 @@ function stripecomprabeat() {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        postId: postId,  // Envia el ID del post
+                        postId: postId, // Envia el ID del post
                         userId: userId, // Envia el ID del usuario
                         nonce: nonce, // Envia el nonce
-                        cantidadCompra: precio  // Envía el precio como cantidad
+                        cantidadCompra: precio // Envía el precio como cantidad
                     })
                 })
                     .then(function (response) {
@@ -36,8 +38,8 @@ function stripecomprabeat() {
                     .then(function (sessionData) {
                         if (sessionData.id) {
                             // Redirigir al checkout de Stripe
-                             var stripe = Stripe('pk_live_51M9uLoCdHJpmDkrr3ZHrVnDdA7pCZ676l1k8dKpNLSiOKG8pvKYYlCI8RaHtNqYERwpZ4qwOhdrPnLW6NgsQyX8H0019HdwAY9');
-                            stripe.redirectToCheckout({ sessionId: sessionData.id }).catch(function (error) {
+                            var stripe = Stripe('pk_test_51M9uLoCdHJpmDkrrDSkwUwvKMDR9safvjDMBgICSGEbQ6NYx3QEGzG0VUpi7rOhB0crc45l9xkxI6BtgI9EUlcJ700Md8GZRwz');
+                            stripe.redirectToCheckout({sessionId: sessionData.id}).catch(function (error) {
                                 console.error('Error en redirectToCheckout:', error);
                                 alert('Hubo un problema al redirigir al checkout de Stripe.');
                             });
