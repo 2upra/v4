678706628be1ca16543a1ac1635ed812956a3560
1ndoryu <andoryyu@gmail.com>
2024-12-20 17:29:36 -0400

    *

--- functions.php
+++ functions.php
@@ -350,7 +350,7 @@ function postLog($log)
 
 function scriptsOrdenados()
 {
-    $global_version = '0.2.226';
+    $global_version = '0.2.227';
     $dev_mode = defined('LOCAL') && LOCAL;
     //$error_log = [];
 
