fa961b1f978033522649e55710c58ad0880651e8
1ndoryu <andoryyu@gmail.com>
2024-12-21 08:06:16 -0400

    *

--- functions.php
+++ functions.php
@@ -350,7 +350,7 @@ function postLog($log)
 
 function scriptsOrdenados()
 {
-    $global_version = '0.2.246';
+    $global_version = '0.2.247';
     $dev_mode = defined('LOCAL') && LOCAL;
     //$error_log = [];
 
--- js/submenu.js
+++ js/submenu.js
@@ -1,5 +1,3 @@
-//hay un pequeño detalle, cuando soy segundo click o dejo presionado sobre EDYQHV, hay un pequeño bug sucede la mitad del tiempo, el background se quita isntataneamente lo cual resulta muy molesto e incomodo, hay alguna forma de evitarlo aca, solo me ocurre un delay que bloque se oculte el background por otros eventos al momento de tocar interfieren, no se es lo que se me ocurre
-
 let submenuIdPrefixes = [];
 let openSubmenu = null;
 let longPressTimer;
@@ -8,42 +6,33 @@ let isTouchEvent = false;
 
 function createSubmenu(triggerSelector, submenuIdPrefix, position = 'auto') {
     const triggers = document.querySelectorAll(triggerSelector);
-
     registrarIdMenu(submenuIdPrefix);
     triggers.forEach(trigger => {
         if (trigger.dataset.submenuInitialized) return;
-        eventosMenu(trigger, triggerSelector, submenuIdPrefix);
+        eventosMenu(trigger, triggerSelector, submenuIdPrefix, position);
         trigger.dataset.submenuInitialized = 'true';
     });
-
     cerrarMenu(triggerSelector, submenuIdPrefix);
     resizeMovilMenu(submenuIdPrefix);
 }
 
-// Función para registrar el prefijo del ID del submenú
 function registrarIdMenu(submenuIdPrefix) {
     if (!submenuIdPrefixes.includes(submenuIdPrefix)) {
         submenuIdPrefixes.push(submenuIdPrefix);
     }
 }
 
-// Función para inicializar los eventos de cada disparador
-function eventosMenu(trigger, triggerSelector, submenuIdPrefix) {
-
-
-    // Manejar el inicio del toque o clic
+function eventosMenu(trigger, triggerSelector, submenuIdPrefix, position) {
     trigger.addEventListener('pointerdown', event => {
         if (window.innerWidth <= 640 && event.pointerType === 'touch') {
             isTouchEvent = true;
             isLongPress = false;
             longPressTimer = setTimeout(() => {
                 isLongPress = true;
-                handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix);
+                handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix, position);
             }, 500);
         }
     });
-
-    // Manejar el final del toque o clic
     trigger.addEventListener('pointerup', event => {
         if (isTouchEvent && triggerSelector === '.EDYQHV') {
             clearTimeout(longPressTimer);
@@ -54,67 +43,54 @@ function eventosMenu(trigger, triggerSelector, submenuIdPrefix) {
         } else if (isTouchEvent) {
             clearTimeout(longPressTimer);
             if (!isLongPress) {
-                handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix);
+                handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix, position);
             }
         }
         isLongPress = false;
     });
-
-    // Manejar el movimiento durante el toque
     trigger.addEventListener('pointermove', event => {
         if (isTouchEvent) {
             clearTimeout(longPressTimer);
             isLongPress = false;
         }
     });
-
-    // Manejar clics en dispositivos de escritorio
     trigger.addEventListener('click', event => {
         if (window.innerWidth > 640 && triggerSelector !== '.EDYQHV') {
-            handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix);
+            handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix, position);
         }
         if (isLongPress) {
             event.preventDefault();
             event.stopPropagation();
         }
     });
-
-    // Manejar el clic derecho en dispositivos de escritorio
     trigger.addEventListener('contextmenu', event => {
         if (window.innerWidth > 640 && triggerSelector === '.EDYQHV') {
             event.preventDefault();
-            handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix);
+            handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix, position);
         }
     });
 }
 
-// Función para manejar la alternancia del submenú
-function handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix) {
+function handleSubmenuToggle(event, trigger, triggerSelector, submenuIdPrefix, position) {
     const submenuId = getSubmenuId(trigger, triggerSelector, submenuIdPrefix);
     const submenu = document.getElementById(submenuId);
-
     if (!submenu) {
         console.error('Submenu not found:', submenuId);
         return;
     }
-
     if (openSubmenu && openSubmenu !== submenu) {
         hideSubmenu(openSubmenu);
     }
-
     submenu._position = position;
     submenu.classList.toggle('mobile-submenu', window.innerWidth <= 640);
-
     if (submenu.style.display === 'block') {
         hideSubmenu(submenu);
     } else {
-        showSubmenu(event, trigger, submenu, submenu._position);
+        showSubmenu(event, trigger, submenu, position);
     }
-
     event.stopPropagation();
 }
 
-// Función para obtener el ID del submenú
 function getSubmenuId(trigger, triggerSelector, submenuIdPrefix) {
     if (triggerSelector === '.EDYQHV') {
         return `${submenuIdPrefix}-${trigger.getAttribute('id-post')}`;
@@ -123,64 +99,48 @@ function getSubmenuId(trigger, triggerSelector, submenuIdPrefix) {
     }
 }
 
-// Función para mostrar el submenú
 function showSubmenu(event, trigger, submenu, position) {
     const {innerWidth: vw, innerHeight: vh} = window;
-
     if (submenu.parentNode !== document.body) {
         document.body.appendChild(submenu);
     }
-
     submenu.style.position = 'fixed';
     submenu.style.zIndex = 1006;
     submenu.style.display = 'block';
     submenu.style.visibility = 'hidden';
-
     let submenuWidth = submenu.offsetWidth;
     let submenuHeight = submenu.offsetHeight;
-
     const rect = trigger.getBoundingClientRect();
-
-    // Calcular la posición del submenú
     if (vw <= 640) {
         submenu.style.top = `${(vh - submenuHeight) / 2}px`;
         submenu.style.left = `${(vw - submenuWidth) / 2}px`;
     } else {
         let {top, left} = calculatePosition(rect, submenuWidth, submenuHeight, position);
-
         if (top + submenuHeight > vh) top = vh - submenuHeight;
         if (left + submenuWidth > vw) left = vw - submenuWidth;
         if (top < 0) top = 0;
         if (left < 0) left = 0;
-
         submenu.style.top = `${top}px`;
         submenu.style.left = `${left}px`;
     }
-
     submenu.style.visibility = 'visible';
-
     createSubmenuDarkBackground();
     document.body.classList.add('no-scroll');
     openSubmenu = submenu;
 }
 
-// Función para ocultar el submenú
 function hideSubmenu(submenu) {
     if (submenu) {
         submenu.style.display = 'none';
         openSubmenu = null;
     }
-
     removeSubmenuDarkBackground();
-
-    const activeSubmenus = Array.from(document.querySelectorAll(`[id^="${submenuIdPrefix}-"]`)).filter(menu => menu.style.display === 'block');
-
+    const activeSubmenus = Array.from(document.querySelectorAll(`[id^="${submenuIdPrefixes[0]}-"]`)).filter(menu => menu.style.display === 'block');
     if (activeSubmenus.length === 0) {
         document.body.classList.remove('no-scroll');
     }
 }
 
-// Función para configurar el listener de clics fuera del submenú
 function cerrarMenu(triggerSelector, submenuIdPrefix) {
     document.addEventListener('click', event => {
         document.querySelectorAll(`[id^="${submenuIdPrefix}-"]`).forEach(submenu => {
@@ -191,7 +151,6 @@ function cerrarMenu(triggerSelector, submenuIdPrefix) {
     });
 }
 
-// Función para configurar el listener de redimensionamiento de la ventana
 function resizeMovilMenu(submenuIdPrefix) {
     window.addEventListener('resize', () => {
         document.querySelectorAll(`[id^="${submenuIdPrefix}-"]`).forEach(submenu => {
@@ -200,13 +159,10 @@ function resizeMovilMenu(submenuIdPrefix) {
     });
 }
 
-// Función para ocultar todos los submenús (versión simplificada)
 window.hideAllSubmenus = function () {
     console.log('Ejecutando hideAllSubmenus (versión simplificada)');
-
     submenuIdPrefixes.forEach(prefix => {
         const allSubmenus = document.querySelectorAll(`[id^="${prefix}-"]`);
-
         if (allSubmenus.length === 0) {
             console.log(`No se encontraron submenús con el prefijo '${prefix}-'.`);
         } else {
@@ -217,9 +173,9 @@ window.hideAllSubmenus = function () {
             });
         }
     });
-
     console.log('hideAllSubmenus (versión simplificada) finalizado');
 };
+
 
 function submenu() {
     createSubmenu('.filtrosboton', 'filtrosMenu', 'abajo');
