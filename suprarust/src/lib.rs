f56183e867fbed73b284a53b82658eb981c9a270
1ndoryu <andoryyu@gmail.com>
2025-01-02 16:40:45 -0400

    *

--- suprarust/src/lib.rs
+++ suprarust/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use ext_php_rs::prelude::*;
+
+#[php_function]
+pub fn hola_mundo() -> String {
+    "¡Hola Mundo desde Rust!".to_string()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[php_module]
+pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
+    module.build()
 }
