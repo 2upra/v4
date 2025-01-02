70fbcb88f87ebca832da728d4ce816dc1435b9c5
1ndoryu <andoryyu@gmail.com>
2025-01-02 18:14:35 -0400

    *

--- TemplatePrueba.php
+++ TemplatePrueba.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /*
 Template Name: Inicio Prueba
 */
@@ -9,9 +9,8 @@ $nologin_class = !is_user_logged_in() ? ' nologin' : '';
 
 <head>
     <meta name="robots" content="noindex, nofollow">
-    <? wp_head(); ?>
+    <?php wp_head(); ?>
 </head>
-
 
 <style>
     .modal {
@@ -31,8 +30,8 @@ $nologin_class = !is_user_logged_in() ? ' nologin' : '';
 </style>
 
 <div id="main">
-    <div id="content" class="<? echo esc_attr($nologin_class); ?>">
-        <input type="hidden" id="pagina_actual" name="pagina_actual" value="<? echo esc_attr(get_the_title()); ?>">
+    <div id="content" class="<?php echo esc_attr($nologin_class); ?>">
+        <input type="hidden" id="pagina_actual" name="pagina_actual" value="<?php echo esc_attr(get_the_title()); ?>">
 
         <div id="menuData" style="display:none;" pestanaActual="">
             <div data-tab="Comentario"> </div>
@@ -43,12 +42,26 @@ $nologin_class = !is_user_logged_in() ? ' nologin' : '';
             <div class="tab-content">
 
                 <div class="tab INICIO S4K7I3" id="Comentario">
-                    
+                    <div class="bloque">
+                        <?php
+                        // Verifica si las funciones de la extensión están disponibles
+                        if (function_exists('calcular_suma') && function_exists('calcular_multiplicacion') && function_exists('operaciones_combinadas')) {
+                            // Llama a las funciones y muestra los resultados
+                            $suma = calcular_suma(5, 3);
+                            $multiplicacion = calcular_multiplicacion(4, 2);
+                            $combinadas = operaciones_combinadas(10);
+
+                            echo "<p>El resultado de la suma es: " . $suma . "</p>";
+                            echo "<p>El resultado de la multiplicación es: " . $multiplicacion . "</p>";
+                            echo "<p>" . $combinadas . "</p>";
+                        } else {
+                            echo "<p>Las funciones de la extensión suprarust no están disponibles.</p>";
+                        }
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <?
-    get_footer();
-    ?>
+    <?php get_footer(); ?>
