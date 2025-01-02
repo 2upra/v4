a4187600c5224fee5f20daa19721897e30cb3fd6
1ndoryu <andoryyu@gmail.com>
2025-01-02 16:59:57 -0400

    *

--- suprarust/src/lib.rs
+++ suprarust/src/lib.rs
@@ -1,11 +1,23 @@
 use ext_php_rs::prelude::*;
 
 #[php_function]
-pub fn hola_mundo() -> String {
-    "¡Hola Mundo desde Rust!".to_string()
+pub fn calcular_suma(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[php_function]
+pub fn calcular_multiplicacion(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[php_function]
+pub fn operaciones_combinadas(x: i32) -> String {
+    let suma = calcular_suma(x, 5);
+    let multiplicacion = calcular_multiplicacion(suma, 2);
+    format!("El resultado de la suma es: {}, y el de la multiplicación es: {}", suma, multiplicacion)
 }
 
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
-    module.build()
+    module.build().expect("Error al construir el módulo PHP")
 }
