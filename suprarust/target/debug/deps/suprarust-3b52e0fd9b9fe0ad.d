1f7237b78cea70c57dcab69e31537cd90a52724c
1ndoryu <andoryyu@gmail.com>
2025-01-02 21:40:30 +0100

    *

---
+++ suprarust/target/debug/deps/suprarust-3b52e0fd9b9fe0ad.d
@@ -0,0 +1,7 @@
+/var/www/wordpress/wp-content/themes/2upra3v/suprarust/target/debug/deps/libsuprarust-3b52e0fd9b9fe0ad.rmeta: src/lib.rs
+
+/var/www/wordpress/wp-content/themes/2upra3v/suprarust/target/debug/deps/libsuprarust-3b52e0fd9b9fe0ad.rlib: src/lib.rs
+
+/var/www/wordpress/wp-content/themes/2upra3v/suprarust/target/debug/deps/suprarust-3b52e0fd9b9fe0ad.d: src/lib.rs
+
+src/lib.rs:
