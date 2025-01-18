9153979b59563bb3e0293f6443e70b3845449f62
1ndoryu <andoryyu@gmail.com>
2025-01-17 20:38:16 -0400

    *

--- app/Pages/asleyTabs.php
+++ app/Pages/asleyTabs.php
@@ -31,11 +31,45 @@ function portafolio()
     ob_start();
 
 ?>
+    <script>
+        if ('IntersectionObserver' in window) {
+            const observer = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        const div = entry.target;
+                        const src = div.getAttribute('data-src');
+                        if (src) {
+                            // Usa fetch para cargar el contenido del SVG
+                            fetch(src)
+                                .then(response => {
+                                    if (!response.ok) {
+                                        throw new Error('Error al cargar el SVG');
+                                    }
+                                    return response.text();
+                                })
+                                .then(svg => {
+                                    div.innerHTML = svg; // Inserta el SVG en el div
+                                    div.removeAttribute('data-src'); // Limpia el data-src
+                                })
+                                .catch(err => {});
 
+                            observer.unobserve(div); // Deja de observar el elemento
+                        } else {}
+                    }
+                });
+            });
+
+            // Seleccionamos todos los elementos con la clase 'lazy-svg'
+            const lazySvgs = document.querySelectorAll('.lazy-svg');
+
+            lazySvgs.forEach(div => {
+                observer.observe(div);
+            });
+        } else {}
+    </script>
 
     <div class="SOKDEOD" id="inicioDiv">
-        <h2>soy <img src="<? echo get_template_directory_uri(); ?>/assets/img/asleywandorius1.jpg" alt=""><span id="asley">Asley </span> developer </h2>
-        <h2></h2>
+        <h2>I'M ASLEY DEVELOPER</h2>
         <h2 id="flst">Full Stack</h2>
         <div class="LXCJWW">
             <button class="borde">Descargar CV</button>
@@ -62,77 +96,47 @@ function portafolio()
         </div>
         <div class="OSFED" id="caracteristicas">
             <div class="ADEEDE">
-                <div class="div1 bloque svg-container">
-                    <div class="tinfo" style="display: none;">
-                        <p class="ttpri" style="display: none;">test</p>
-                        <p class="ttsec">2UPRA nace debido a la falta de centralización e innovación en apps de producción musical. Las herramientas actuales están dispersas y no optimizadas. Por eso nuestro enfoque es ofrecer una experiencia única en todas las etapas de producción musical.</p>
-                    </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-1.svg"></div>
+                <div class="diva1 bloque svg-container">
+
+                    <h3>BIO</h3>
+
+                    <p>Web developer highly skilled in complex algorithm development, efficient architecture design, and database management within Linux environments.</p>
+
+                    <p>Disciplined and focused on project planning and execution. Proven ability to create interactive web applications, from initial design to final implementation. Seeking a challenging role where I can contribute with innovative and high-impact solutions.</p>
+
                 </div>
-                <div class="div2 bloque svg-container">
+                <div class="diva2 bloque svg-container">
                     <div class="tinfo" style="display: none;">
                         <p class="ttpri" style="display: none;">test</p>
                         <p class="ttsec">La principal virtud del proyecto es el uso de inteligencia artificial y machine learning para reconocimiento de patrones, optimización, mejora de algoritmos y supervisión. 2UPRA aprende de los usuarios para mejorar continuamente.</p>
                     </div>
-                    <img src="https://2upra.com/wp-content/uploads/2024/11/Recurso-4@2x-1.png" alt="">
+                    
+                    <div class="lazy-svg" data-src="<?php echo get_template_directory_uri(); ?>/assets/svgs/logos.svg"></div>
                 </div>
-                <div class="div3 bloque svg-container">
+
+                <div class="diva3 bloque svg-container">
                     <div class="tinfo" style="display: none;">
                         <p class="ttpri" style="display: none;"></p>
                         <p class="ttsec">Enfoque social: plataforma optimizada para la comunicación entre productores, artistas y fans, ofreciendo una experiencia única que facilita los procesos creativos.</p>
                     </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-3.svg"></div>
+
                 </div>
-                <div class="div4 bloque svg-container">
-                    <div class="tinfo" style="display: none;">
-                        <p class="ttpri" style="display: none;"></p>
-                        <p class="ttsec">Sync 2UPRA: herramienta única que facilita la organización, recolección y uso de recursos. Olvídate de dónde guardar tus samples uno por uno, se sincronizarán y organizarán automáticamente de forma eficaz para que puedas acceder a ellos fácilmente cuando los necesites en tu pc.</p>
-                    </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-4.svg"></div>
-                </div>
-                <div class="div5 bloque svg-container">
-                    <div class="tinfo" style="display: none;">
-                        <p class="ttpri" style="display: none;"></p>
-                        <p class="ttsec">Colecciones: organiza tus recursos musicales en colecciones personalizables y compártelas con la comunidad o de forma privada. Descubre nuevas colecciones y amplía tus horizontes musicales.</p>
-                    </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-5.svg"></div>
-                </div>
-                <div class="div6 bloque svg-container">
-                    <div class="tinfo" style="display: none;">
-                        <p class="ttpri" style="display: none;"></p>
-                        <p class="ttsec">Algoritmos inteligentes de recomendación: 2UPRA entiende tus gustos, te ayuda a organizar ideas y recomienda recursos apropiados para ti y tus colecciones.</p>
-                    </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-6.svg"></div>
-                </div>
-                <div class="div7 bloque svg-container">
-                    <div class="tinfo" style="display: none;">
-                        <p class="ttpri" style="display: none;"></p>
-                        <p class="ttsec">Mantente conectado como en cualquier red social, pero impulsando tu crecimiento artístico. Comparte tus creaciones, colabora con otros artistas, descubre oportunidades y sigue la trayectoria de tus ídolos.</p>
-                    </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-7.svg"></div>
-                </div>
-                <div class="div8 bloque svg-container">
-                    <div class="tinfo" style="display: none;">
-                        <p class="ttpri" style="display: none;"></p>
-                        <p class="ttsec">Múltiples fuentes de ingresos: en 2UPRA usamos algoritmos inteligentes para compensar tu esfuerzo. Invita a tus fans a suscribirse o publica tus servicios, tu crecimiento artístico y tu bienestar van de la mano.</p>
-                    </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-8.svg"></div>
-                </div>
-                <div class="div9 bloque svg-container">
-                    <div class="tinfo" style="display: none;">
-                        <p class="ttpri" style="display: none;"></p>
-                        <p class="ttsec">Exprésate de muchas formas: en 2UPRA comprendemos que el arte tiene múltiples expresiones. Comunica tus ideas, emociones y proyectos como desees, ya sea a través de música, videos, imágenes o texto, y acércate a otros artistas y fans de maneras diversas.</p>
-                    </div>
-                    <div class="lazy-svg" data-src="https://2upra.com/wp-content/themes/2upra3v/assets/svgs/div-9.svg"></div>
-                </div>
-                <div class="div10 bloquesvg-container" style="display: none">
-                </div>
+
             </div>
         </div>
 
         <style>
-            /* Contenedor principal */
-            /* Posicionamiento personalizado de los bloques */
+            .diva1 {
+                grid-area: 1 / 1 / 2 / 2;
+            }
+
+            .diva2 {
+                grid-area: 1 / 2 / 2 / 3;
+            }
+
+            .diva3 {
+                grid-area: 1 / 3 / 2 / 4;
+            }
         </style>
 
 
