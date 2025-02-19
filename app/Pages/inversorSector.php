363c4583fcc8270d0c729542121043d26d33fc14
1ndoryu <andoryyu@gmail.com>
2025-02-18 20:38:44 -0400

    *

--- app/Pages/inversorSector.php
+++ app/Pages/inversorSector.php
@@ -97,7 +97,7 @@ function inversorSector()
 
                     <div class="RLSDSAE">
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/img/pd1.jpg" alt="pd1">
-                        <h3>Pintura digital de personajes o escenarios</h3>
+                        <h3>Pintura digital de personajes o escenarios, estilo de pintura/anime</h3>
                         <button>Desde 5$</button>
                         <p>30$</p>
                     </div>
