9cf9aa3f0d01d2448cfc1463b7e48d344e707376
1ndoryu <andoryyu@gmail.com>
2025-02-23 18:07:48 -0400

    *

--- functions.php
+++ functions.php
@@ -378,7 +378,7 @@ function postLog($log)
 
 function scriptsOrdenados()
 {
-    $global_version = '0.2.360';
+    $global_version = '0.2.361';
     $dev_mode = defined('LOCAL') && LOCAL;
     //$error_log = [];
 
