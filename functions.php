89a27315f869ae8603dafa5a77d147de36317750
1ndoryu <andoryyu@gmail.com>
2025-03-29 11:47:08 +0100

    Refactor: Mueve funcion obtenerIdiomaDelNavegador a functions.php para reutilización.

--- functions.php
+++ functions.php
@@ -698,3 +698,18 @@ EOD;
     wp_add_inline_script('script-base', $script_inline);
 }
 add_action('wp_enqueue_scripts', 'innerHeight');
+
+// Función para obtener el idioma preferido del navegador
+function obtenerIdiomaDelNavegador() {
+    if (!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+        return 'en';
+    }
+    $accepted_languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+    foreach ($accepted_languages as $language) {
+        $lang = substr($language, 0, 2);
+        if (in_array($lang, ['es', 'en'])) {
+            return $lang;
+        }
+    }
+    return 'en';
+}
