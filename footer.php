d7f7942f5235adbedcc84c42b15bb13a3dd46cb6
1ndoryu <andoryyu@gmail.com>
2025-03-29 22:01:26 +0100

    Refactor: Elimina definición de filtro SVG pixelate no utilizado de footer.php.

--- footer.php
+++ footer.php
@@ -20,15 +20,7 @@ if (!defined('ABSPATH')) {
 
 <? get_template_part('templates/to-top'); ?>
 </body>
-<svg style="display:none;">
-  <filter id="pixelate" x="0" y="0">
-    <feFlood x="4" y="4" height="2" width="2"/>
-    <feComposite width="10" height="10"/>
-    <feTile result="a"/>
-    <feComposite in="SourceGraphic" in2="a" operator="in"/>
-    <feMorphology operator="dilate" radius="5"/>
-  </filter>
-</svg>
+
 <script>
 window.addEventListener('load', function () {
     document.body.classList.add('loaded');
