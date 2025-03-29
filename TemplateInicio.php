2263f4b261cb818c3c3b1eecf39dbec3883a4da8
1ndoryu <andoryyu@gmail.com>
2025-03-29 21:45:22 +0100

    Refactor: Mueve código común de templates a functions.php para reutilización.

--- TemplateInicio.php
+++ TemplateInicio.php
@@ -11,12 +11,7 @@ Template Name: Inicio
     <? wp_head(); ?>
 </head>
 
-<?
-get_header();
-$user_id = get_current_user_id();
-$acciones = get_user_meta($user_id, 'acciones', true);
-$nologin_class = !is_user_logged_in() ? ' nologin' : '';
-?>
+
 
 <div id="main">
     <div id="content" class="<? echo esc_attr($nologin_class); ?>">
