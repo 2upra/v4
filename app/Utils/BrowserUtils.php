2c974e216290de1f3c07170ecce9d804d7e8eb52
1ndoryu <andoryyu@gmail.com>
2025-03-31 16:38:10 +0200

    Refactor(Org): Mueve función innerHeight de functions.php a app/Utils/BrowserUtils.php

--- app/Utils/BrowserUtils.php
+++ app/Utils/BrowserUtils.php
@@ -29,3 +29,34 @@ function obtenerIdiomaDelNavegador()
 
     return 'en'; // Retorna inglés si ninguno de los idiomas preferidos ('es', 'en') se encuentra
 }
+
+//CALCULAR ALTURA CORRECTA CON SCRIPT
+function innerHeight()
+{
+    wp_register_script('script-base', '');
+    wp_enqueue_script('script-base');
+    $script_inline = <<<'EOD'
+    function setVHVariable() {
+        var vh;
+        if (window.visualViewport) {
+            vh = window.visualViewport.height * 0.01;
+        } else {
+            vh = window.innerHeight * 0.01;
+        }
+        document.documentElement.style.setProperty('--vh', vh + 'px');
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        setVHVariable();
+
+        if (window.visualViewport) {
+            window.visualViewport.addEventListener('resize', setVHVariable);
+        } else {
+            window.addEventListener('resize', setVHVariable);
+        }
+    });
+EOD;
+    wp_add_inline_script('script-base', $script_inline);
+}
+// La llamada add_action debe usar el namespace completo
+add_action('wp_enqueue_scripts', 'App\\Utils\\innerHeight');
