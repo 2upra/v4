0a0a7a2f333f373d283d2dedebe9b0bf9abf2ed0
1ndoryu <andoryyu@gmail.com>
2025-03-31 21:24:51 +0200

    Refactor(Clean): Reemplaza lógica de metadatos duplicada en TemplateColec.php con llamada a configurarMetadatosPaginaIdioma()

--- TemplateColec.php
+++ TemplateColec.php
@@ -3,27 +3,7 @@
 Template Name: Colecc
 */
 
-// Detectar idioma y definir titulo y descripcion
-$idioma = obtenerIdiomaDelNavegador();
-if ($idioma === 'es') {
-    $titulo = "Social Media para Artistas | Samples y VST Gratis";
-    $descripcion = "Unete a una red de creadores donde puedes conectar con artistas, colaborar en proyectos, y encontrar una amplia variedad de samples y plugins VST gratuitos para potenciar tus producciones musicales.";
-} else {
-    $titulo = "Social Media for Artists | Free Samples & VSTs";
-    $descripcion = "Join a network of creators where you can connect with artists, collaborate on projects, and access a wide range of free samples and VST plugins to enhance your music productions.";
-}
-
-// Anadir el titulo y la descripcion al <head>
-add_action('wp_head', function () use ($titulo, $descripcion) {
-    echo '<title>' . esc_html($titulo) . '</title>' . "
-";
-    echo '<meta name="description" content="' . esc_attr($descripcion) . '">' . "
-";
-}, 1); // Prioridad baja para que se ejecute temprano en wp_head
-
-// Configurar cookies para el titulo y descripcion
-setcookie("page_title", $titulo, time() + 3600, "/");
-setcookie("page_description", $descripcion, time() + 3600, "/");
+configurarMetadatosPaginaIdioma();
 ?>
 
 <head>
