86e58e6646fcca255081ee836c56db93a30115ef
1ndoryu <andoryyu@gmail.com>
2025-04-01 12:01:31 +0200

    Refactor(Org): Mueve función innerHeight de app/Utils/BrowserUtils.php a app/Setup/ThemeSetup.php

--- app/Setup/ThemeSetup.php
+++ app/Setup/ThemeSetup.php
@@ -253,4 +253,35 @@ function custom_site_icon($meta_tags)
 }
 add_filter('site_icon_meta_tags', 'custom_site_icon');
 
+//CALCULAR ALTURA CORRECTA CON SCRIPT
+function innerHeight()
+{
+    wp_register_script('script-base', '');
+    wp_enqueue_script('script-base');
+    $script_inline = <<<'EOD'
+    function setVHVariable() {
+        var vh;
+        if (window.visualViewport) {
+            vh = window.visualViewport.height * 0.01;
+        } else {
+            vh = window.innerHeight * 0.01;
+        }
+        document.documentElement.style.setProperty('--vh', vh + 'px');
+    }
+
+    document.addEventListener('DOMContentLoaded', function() {
+        setVHVariable();
+
+        if (window.visualViewport) {
+            window.visualViewport.addEventListener('resize', setVHVariable);
+        } else {
+            window.addEventListener('resize', setVHVariable);
+        }
+    });
+EOD;
+    wp_add_inline_script('script-base', $script_inline);
+}
+
+add_action('wp_enqueue_scripts', 'innerHeight');
+
 ?>
--- app/Utils/BrowserUtils.php
+++ app/Utils/BrowserUtils.php
@@ -1,32 +1,3 @@
 <?php
 
-//CALCULAR ALTURA CORRECTA CON SCRIPT
-function innerHeight()
-{
-    wp_register_script('script-base', '');
-    wp_enqueue_script('script-base');
-    $script_inline = <<<'EOD'
-    function setVHVariable() {
-        var vh;
-        if (window.visualViewport) {
-            vh = window.visualViewport.height * 0.01;
-        } else {
-            vh = window.innerHeight * 0.01;
-        }
-        document.documentElement.style.setProperty('--vh', vh + 'px');
-    }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        setVHVariable();
-
-        if (window.visualViewport) {
-            window.visualViewport.addEventListener('resize', setVHVariable);
-        } else {
-            window.addEventListener('resize', setVHVariable);
-        }
-    });
-EOD;
-    wp_add_inline_script('script-base', $script_inline);
-}
-
-add_action('wp_enqueue_scripts', 'innerHeight');
