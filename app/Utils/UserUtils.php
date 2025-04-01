f069d3b6603644c05b78b05272d1f8d0e2ff7f1c
1ndoryu <andoryyu@gmail.com>
2025-04-01 21:01:28 +0200

    Refactor(Org): Mueve función cambiar_tipo_usuario_callback y sus hooks AJAX de app/Functions/cambiarTipoUser.php a app/Utils/UserUtils.php

--- app/Utils/UserUtils.php
+++ app/Utils/UserUtils.php
@@ -265,3 +265,22 @@ add_action('restablecer_pinkys_semanal', 'restablecerPinkys');
 if (!wp_next_scheduled('restablecer_pinkys_semanal')) {
     wp_schedule_event(time(), 'weekly', 'restablecer_pinkys_semanal');
 }
+
+// Funciones movidas desde app/Functions/cambiarTipoUser.php
+
+function cambiar_tipo_usuario_callback()
+{
+    $user_id = get_current_user_id();
+    $tipo = $_POST['tipo'];
+
+    if ($tipo === 'fan') {
+        $estado_actual = get_user_meta($user_id, 'fan', true);
+        update_user_meta($user_id, 'fan', !$estado_actual);
+    }
+
+    echo !$estado_actual;
+    wp_die();
+}
+
+add_action('wp_ajax_cambiar_tipo_usuario', 'cambiar_tipo_usuario_callback');
+add_action('wp_ajax_nopriv_cambiar_tipo_usuario', 'cambiar_tipo_usuario_callback');
