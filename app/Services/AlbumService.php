ddbeb5cf3bca59b512fda5286c1156a3e6f92074
1ndoryu <andoryyu@gmail.com>
2025-04-03 09:06:41 -0400

    *

--- app/Services/AlbumService.php
+++ app/Services/AlbumService.php
@@ -1,106 +1,99 @@
 <?php
 
-function process_album_post($post_id)
+# Procesa un post de tipo álbum, creando un nuevo post de álbum y posts de rolas asociados.
+function process_album_post($idPost)
 {
-
-    $is_rola = get_post_meta($post_id, 'rola', true);
-    if ($is_rola == '1') {
-        return; 
+    $esRola = get_post_meta($idPost, 'rola', true);
+    if ($esRola == '1') {
+        return;
     }
 
-    $is_album = get_post_meta($post_id, 'albumRolas', true);
-    if ($is_album != '1') {
-        return; 
+    $esAlbum = get_post_meta($idPost, 'albumRolas', true);
+    if ($esAlbum != '1') {
+        return;
     }
 
-    $original_post = get_post($post_id);
-    $album_title = $original_post->post_title;
-    $album_content = $original_post->post_content;
-    $album_author = $original_post->post_author;
-    $album_thumbnail_id = get_post_thumbnail_id($post_id);
-    $album_post_id = wp_insert_post(array(
+    $postOriginal = get_post($idPost);
+    $tituloAlbum = $postOriginal->post_title;
+    $contenidoAlbum = $postOriginal->post_content;
+    $autorAlbum = $postOriginal->post_author;
+    $idMiniaturaAlbum = get_post_thumbnail_id($idPost);
+
+    $idPostAlbum = wp_insert_post(array(
         'post_type' => 'albums',
-        'post_title' => $album_title,
-        'post_content' => $album_content,
-        'post_author' => $album_author,
+        'post_title' => $tituloAlbum,
+        'post_content' => $contenidoAlbum,
+        'post_author' => $autorAlbum,
         'post_status' => 'publish',
     ));
 
-    $meta_keys_to_copy = array('_post_puntuacion_final', 'paraDescarga', 'esExclusivo', 'paraColab', 'real_name', 'artistic_name', 'email', 'public', 'genre_tags', 'instrument_tags');
-    foreach ($meta_keys_to_copy as $key) {
-        $value = get_post_meta($post_id, $key, true);
-        if ($value) {
-            update_post_meta($album_post_id, $key, $value);
+    $clavesMetaCopiar = array('_post_puntuacion_final', 'paraDescarga', 'esExclusivo', 'paraColab', 'real_name', 'artistic_name', 'email', 'public', 'genre_tags', 'instrument_tags');
+    foreach ($clavesMetaCopiar as $clave) {
+        $valor = get_post_meta($idPost, $clave, true);
+        if ($valor) {
+            update_post_meta($idPostAlbum, $clave, $valor);
         }
     }
 
-
-    if ($album_thumbnail_id) {
-        set_post_thumbnail($album_post_id, $album_thumbnail_id);
+    if ($idMiniaturaAlbum) {
+        set_post_thumbnail($idPostAlbum, $idMiniaturaAlbum);
     }
 
-    $rolas_meta = get_post_meta($post_id, 'rolas_meta_key', true);
-    $rola_names = maybe_unserialize($rolas_meta);
+    $rolasMeta = get_post_meta($idPost, 'rolas_meta_key', true);
+    $nombresRola = maybe_unserialize($rolasMeta);
 
-    if (empty($rola_names)) {
+    if (empty($nombresRola)) {
         return;
     }
-    $artistic_name = get_post_meta($post_id, 'artistic_name', true);
-    $real_name = get_post_meta($post_id, 'real_name', true);
+    $nombreArtistico = get_post_meta($idPost, 'artistic_name', true);
+    $nombreReal = get_post_meta($idPost, 'real_name', true);
 
-    $rola_posts = [];
+    $postsRola = [];
 
-    for ($i = 0; $i < count($rola_names); $i++) {
-        $rola_title = $rola_names[$i];
-        guardarLog("Processing rola " . ($i + 1) . ": $rola_title");
+    for ($i = 0; $i < count($nombresRola); $i++) {
+        $tituloRola = $nombresRola[$i];
 
-        // Agregar logs adicionales aquí
-        guardarLog("Obteniendo metadatos para rola $i");
-        $audio_id = get_post_meta($post_id, "post_audio" . ($i + 1), true);
-        $audio_lite_id = get_post_meta($post_id, "post_audio_lite_" . ($i + 1), true);
-        $audio_hd_id = get_post_meta($post_id, "post_audio_hd_" . ($i + 1), true);
-        $waveform_image = get_post_meta($post_id, "audio_waveform_image_" . ($i + 1), true);
-        $duration = get_post_meta($post_id, "audio_duration_" . ($i + 1), true);
+        $idAudio = get_post_meta($idPost, "post_audio" . ($i + 1), true);
+        $idAudioLite = get_post_meta($idPost, "post_audio_lite_" . ($i + 1), true);
+        $idAudioHd = get_post_meta($idPost, "post_audio_hd_" . ($i + 1), true);
+        $imagenOnda = get_post_meta($idPost, "audio_waveform_image_" . ($i + 1), true);
+        $duracion = get_post_meta($idPost, "audio_duration_" . ($i + 1), true);
 
-        if (!$audio_id || !$rola_title) {
-            guardarLog("Faltan datos críticos para la rola " . ($i + 1) . ": $rola_title. Continuando con la siguiente rola.");
+        if (!$idAudio || !$tituloRola) {
             continue;
         }
 
-        $rola_post_id = wp_insert_post(array(
+        $idPostRola = wp_insert_post(array(
             'post_type' => 'social_post',
-            'post_title' => $rola_title,
-            'post_content' => $rola_title,
-            'post_author' => $album_author,
+            'post_title' => $tituloRola,
+            'post_content' => $tituloRola,
+            'post_author' => $autorAlbum,
             'post_status' => 'publish',
         ));
 
-        update_post_meta($rola_post_id, 'post_audio', $audio_id);
-        update_post_meta($rola_post_id, 'album_id', $album_post_id);
-        update_post_meta($rola_post_id, 'real_name', $real_name);
-        update_post_meta($rola_post_id, 'post_audio_lite', $audio_lite_id);
-        update_post_meta($rola_post_id, 'post_audio_hd', $audio_hd_id);
-        update_post_meta($rola_post_id, 'audio_waveform_image', $waveform_image);
-        update_post_meta($rola_post_id, 'audio_duration', $duration);
-        update_post_meta($rola_post_id, 'rola', true);
-        update_post_meta($rola_post_id, 'artistic_name', $artistic_name);
-        update_post_meta($rola_post_id, '_post_puntuacion_final', 100);
+        update_post_meta($idPostRola, 'post_audio', $idAudio);
+        update_post_meta($idPostRola, 'album_id', $idPostAlbum);
+        update_post_meta($idPostRola, 'real_name', $nombreReal);
+        update_post_meta($idPostRola, 'post_audio_lite', $idAudioLite);
+        update_post_meta($idPostRola, 'post_audio_hd', $idAudioHd);
+        update_post_meta($idPostRola, 'audio_waveform_image', $imagenOnda);
+        update_post_meta($idPostRola, 'audio_duration', $duracion);
+        update_post_meta($idPostRola, 'rola', true);
+        update_post_meta($idPostRola, 'artistic_name', $nombreArtistico);
+        update_post_meta($idPostRola, '_post_puntuacion_final', 100);
 
-        $additional_search_data = array(
+        $datosAdicionalesBusqueda = array(
             'rola' => true,
-            'artistic_name' => $artistic_name,
-            'titulo' => $rola_title
+            'artistic_name' => $nombreArtistico,
+            'titulo' => $tituloRola
         );
-        update_post_meta($rola_post_id, 'additional_search_data', json_encode($additional_search_data));
+        update_post_meta($idPostRola, 'additional_search_data', json_encode($datosAdicionalesBusqueda));
 
-        if ($album_thumbnail_id) {
-            set_post_thumbnail($rola_post_id, $album_thumbnail_id);
+        if ($idMiniaturaAlbum) {
+            set_post_thumbnail($idPostRola, $idMiniaturaAlbum);
         }
-        guardarLog("Rola creada: ID = $rola_post_id, Título = $rola_title");
-        $rola_posts[] = $rola_post_id;
+        $postsRola[] = $idPostRola;
     }
 
-   
-    update_post_meta($album_post_id, 'album_rolas', $rola_posts);
-    guardarLog("Valor final de rola_posts: " . print_r($rola_posts, true));
+    update_post_meta($idPostAlbum, 'album_rolas', $postsRola);
 }
