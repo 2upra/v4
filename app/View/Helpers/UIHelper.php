53725c7c8b362c559e092e1c04f365f6b4d3d206
1ndoryu <andoryyu@gmail.com>
2025-04-03 11:03:35 +0200

    Refactor(Org): Mueve función botonColeccion() de app/Content/Colecciones/Logic/logicColecciones.php a app/View/Helpers/UIHelper.php

--- app/View/Helpers/UIHelper.php
+++ app/View/Helpers/UIHelper.php
@@ -748,3 +748,28 @@ function botonColab($postId, $colab)
 {
     return $colab ? "<div class='XFFPOX'><button class='ZYSVVV' data-post-id='$postId'>{$GLOBALS['iconocolab']}</button></div>" : '';
 }
+
+// Refactor(Org): Funcion botonColeccion() movida desde app/Content/Colecciones/Logic/logicColecciones.php
+function botonColeccion($postId)
+{
+
+    $extraClass = '';
+    if (is_user_logged_in()) {
+        $userId = get_current_user_id();
+        $coleccion = get_user_meta($userId, 'samplesGuardados', true);
+        if (is_array($coleccion) && isset($coleccion[$postId])) {
+            $extraClass = ' colabGuardado';
+        }
+    }
+
+    ob_start();
+?>
+    <div class="ZAQIBB botonColeccion<? echo esc_attr($extraClass); ?>">
+        <button class="botonColeccionBtn" aria-label="Guardar sonido" data-post_id="<? echo esc_attr($postId); ?>" data-nonce="<? echo wp_create_nonce('colec_nonce'); ?>">
+            <? echo isset($GLOBALS['iconoGuardar']) ? $GLOBALS['iconoGuardar'] : ''; // Verifica si $GLOBALS['iconoGuardar'] está definida 
+            ?>
+        </button>
+    </div>
+<?
+    return ob_get_clean();
+}
