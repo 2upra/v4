27d803a06ae04362799f4b15309fb70214ea730d
1ndoryu <andoryyu@gmail.com>
2025-04-04 01:31:34 +0200

    Refactor(Org): Mueve función de utilidad maybe_unserialize_dos() de renderPostColec.php a ArrayUtils.php

--- app/Content/Colecciones/View/renderPostColec.php
+++ app/Content/Colecciones/View/renderPostColec.php
@@ -252,34 +252,7 @@ function datosColeccion($postId)
     }
 }
 
-function maybe_unserialize_dos($data)
-{
-    if (empty($data)) {
-        return $data;
-    }
-
-    // Si el dato ya es un array, devolverlo tal cual
-    if (is_array($data)) {
-        return $data;
-    }
-
-    // Intentar decodificar JSON si es un string
-    if (is_string($data)) {
-        $json = json_decode($data, true);
-        if (json_last_error() === JSON_ERROR_NONE) {
-            return $json;
-        }
-    }
-
-    // Intentar deserializar si es un string
-    $unserialized = @unserialize($data);
-    if ($unserialized !== false || $data === 'b:0;') {
-        return $unserialized;
-    }
-
-    // Devolver el original si no se pudo deserializar ni decodificar
-    return $data;
-}
+// Funcion maybe_unserialize_dos movida a app/Utils/ArrayUtils.php
 
 
 
--- app/Utils/ArrayUtils.php
+++ app/Utils/ArrayUtils.php
@@ -1,5 +1,5 @@
 <?php
-// Funcion movida desde app/Content/Colecciones/View/renderPostColec.php
+// Funcion aplanarArray movida desde app/Content/Colecciones/View/renderPostColec.php
 
 /**
  * Aplana un array multidimensional en un array de una sola dimensión.
@@ -22,4 +22,34 @@ function aplanarArray($input)
         $result[] = $input;
     }
     return $result;
+}
+
+// Funcion movida desde app/Content/Colecciones/View/renderPostColec.php
+function maybe_unserialize_dos($data)
+{
+    if (empty($data)) {
+        return $data;
+    }
+
+    // Si el dato ya es un array, devolverlo tal cual
+    if (is_array($data)) {
+        return $data;
+    }
+
+    // Intentar decodificar JSON si es un string
+    if (is_string($data)) {
+        $json = json_decode($data, true);
+        if (json_last_error() === JSON_ERROR_NONE) {
+            return $json;
+        }
+    }
+
+    // Intentar deserializar si es un string
+    $unserialized = @unserialize($data);
+    if ($unserialized !== false || $data === 'b:0;') {
+        return $unserialized;
+    }
+
+    // Devolver el original si no se pudo deserializar ni decodificar
+    return $data;
 }
