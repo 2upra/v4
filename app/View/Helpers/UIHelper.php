53d7c2354358bd555c071137385f6bd37b9b942d
1ndoryu <andoryyu@gmail.com>
2025-04-03 23:02:11 +0200

    Refactor(Org): Mueve función botonColeccion() de UIHelper a CollectionHelper

--- app/View/Helpers/UIHelper.php
+++ app/View/Helpers/UIHelper.php
@@ -709,30 +709,7 @@ function formRs()
 
 // Refactor(Exec): Mover función botonColab() a ColabHelper.php
 
-// Refactor(Org): Funcion botonColeccion() movida desde app/Content/Colecciones/Logic/logicColecciones.php
-function botonColeccion($postId)
-{
-
-    $extraClass = '';
-    if (is_user_logged_in()) {
-        $userId = get_current_user_id();
-        $coleccion = get_user_meta($userId, 'samplesGuardados', true);
-        if (is_array($coleccion) && isset($coleccion[$postId])) {
-            $extraClass = ' colabGuardado';
-        }
-    }
-
-    ob_start();
-?>
-    <div class="ZAQIBB botonColeccion<? echo esc_attr($extraClass); ?>">
-        <button class="botonColeccionBtn" aria-label="Guardar sonido" data-post_id="<? echo esc_attr($postId); ?>" data-nonce="<? echo wp_create_nonce('colec_nonce'); ?>">
-            <? echo isset($GLOBALS['iconoGuardar']) ? $GLOBALS['iconoGuardar'] : ''; // Verifica si $GLOBALS['iconoGuardar'] está definida 
-            ?>
-        </button>
-    </div>
-<?
-    return ob_get_clean();
-}
+// Refactor(Org): Funcion botonColeccion() movida a app/View/Helpers/CollectionHelper.php
 
 // Refactor(Org): Mueve función botonSincronizar() desde app/Functions/descargas.php
 function botonSincronizar($postId)
