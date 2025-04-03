17986ccc36fcb560653ffae006ba3c15aca2d563
1ndoryu <andoryyu@gmail.com>
2025-04-03 11:50:08 +0200

    Refactor(Org): Mueve función colabTabs() de app/Pages/colabTabss.php a app/View/Components/ColabTabs.php

---
+++ app/View/Components/ColabTabs.php
@@ -0,0 +1,28 @@
+<?php
+// Función colabTabs() movida desde app/Pages/colabTabss.php
+
+function colabTabs()
+{
+    ob_start();
+
+?>
+    <div id="menuData" style="display:none;" pestanaActual="">
+        <div data-tab="Colabs"></div>
+    </div>
+    
+    <div class="tabs">
+        <div class="tab-content">
+
+            <div class="tab INICIO S4K7I3" id="Colabs">
+                <div class="GSDKRA">
+                    <? echo colab(); ?>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+<?
+
+    return ob_get_clean();
+}
