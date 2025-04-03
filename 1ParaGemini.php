7d2e772ff357da664d76287637097d6b25b8aeb7
1ndoryu <andoryyu@gmail.com>
2025-04-03 08:16:12 -0400

    *

--- 1ParaGemini.php
+++ 1ParaGemini.php
@@ -1,9 +1,14 @@
 <?php
+
 /*
+
+No elimines este archivo.  
+
 Este archivo ha sido creado por el usuario humano que maneja de este proyecto para gemini IA o cualquier IA que refactorice o ayude a mejorar el codigo del proyecto. Aquí se agregarán evaluaciones acerca del trabajo realizado por la IA, asi que presta atención para mejorar lo que haces. Ten encuenta que esto es atemporal es decir, pueden haber problemas que ya solucionaste pero intentare mantener actualizado esto.
 
 
 3 de Abril de 2025 
 
 1) Hay algunos archivos que son muy extensos que tu mismo creaste, por ejemplo UIHelper.php es muy extenso, y contiene funciones php que deberían estar ordenadas en carpetas especificas. Algo similar paso en postService pero ya lo estas arreglando. 
+
 */
