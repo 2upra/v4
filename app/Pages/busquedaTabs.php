9eb0fe20f0727f400243eb89ae0eb9daab66b468
1ndoryu <andoryyu@gmail.com>
2025-04-03 11:47:31 +0200

    Revert "Refactor(Clean): Elimina archivo vacío app/Pages/busquedaTabs.php"
    This reverts commit d5088189621330fc44517727ffdfd2b4240cc440.

---
+++ app/Pages/busquedaTabs.php
@@ -0,0 +1,27 @@
+<?
+
+function busquedaTabs()
+{
+    ob_start();
+
+?>
+    <div id="menuData" style="display:none;" pestanaActual="">
+        <div data-tab="Busquedatab"></div>
+    </div>
+    
+    <div class="tabs">
+        <div class="tab-content">
+
+            <div class="tab INICIO S4K7I3" id="Busquedatab">
+                <div class="GSDKRA DSOE4LS">
+                    <? echo busqueda(); ?>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+<?
+
+    return ob_get_clean();
+}
