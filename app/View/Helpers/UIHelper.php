2d6db4bdc034a535ed419a590fa1275cfd33f427
1ndoryu <andoryyu@gmail.com>
2025-04-03 11:26:51 +0200

    Refactor(Org): Mueve función botonSincronizar() de app/Functions/descargas.php a app/View/Helpers/UIHelper.php

--- app/View/Helpers/UIHelper.php
+++ app/View/Helpers/UIHelper.php
@@ -773,3 +773,42 @@ function botonColeccion($postId)
 <?
     return ob_get_clean();
 }
+
+// Refactor(Org): Mueve función botonSincronizar() desde app/Functions/descargas.php
+function botonSincronizar($postId)
+{
+    ob_start();
+    $paraDescarga = get_post_meta($postId, 'paraDescarga', true);
+    $userId = get_current_user_id();
+
+    if ($paraDescarga == '1') {
+        if ($userId) {
+            $descargasAnteriores = get_user_meta($userId, 'descargas', true);
+            $yaDescargado = isset($descargasAnteriores[$postId]);
+            $claseExtra = $yaDescargado ? 'yaDescargado' : '';
+            $esColeccion = get_post_type($postId) === 'colecciones' ? 'true' : 'false';
+
+
+?>
+            <div class="ZAQIBB">
+                <button class="icon-arrow-down <?php echo esc_attr($claseExtra); ?>"
+                    data-post-id="<?php echo esc_attr($postId); ?>"
+                    aria-label="Boton Descarga"
+                    id="download-button-<?php echo esc_attr($postId); ?>"
+                    onclick="return procesarDescarga('<?php echo esc_js($postId); ?>', '<?php echo esc_js($userId); ?>', '<?php echo $esColeccion; ?>')">
+                    <?php echo $GLOBALS['descargaicono']; ?>
+                </button>
+            </div>
+        <?php
+        } else {
+        ?>
+            <div class="ZAQIBB">
+                <button onclick="alert('Para descargar el archivo necesitas registrarte e iniciar sesión.');" class="icon-arrow-down" aria-label="Descargar">
+                    <?php echo $GLOBALS['descargaicono']; ?>
+                </button>
+            </div>
+<?php
+        }
+    }
+    return ob_get_clean();
+}
