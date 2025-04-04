32898e1a0af0961b4f1e4c9b019e0731dd7c3da5
1ndoryu <andoryyu@gmail.com>
2025-04-04 22:07:11 +0200

    Refactor(Org): Mueve función panel() de app/Pages/Sello.php a app/View/Components/Sello/SelloPanel.php

--- app/Pages/Sello.php
+++ app/Pages/Sello.php
@@ -1,21 +1,6 @@
-<?
+<?php
 
-function panel()
-{
-    ob_start();
-?>
-
-    <div class="FLXVTQ">
-        <a href="https://2upra.com/">
-            <p>Aquí podrás ver tus rolas enviadas a las plataformas de stream, pero aún estamos trabajando en esta funcionalidad.</p>
-            <button class="borde">Volver</button>
-        </a>
-    </div>
-
-<?
-    return ob_get_clean();
-}
-
+// Funcion panel() movida a app/View/Components/Sello/SelloPanel.php
 
 /*
 function panel()
