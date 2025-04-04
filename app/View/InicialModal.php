32b4c1cc8b62bd5dc4a2f22a9527a100477ed53b
1ndoryu <andoryyu@gmail.com>
2025-04-04 23:07:15 +0200

    Refactor(Clean): Elimina archivo vacío app/View/InicialModal.php

--- app/View/InicialModal.php
+++
@@ -1,6 +0,0 @@
-<?
-// Refactor(Org): Funcion modalTipoUsuario() movida a app/View/Modals/TipoUsuarioModal.php
-// Refactor(Org): Funcion modalGeneros() movida a app/View/Modals/GenerosModal.php
-
-// Refactor(Org): Funcion guardarGenerosUsuario() y hook AJAX movidos a app/Services/UserService.php
-
