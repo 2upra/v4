2f09e4a9789b599f1687d7cf4e02b6fea9c9bee2
1ndoryu <andoryyu@gmail.com>
2025-04-04 10:02:06 +0200

    Refactor(Org): Mueve función opcionesComentarios() a CommentHelper

--- app/Content/Posts/View/componentPost.php
+++ app/Content/Posts/View/componentPost.php
@@ -54,35 +54,7 @@ function variablesPosts($postId = null)
 
 // Refactor(Org): Función opcionesRola() movida a app/View/Components/PostOptions.php
 
-function opcionesComentarios($postId, $autorId)
-{
-    $usuarioActual = get_current_user_id();
-    ob_start();
-?>
-    <button class="submenucomentario" data-post-id="<? echo $postId; ?>"><? echo $GLOBALS['iconotrespuntos']; ?></button>
-
-    <div class="A1806241" id="opcionescomentarios-<? echo $postId; ?>">
-        <div class="A1806242">
-            <? if (current_user_can('administrator')) : ?>
-                <button class="eliminarPost" data-post-id="<? echo $postId; ?>">Eliminar</button>
-                <button class="editarPost" data-post-id="<? echo $postId; ?>">Editar</button>
-                <button class="editarWordPress" data-post-id="<? echo $postId; ?>">Editar en WordPress</button>
-                <button class="banearUsuario" data-post-id="<? echo $postId; ?>">Banear</button>
-            <? elseif ($usuarioActual == $autorId) : ?>
-                <button class="editarPost" data-post-id="<? echo $postId; ?>">Editar</button>
-                <button class="eliminarPost" data-post-id="<? echo $postId; ?>">Eliminar</button>
-            <? else : ?>
-                <button class="iralpost"><a ajaxUrl="<? echo esc_url(get_permalink()); ?>">Ir al post</a></button>
-                <button class="reporte" data-post-id="<? echo $postId; ?>" tipoContenido="social_post">Reportar</button>
-                <button class="bloquear" data-post-id="<? echo $postId; ?>">Bloquear</button>
-            <? endif; ?>
-        </div>
-    </div>
-
-    <div id="modalBackground4" class="modal-background submenu modalBackground2 modalBackground3" style="display: none;"></div>
-<?
-    return ob_get_clean();
-}
+// Refactor(Org): Función opcionesComentarios() movida a app/View/Helpers/CommentHelper.php
 
 
 
--- app/View/Helpers/CommentHelper.php
+++ app/View/Helpers/CommentHelper.php
@@ -18,3 +18,34 @@ function botonComentar($postId)
 <?
     return ob_get_clean();
 }
+
+// Refactor(Org): Función opcionesComentarios() movida desde app/Content/Posts/View/componentPost.php
+function opcionesComentarios($postId, $autorId)
+{
+    $usuarioActual = get_current_user_id();
+    ob_start();
+?>
+    <button class="submenucomentario" data-post-id="<? echo $postId; ?>"><? echo $GLOBALS['iconotrespuntos']; ?></button>
+
+    <div class="A1806241" id="opcionescomentarios-<? echo $postId; ?>">
+        <div class="A1806242">
+            <? if (current_user_can('administrator')) : ?>
+                <button class="eliminarPost" data-post-id="<? echo $postId; ?>">Eliminar</button>
+                <button class="editarPost" data-post-id="<? echo $postId; ?>">Editar</button>
+                <button class="editarWordPress" data-post-id="<? echo $postId; ?>">Editar en WordPress</button>
+                <button class="banearUsuario" data-post-id="<? echo $postId; ?>">Banear</button>
+            <? elseif ($usuarioActual == $autorId) : ?>
+                <button class="editarPost" data-post-id="<? echo $postId; ?>">Editar</button>
+                <button class="eliminarPost" data-post-id="<? echo $postId; ?>">Eliminar</button>
+            <? else : ?>
+                <button class="iralpost"><a ajaxUrl="<? echo esc_url(get_permalink()); ?>">Ir al post</a></button>
+                <button class="reporte" data-post-id="<? echo $postId; ?>" tipoContenido="social_post">Reportar</button>
+                <button class="bloquear" data-post-id="<? echo $postId; ?>">Bloquear</button>
+            <? endif; ?>
+        </div>
+    </div>
+
+    <div id="modalBackground4" class="modal-background submenu modalBackground2 modalBackground3" style="display: none;"></div>
+<?
+    return ob_get_clean();
+}
