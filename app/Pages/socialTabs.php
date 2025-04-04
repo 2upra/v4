9ee70e9caaa1d572d4ec6c2832775a689c5bbeaf
1ndoryu <andoryyu@gmail.com>
2025-04-04 21:48:53 +0200

    Refactor(Clean): Elimina archivo vacío app/Pages/socialTabs.php

--- app/Pages/socialTabs.php
+++
@@ -1,9 +0,0 @@
-<?
-
-// Refactor(Org): Removed function socialTabs (moved to app/View/Components/SocialTabs.php)
-// Refactor(Org): Moved function socialTabsFEED to app/View/Components/Tabs/SocialTabs.php
-// Refactor(Org): Moved function socialTabsSAMPLE to app/View/Components/Tabs/SocialTabs.php
-// Refactor(Org): Moved function momentosfijos to app/View/Components/MomentosFijos.php
-
-
-
