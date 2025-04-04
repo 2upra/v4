8011b40833da475d53d3f04ccf02f5b220439a48
1ndoryu <andoryyu@gmail.com>
2025-04-04 22:57:07 +0200

    Refactor(Org): Mueve función guardarGenerosUsuario() y hook AJAX a UserService.php

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -269,4 +269,23 @@ function obtenerInteresesUsuario($userId) {
     return $intereses;
 }
 
+// Refactor(Org): Funcion guardarGenerosUsuario() y hook AJAX movidos desde app/View/InicialModal.php
+function guardarGenerosUsuario()
+{
+    if (!is_user_logged_in()) {
+        wp_send_json_error('Debes iniciar sesión para realizar esta acción.');
+    }
+    $generos = isset($_POST['generos']) ? explode(',', $_POST['generos']) : array();
+
+    if (empty($generos) || !is_array($generos)) {
+        wp_send_json_error('No se recibieron géneros seleccionados.');
+    }
+
+    $generos_sanitizados = array_map('sanitize_text_field', $generos);
+    $userId = get_current_user_id();
+    update_user_meta($userId, 'usuarioPreferencias', $generos_sanitizados);
+    wp_send_json_success('Los géneros han sido guardados.');
+}
+add_action('wp_ajax_guardarGenerosUsuario', 'guardarGenerosUsuario');
+
 ?>
