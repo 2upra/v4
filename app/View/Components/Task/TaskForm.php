6ee939226d4b15a4e0316fde5f3ef5ae63f8a3ff
1ndoryu <andoryyu@gmail.com>
2025-04-05 03:25:32 +0200

    Refactor(Org): Mueve función formTareaEstilo() a app/View/Helpers/StyleHelper.php

--- app/View/Components/Task/TaskForm.php
+++ app/View/Components/Task/TaskForm.php
@@ -1,5 +1,6 @@
 <?php
 // Refactor(Org): Funciones formTarea() y formTareaEstilo() movidas desde app/Content/Task/logicTareas.php
+// Refactor(Org): Función formTareaEstilo() movida a app/View/Helpers/StyleHelper.php
 
 function formTarea()
 {
@@ -43,58 +44,3 @@ function formTarea()
     return ob_get_clean();
 }
 
-function formTareaEstilo()
-{
-    ob_start();
-?>
-    <style>
-        span.icono p {
-            font-size: 12px;
-        }
-
-        span.icono {
-            display: flex;
-            flex-direction: row;
-            font-size: 11px;
-            gap: 6px;
-            padding: 0px 5px;
-            border-radius: 100px;
-            align-items: center;
-            justify-content: center;
-            width: max-content;
-            opacity: 0.9;
-            cursor: pointer;
-        }
-
-        .selectorIcono {
-            padding: 10px 0px;
-        }
-
-        .bloque.tareasbloque svg {
-            cursor: pointer;
-        }
-
-        .bloque.tareasbloque {
-            display: flex;
-            flex-direction: row;
-            height: 40px;
-            padding: 5px;
-            align-items: center;
-            padding-right: 20px;
-            background: unset;
-        }
-
-        .tareasbloque input {
-            background: none;
-        }
-
-        .LNVHED.no-tareas {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            padding: 100px;
-        }
-    </style>
-<?
-    return ob_get_clean();
-}
