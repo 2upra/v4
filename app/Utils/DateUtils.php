b79b7cb3da23a71b7c364b2e37f28d9b47498133
1ndoryu <andoryyu@gmail.com>
2025-04-05 16:30:52 +0200

    Refactor(Clean): Elimina archivo vacío app/Utils/DateUtils.php

--- app/Utils/DateUtils.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Refactor(Org): Funcion ajustarZonaHoraria() y hooks AJAX movidos a app/Services/UserService.php
-
-// Refactor(Org): Funcion TiempoRelativoNoti movida a app/View/Helpers/NotificationHelper.php
-
-// Refactor(Org): Funcion tiempoRelativo() movida a app/View/Helpers/DateHelper.php
