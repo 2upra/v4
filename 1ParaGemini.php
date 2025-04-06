633427244d06248dcaf85ed636c7e0da01a6b48f
1ndoryu <andoryyu@gmail.com>
2025-04-06 18:14:54 -0400

    *

--- 1ParaGemini.php
+++ 1ParaGemini.php
@@ -11,4 +11,10 @@ Este archivo ha sido creado por el usuario humano que maneja de este proyecto pa
 
 1) Hay algunos archivos que son muy extensos que tu mismo creaste, por ejemplo UIHelper.php es muy extenso, y contiene funciones php que deberían estar ordenadas en carpetas especificas. Algo similar paso en postService pero ya lo estas arreglando. 
 
+6 de abril de 2025 
+
+1) te quedas en bucles absurdos, por ejemplo 
+
+// Refactor(Org): Moved function recalcularHash to app/Services/FileHashService.php tu acción, despues de vomverla a la carpeta services, la mueves a la carpeta utils, y asi, a veces en bucle repite movera como si no te decidieras si es un servicio o mejor va en la carpeta utils. No se cual es la mejor solución, pero mejor decidite y toma en cuenta los comentarios de los movimientos realizados para que no caigas en bucle. 
+
 */
