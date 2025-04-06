93d05ec1dd4ef3c7c2d3826edee8f7ec724d05ae
1ndoryu <andoryyu@gmail.com>
2025-04-07 01:42:23 +0200

    Refactor(Org): Mueve funciones botonDescargaColec y botonSincronizarColec a app/View/Helpers/DownloadHelper.php

--- app/View/Helpers/DownloadHelper.php
+++ app/View/Helpers/DownloadHelper.php
@@ -77,3 +77,75 @@ function botonSincronizar($postId)
     }
     return ob_get_clean();
 }
+
+// Refactor(Exec): Funciones botonDescargaColec y botonSincronizarColec movidas desde app/Functions/descargarColeccion.php
+function botonDescargaColec($postId, $sampleCount)
+{
+    ob_start();
+
+    $userID = get_current_user_id();
+
+    if ($userID) {
+        $descargas_anteriores = get_user_meta($userID, 'descargas', true);
+        $yaDescargado = isset($descargas_anteriores[$postId]);
+        $claseExtra = $yaDescargado ? 'yaDescargado' : '';
+
+?>
+        <div class="ZAQIBB">
+            <button class="icon-arrow-down botonprincipal <? echo esc_attr($claseExtra); ?>"
+                data-post-id="<? echo esc_attr($postId); ?>"
+                aria-label="Boton Descarga"
+                id="download-button-<? echo esc_attr($postId); ?>"
+                onclick="return procesarDescarga('<? echo esc_js($postId); ?>', '<? echo esc_js($userID); ?>', 'true', '<? echo esc_js($sampleCount); ?>')">
+                <? echo $GLOBALS['descargaicono']; ?> Descargar
+            </button>
+        </div>
+    <?php
+    } else {
+    ?>
+        <div class="ZAQIBB">
+            <button onclick="alert('Para descargar el archivo necesitas registrarte e iniciar sesión.');" class="icon-arrow-down" aria-label="Descargar">
+                <? echo $GLOBALS['descargaicono']; ?>
+            </button>
+        </div>
+    <?php
+    }
+
+    return ob_get_clean();
+}
+
+function botonSincronizarColec($postId, $sampleCount)
+{
+    ob_start();
+
+    $userID = get_current_user_id();
+
+    if ($userID) {
+        $descargas_anteriores = get_user_meta($userID, 'descargas', true);
+        $yaDescargado = isset($descargas_anteriores[$postId]);
+        $claseExtra = $yaDescargado ? 'yaDescargado' : '';
+
+    ?>
+        <div class="ZAQIBB">
+            <button class="icon-arrow-down botonsecundario <? echo esc_attr($claseExtra); ?>"
+                data-post-id="<? echo esc_attr($postId); ?>"
+                aria-label="Boton Descarga"
+                id="download-button-<? echo esc_attr($postId); ?>"
+                aca necesito que envie otro valor en true de forma de segura, ahora procesarDescarga recibira otro valor
+                onclick="return procesarDescarga('<? echo esc_js($postId); ?>', '<? echo esc_js($userID); ?>', 'true', '<? echo esc_js($sampleCount); ?>', 'true')">
+                Sincronizar
+            </button>
+        </div>
+    <?php
+    } else {
+    ?>
+        <div class="ZAQIBB">
+            <button onclick="alert('Para descargar el archivo necesitas registrarte e iniciar sesión.');" class="icon-arrow-down" aria-label="Descargar">
+                Sincronizar
+            </button>
+        </div>
+<?php
+    }
+
+    return ob_get_clean();
+}
