4d17ad01874bcdf3d69eff24e1a0166546307789
1ndoryu <andoryyu@gmail.com>
2025-04-06 14:25:38 +0200

    Refactor(Clean): Elimina archivo vacío app/Functions/protegerAudio.php

--- app/Functions/protegerAudio.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Refactor(Org): Moved audio optimization (64k) and protection logic to app/Services/AudioProcessingService.php
-// This file is now empty and can be potentially removed in a later step.
