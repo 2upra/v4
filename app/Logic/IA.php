c9ccbfb56d28049c115bf13e16f7a769e16659e8
1ndoryu <andoryyu@gmail.com>
2025-04-07 08:51:40 +0200

    Refactor(Org): Mueve función generarDescripcionIAConURI() de app/Logic/IA.php a app/Services/IAService.php

--- app/Logic/IA.php
+++ app/Logic/IA.php
@@ -2,69 +2,7 @@
 
 use GuzzleHttp\Client;
 
-function generarDescripcionIAConURI($audio_uri, $prompt) {
-    iaLog("Generando descripción IA con prompt: " . $prompt . " y URI: " . $audio_uri);
-
-    try {
-        // Construir el cuerpo de la solicitud
-        $data = [
-            "contents" => [
-                [
-                    "parts" => [
-                        [
-                            "text" => $prompt
-                        ],
-                        [
-                            "inline_data" => [
-                                "mime_type" => "audio/mp3",
-                                "uri" => $audio_uri
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ];
-
-        // Hacer la solicitud POST usando CURL
-        $apiKey = $_ENV['API_KEY'];
-        $url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=$apiKey";
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            'Content-Type: application/json',
-        ]);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-
-        $response = curl_exec($ch);
-        if (curl_errno($ch)) {
-            $error_message = "Error en CURL: " . curl_error($ch);
-            iaLog($error_message);
-            return $error_message;
-        }
-
-        curl_close($ch);
-        $bodyGenerate = json_decode($response, true);
-
-        // Verificar si la respuesta contiene los datos esperados
-        if (isset($bodyGenerate['contents'][0]['parts'][0]['text'])) {
-            $generated_text = $bodyGenerate['contents'][0]['parts'][0]['text'];
-            iaLog("Contenido generado: " . $generated_text);
-
-            return $generated_text;
-        } else {
-            $error_message = "Error: Respuesta inesperada de la API.";
-            iaLog($error_message);
-            return $error_message;
-        }
-
-    } catch (Exception $e) {
-        $error_message = "Error: " . $e->getMessage();
-        iaLog($error_message);
-        return $error_message;
-    }
-}
+// Refactor(Org): Función generarDescripcionIAConURI movida a app/Services/IAService.php
 
 
 
