f18c20b33f75cb7e6b7a595e72ea816d46f53cfe
1ndoryu <andoryyu@gmail.com>
2025-04-07 12:32:19 +0200

    Refactor(Org): Mueve función borrarTarea() y hook AJAX de TaskService.php a Task/TaskCrudService.php

--- app/Services/Task/TaskCrudService.php
+++ app/Services/Task/TaskCrudService.php
@@ -101,3 +101,51 @@ function crearTarea()
 }
 
 add_action('wp_ajax_crearTarea', 'crearTarea');
+
+// Refactor(Org): Funcion borrarTarea() y hook AJAX movidos desde app/Services/TaskService.php
+function borrarTarea()
+{
+    // Añadir verificacion de nonce
+    if (!isset($_POST['nonce']) || empty($_POST['nonce']) || !wp_verify_nonce($_POST['nonce'], 'borrar_tarea_nonce')) {
+        wp_send_json_error('Nonce invalido.');
+        // wp_die(); // wp_send_json_error ya incluye wp_die()
+    }
+
+    $log = '';
+    if (!current_user_can('edit_posts')) {
+        $log .= 'No tienes permisos.';
+        guardarLog("borrarTarea: \n $log");
+        wp_send_json_error('No tienes permisos.');
+    }
+
+    $id = isset($_POST['id']) ? intval($_POST['id']) : 0;
+
+    if ($id === 0) {
+        $log .= 'ID de tarea inválido.';
+        guardarLog("borrarTarea: \n $log");
+        wp_send_json_error('ID de tarea inválido.');
+    }
+
+    $tarea = get_post($id);
+
+    if (empty($tarea) || $tarea->post_type != 'tarea') {
+        $log .= 'Tarea no encontrada.';
+        guardarLog("borrarTarea: \n $log");
+        wp_send_json_error('Tarea no encontrada.');
+    }
+
+    $res = wp_delete_post($id, true);
+
+    if (is_wp_error($res)) {
+        $msg = $res->get_error_message();
+        $log .= "Error al borrar tarea: $msg";
+        guardarLog("borrarTarea: \n $log");
+        wp_send_json_error($msg);
+    }
+
+    $log .= "Tarea con ID $id borrada exitosamente.";
+    guardarLog("borrarTarea: \n $log");
+    wp_send_json_success();
+}
+
+add_action('wp_ajax_borrarTarea', 'borrarTarea');
