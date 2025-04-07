93dd9dce63b6b70dc33b41098cab0098cdea37c0
1ndoryu <andoryyu@gmail.com>
2025-04-07 05:36:57 +0200

    Refactor(Clean): Elimina archivo vacío app/Content/Comentarios/logicComentarios.php

--- app/Content/Comentarios/logicComentarios.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// La función procesarComentario() y su hook AJAX se movieron a app/Services/CommentService.php
-// La función redirigir_comentarios() y su hook se movieron a app/Services/CommentService.php
-
-?>
