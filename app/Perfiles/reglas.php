866d3fa44f182ea89687dcabd69e652e4e9a2994
1ndoryu <andoryyu@gmail.com>
2025-04-07 11:00:57 +0200

    Refactor(Clean): Elimina archivo app/Perfiles/reglas.php vaciado en refactorizaciones anteriores

--- app/Perfiles/reglas.php
+++
@@ -1,6 +0,0 @@
-<?
-
-// Refactor(Org): Lógica de redirección de búsqueda movida a app/Setup/ThemeSetup.php
-// Refactor(Org): Lógica de redirección/reescritura de perfiles movida a ThemeSetup.php
-
-?>
