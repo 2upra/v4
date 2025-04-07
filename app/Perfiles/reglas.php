89b88f8d1a28ec6f0ba11efebdec2045cd53ab34
1ndoryu <andoryyu@gmail.com>
2025-04-07 10:57:45 +0200

    Refactor(Org): Mueve lógica de redirección de búsqueda de app/Perfiles/reglas.php a app/Setup/ThemeSetup.php

--- app/Perfiles/reglas.php
+++ app/Perfiles/reglas.php
@@ -1,19 +1,6 @@
 <?
 
-add_action('template_redirect', function () {
-    // Verificar si estamos en la página de inicio y si el parámetro 'search' está en la URL
-    if (!is_front_page() && isset($_GET['search'])) {
-        $search_query = sanitize_text_field($_GET['search']); // Sanitiza el término de búsqueda
-
-        // Evitar redirección infinita comprobando si ya estamos redirigiendo a esta URL
-        if (!isset($_GET['redirected'])) {
-            // Redirige a la página de inicio con el parámetro de búsqueda y una marca para evitar bucles
-            wp_redirect(home_url('/?search=' . urlencode($search_query) . '&redirected=1'));
-            exit;
-        }
-    }
-});
-
+// Refactor(Org): Lógica de redirección de búsqueda movida a app/Setup/ThemeSetup.php
 // Refactor(Org): Lógica de redirección/reescritura de perfiles movida a ThemeSetup.php
 
 ?>
--- app/Setup/ThemeSetup.php
+++ app/Setup/ThemeSetup.php
@@ -824,4 +824,19 @@ add_filter('query_vars', 'custom_query_vars');
 add_filter('template_include', 'custom_template_include');
 add_action('init', 'flush_rewrite_rules_once');
 
+// Refactor(Org): Mover lógica de redirección de búsqueda desde app/Perfiles/reglas.php
+add_action('template_redirect', function () {
+    // Verificar si estamos en la página de inicio y si el parámetro 'search' está en la URL
+    if (!is_front_page() && isset($_GET['search'])) {
+        $search_query = sanitize_text_field($_GET['search']); // Sanitiza el término de búsqueda
+
+        // Evitar redirección infinita comprobando si ya estamos redirigiendo a esta URL
+        if (!isset($_GET['redirected'])) {
+            // Redirige a la página de inicio con el parámetro de búsqueda y una marca para evitar bucles
+            wp_redirect(home_url('/?search=' . urlencode($search_query) . '&redirected=1'));
+            exit;
+        }
+    }
+});
+
 ?>
