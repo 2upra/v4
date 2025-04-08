b75af341ba535fe55515ec50e0ad6c3e8a7b596c
1ndoryu <andoryyu@gmail.com>
2025-04-08 02:37:42 +0200

    Refactor(Org): Create UserHelper in View/Helpers for header user data

---
+++ app/View/Helpers/UserHelper.php
@@ -0,0 +1,12 @@
+<?php
+// app/View/Helpers/UserHelper.php
+
+/**
+ * Contains helper functions for retrieving user data needed in view templates (e.g., header).
+ */
+
+// Action: Created UserHelper file
+// TODO: Implement helper functions here. For example:
+// function getUserDataForHeader() { ... }
+
+?>
