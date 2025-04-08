0e5b4d025ae28a028fa326c12d41fdff08a71f74
1ndoryu <andoryyu@gmail.com>
2025-04-08 04:25:44 +0200

    Refactor(Org): Crea archivo Post/PostDataService.php para funciones de obtención de datos de posts

---
+++ app/Services/Post/PostDataService.php
@@ -0,0 +1,7 @@
+<?php
+
+// Archivo creado para contener funciones auxiliares de obtención de datos de posts.
+// Originalmente, estas funciones estaban o estarán en PostService.php.
+
+// TODO: Mover la función variablesPosts y otras funciones relacionadas aquí desde PostService.php.
+
