8af14e3d0a81a814977e61d9031dc5d47b22248c
1ndoryu <andoryyu@gmail.com>
2025-04-08 04:36:09 +0200

    Refactor(Org): Crea archivo PapeleraContent.php para componente de vista informativa

---
+++ app/View/Components/Info/PapeleraContent.php
@@ -0,0 +1,8 @@
+<?php
+// Archivo creado para contener la función papelera() movida desde app/Pages/Temporal.php.
+// Acción realizada: crear_archivo
+// Propósito: Contendrá la función papelera() movida desde app/Pages/Temporal.php.
+
+// TODO: Mover la función papelera() aquí desde app/Pages/Temporal.php
+
+?>
