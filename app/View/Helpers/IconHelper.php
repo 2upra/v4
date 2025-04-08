1b38fb6969970fe23c46a8e4288992893ed67b86
1ndoryu <andoryyu@gmail.com>
2025-04-08 04:57:54 +0200

    Refactor(Org): Create IconHelper.php for centralizing SVG icon rendering

---
+++ app/View/Helpers/IconHelper.php
@@ -0,0 +1,10 @@
+<?php
+// Action: Created file app/View/Helpers/IconHelper.php
+// Purpose: Centralizes SVG icon rendering logic.
+// This helper will contain functions to render SVG icons,
+// replacing the global variables previously defined in app/Misc/iconos.php.
+
+class IconHelper {
+    // TODO: Implement methods to load and render icons based on their names.
+    // Example: public function render($iconName, $options = []) { ... }
+}
