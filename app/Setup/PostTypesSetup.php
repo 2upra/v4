992ea6f54107ebbe571a6fd48b2a6f9eecd3cedc
1ndoryu <andoryyu@gmail.com>
2025-04-09 14:31:15 +0200

    Refactor(Org): Crea archivo app/Setup/PostTypesSetup.php para lógica de registro CPT

---
+++ app/Setup/PostTypesSetup.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Archivo creado por refactorización.
+ * Contiene funciones para registrar Custom Post Types y estados de post personalizados.
+ */
+
+// Aquí se añadirán las funciones para registrar CPTs y estados.
+
+?>
