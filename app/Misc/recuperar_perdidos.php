5cbe8a360a08ae4863623c451464ae9ca030f2a9
1ndoryu <andoryyu@gmail.com>
2025-04-09 02:19:13 +0200

    Refactor(Clean): Elimina archivo vacío app/Misc/recuperar_perdidos.php

--- app/Misc/recuperar_perdidos.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Acción realizada: Mover funciones de recuperación de archivos perdidos a app/Admin/LostFilesRecoveryAdmin.php
-// Este archivo ahora está vacío ya que su contenido fue movido.
-
-?>
