fe5bd0d801e62ca58c963f808b43dfd021237019
1ndoryu <andoryyu@gmail.com>
2025-04-09 16:01:09 +0200

    Refactor(Org): Mueve función htmlColab() de renderColab.php a app/View/Renderers/ColabRenderer.php

--- app/Content/Colab/renderColab.php
+++ app/Content/Colab/renderColab.php
@@ -14,46 +14,7 @@
     
 */
 
-function htmlColab($filtro)
-{
-    $post_id = get_the_ID();
-    $var = variablesColab($post_id);
-    extract($var);
-    ob_start();
-
-?>
-
-    <li class="modal POST-<? echo esc_attr($filtro); ?> EDYQHV"
-        filtro="<? echo esc_attr($filtro); ?>"
-        id-post="<? echo get_the_ID(); ?>"
-        autor="<? echo esc_attr($colabColaborador); ?>">
-
-        <div class="colab-content">
-            <? if ($filtro === 'colabPendiente'): ?>
-                <? echo opcionesColab($var); ?>
-                <? echo contenidoColab($var); ?>
-            <? else: ?>
-                <div class="UICMCG">
-                    <? echo tituloColab($var); ?>
-                    <? echo participantesColab($var) ?>
-                    <button class="cerrarColab" id-post="<? echo get_the_ID(); ?>"><? echo $GLOBALS['cancelicon']; ?></button>
-                    <? // echo opcionesColabActivo($var); ?>
-                </div>
-                <div class="MXPLYN">
-                    <? echo chatColab($var); ?>
-                    <? //echo archivosColab($var); ?>
-                    <? //echo historialColab($var); ?>
-                    <? //echo comandosColab($var); ?>
-                    <? //echo enviarColab($var);?>
-                </div>
-            <? endif; ?>
-
-        </div>
-    </li>
-
-<?
-    return ob_get_clean();
-}
+// Refactor(Org): Función htmlColab movida a app/View/Renderers/ColabRenderer.php
 
 function colab()
 {
