a45b81f8064c0341c0d576ede9ed6fd9b5224a34
1ndoryu <andoryyu@gmail.com>
2025-04-09 05:14:26 +0200

    Refactor(Org): Mueve función opcionesRola() de PostOptions.php a PostHelper.php

--- app/View/Components/PostOptions.php
+++ app/View/Components/PostOptions.php
@@ -1,39 +1,5 @@
 <?php
 // Refactor(Org): Función opcionesPost() movida a app/View/Helpers/PostHelper.php
 
-// Refactor(Org): Función opcionesRola() movida desde app/Content/Posts/View/componentPost.php
-//OPCIONES EN LAS ROLAS 
-function opcionesRola($postId, $post_status, $audio_url)
-{
-    ob_start();
-?>
-    <button class="HR695R7" data-post-id="<? echo $postId; ?>"><? echo $GLOBALS['iconotrespuntos']; ?></button>
+// Refactor(Org): Función opcionesRola() movida a app/View/Helpers/PostHelper.php
 
-    <div class="A1806241" id="opcionesrola-<? echo $postId; ?>">
-        <div class="A1806242">
-            <? if (current_user_can('administrator') && $post_status != 'publish' && $post_status != 'pending_deletion') { ?>
-                <button class="toggle-status-rola" data-post-id="<? echo $postId; ?>">Cambiar estado</button>
-            <? } ?>
-
-            <? if (current_user_can('administrator') && $post_status != 'publish' && $post_status != 'rejected' && $post_status != 'pending_deletion') { ?>
-                <button class="rechazar-rola" data-post-id="<? echo $postId; ?>">Rechazar rola</button>
-            <? } ?>
-
-            <button class="download-button" data-audio-url="<? echo $audio_url; ?>" data-filename="<? echo basename($audio_url); ?>">Descargar</button>
-
-            <? if ($post_status != 'rejected' && $post_status != 'pending_deletion') { ?>
-                <? if ($post_status == 'pending') { ?>
-                    <button class="request-deletion" data-post-id="<? echo $postId; ?>">Cancelar publicación</button>
-                <? } else { ?>
-                    <button class="request-deletion" data-post-id="<? echo $postId; ?>">Solicitar eliminación</button>
-                <? } ?>
-            <? } ?>
-
-        </div>
-    </div>
-
-    <div id="modalBackground3" class="modal-background submenu modalBackground2 modalBackground3" style="display: none;"></div>
-
-<?
-    return ob_get_clean();
-}
