d7a1bd044689f6b8c9f63b954125d7f00e1b369b
1ndoryu <andoryyu@gmail.com>
2025-04-09 03:01:53 +0200

    Refactor(Org): Mueve función renderChat() de app/Chat/renderChat.php a app/View/Renderers/ChatRenderer.php

--- app/View/Renderers/ChatRenderer.php
+++ app/View/Renderers/ChatRenderer.php
@@ -76,3 +76,52 @@ function renderListaChats($conversaciones, $usuarioId)
     return $htmlGenerado;
 }
 
+// Refactor(Org): Mueve función renderChat() de app/Chat/renderChat.php a app/View/Renderers/ChatRenderer.php
+function renderChat()
+{
+    ob_start();
+    ?>
+    <div class="bloque modal bloqueChat" id="bloqueChat" data-user-id="" style="display: none;">
+        <div class="infoChat">
+            <div class="imagenMensaje">
+                <img src="" alt="Imagen de perfil">
+            </div>
+            <div class="nombreConversacion">
+                <p></p>
+                <span class="estadoConexion">Desconectado</span>
+            </div>
+            <div class="botoneschat">
+                <button class="minizarChat" id="minizarChat"><? echo $GLOBALS['minus']; ?></button>
+                <button class="cerrarChat" id="cerrarChat"><? echo $GLOBALS['cancelicon']; ?></button>
+            </div>
+        </div>
+        <ul class="listaMensajes"></ul>
+
+        <div class="previewsForm NGEESM previewsChat" style="position: relative;">
+            <!-- Vista previa de imagen -->
+            <div class="previewAreaArchivos previewChatImagen" id="previewChatImagen" style="display: none;">
+                <label>Imagen</label>
+            </div>
+            <!-- Vista previa de audio -->
+            <div class="previewAreaArchivos previewChatAudio" id="previewChatAudio" style="display: none;">
+                <label>Audio</label>
+            </div>
+            <!-- Vista previa de archivo -->
+            <div class="previewAreaArchivos previewChatArchivo" id="previewChatArchivo" style="display: none;">
+                <label>Archivo</label>
+            </div>
+
+            <!-- Botón de cancelar único, que aparecerá en cualquier vista previa -->
+            <button class="cancelButton borde cancelUploadButton" id="cancelUploadButton" style="display: none;">Cancelar</button>
+        </div>
+
+        <div class="chatEnvio individualSend">
+            <textarea class="mensajeContenido" rows="1"></textarea>
+            <button class="enviarMensaje"><? echo $GLOBALS['enviarMensaje']; ?></button>
+            <button class="enviarAdjunto" id="enviarAdjunto"><? echo $GLOBALS['enviarAdjunto']; ?></button>
+        </div>
+    </div>
+    <?
+    $htmlGenerado = ob_get_clean();
+    return $htmlGenerado;
+}
