93e2e4cb06dce1815b8593d83c6dae3d44b32503
1ndoryu <andoryyu@gmail.com>
2025-04-09 11:19:54 +0200

    Refactor(Org): Mueve función htmlPost() de renderPost.php a app/View/Renderers/PostRenderer.php

--- app/Content/Posts/View/renderPost.php
+++ app/Content/Posts/View/renderPost.php
@@ -1,55 +1,6 @@
 <?
 
-function htmlPost($filtro)
-{
-    $post_id = get_the_ID();
-    $vars = variablesPosts($post_id);
-    extract($vars);
-    $music = ($filtro === 'rola' || $filtro === 'likes');
-    if (in_array($filtro, ['rolasEliminadas', 'rolasRechazadas', 'rola', 'likes'])) {
-        $filtro = 'rolastatus';
-    }
-    $sampleList = $filtro === 'sampleList';
-    $rolaList = $filtro === 'rolaListLike';
-    $momento = $filtro === 'momento';
-    //llevar sample list
-    $wave = get_post_meta($post_id, 'waveform_image_url', true);
-    $waveCargada = get_post_meta($post_id, 'waveCargada', true);
-    $postAut = get_post_meta($post_id, 'postAut', true);
-    $verificado = get_post_meta($post_id, 'Verificado', true);
-    $recortado = get_post_meta($post_id, 'recortado', true);
-    $urlAudioSegura = audioUrlSegura($audio_id_lite);
-    if (is_wp_error($urlAudioSegura)) {
-        $urlAudioSegura = '';
-    }
-    ob_start();
-?>
-    <li class="POST-<? echo esc_attr($filtro); ?> EDYQHV <? echo get_the_ID(); ?>"
-        filtro="<? echo esc_attr($filtro); ?>"
-        id-post="<? echo get_the_ID(); ?>"
-        autor="<? echo esc_attr($author_id); ?>">
-
-        <? if ($sampleList || $rolaList):
-            // Refactor(Org): Función limpiarJSON movida a StringUtils.php
-             ?>
-            <? sampleListHtml($block, $es_suscriptor, $post_id, $datosAlgoritmo, $verificado, $postAut, $urlAudioSegura, $wave, $waveCargada, $colab, $author_id, $audio_id_lite); ?>
-        <? else: ?>
-            <? echo fondoPost($filtro, $block, $es_suscriptor, $post_id); ?>
-            <? if ($music || $momento): ?>
-                <? renderMusicContent($filtro, $post_id, $author_name, $block, $es_suscriptor, $post_status, $audio_url); ?>
-            <? else: ?>
-                <? renderNonMusicContent($filtro, $post_id, $author_id, $author_avatar, $author_name, $post_date, $block, $colab, $es_suscriptor, $audio_url, $scale, $key, $bpm, $datosAlgoritmo, $post_status, $audio_id_lite); ?>
-            <? endif; ?>
-        <? endif; ?>
-    </li>
-
-    <li class="comentariosPost">
-
-    </li>
-<?
-    return ob_get_clean();
-}
-
+// Refactor(Exec): Función htmlPost() movida a app/View/Renderers/PostRenderer.php
 
 
 
@@ -476,6 +427,40 @@ function renderContentAndMedia($filtro, $post_id, $audio_url, $scale, $key, $bpm
 // Refactor(Org): Función limpiarJSON movida a StringUtils.php
 
 // Refactor(Org): Función nohayPost movida a app/View/Helpers/PostHelper.php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
