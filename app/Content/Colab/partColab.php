71c6710af8da3c86c8ac66fbeef63f5b0be73902
1ndoryu <andoryyu@gmail.com>
2025-04-10 11:01:31 +0200

    Refactor(Org): Mueve función participantesColab() de partColab.php a app/View/Helpers/ColabHelper.php

--- app/Content/Colab/partColab.php
+++ app/Content/Colab/partColab.php
@@ -56,21 +56,7 @@ function variablesColab($post_id = null)
 
 // Refactor(Exec): Función tituloColab() movida a app/View/Renderers/ColabRenderer.php
 
-function participantesColab($var)
-{
-    $post_id = $var['post_id'];
-    $colabColaboradorAvatar = $var['colabColaboradorAvatar'];
-    $colabAutorAvatar = $var['colabAutorAvatar'];
-
-    ob_start();
-?>
-    <div class="LIFVXC">
-        <img src="<? echo esc_url($colabColaboradorAvatar); ?>">
-        <img src="<? echo esc_url($colabAutorAvatar); ?>">
-    </div>
-<?php
-    return ob_get_clean();
-}
+// Refactor(Exec): Función participantesColab() movida a app/View/Helpers/ColabHelper.php
 
 function opcionesColabActivo($var)
 {
--- app/View/Helpers/ColabHelper.php
+++ app/View/Helpers/ColabHelper.php
@@ -100,3 +100,20 @@ function contenidoColab($var)
 <?php
     return ob_get_clean();
 }
+
+// Refactor(Exec): Mover función participantesColab() desde app/Content/Colab/partColab.php
+function participantesColab($var)
+{
+    $post_id = $var['post_id'];
+    $colabColaboradorAvatar = $var['colabColaboradorAvatar'];
+    $colabAutorAvatar = $var['colabAutorAvatar'];
+
+    ob_start();
+?>
+    <div class="LIFVXC">
+        <img src="<? echo esc_url($colabColaboradorAvatar); ?>">
+        <img src="<? echo esc_url($colabAutorAvatar); ?>">
+    </div>
+<?php
+    return ob_get_clean();
+}
