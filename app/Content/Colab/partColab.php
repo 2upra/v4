49dc07217731f20a8c39c0b040a464fcf648cb8c
1ndoryu <andoryyu@gmail.com>
2025-04-10 11:31:06 +0200

    Refactor(Org): Mueve función variablesColab() a app/Services/ColabService.php

--- app/Content/Colab/partColab.php
+++ app/Content/Colab/partColab.php
@@ -1,51 +1,6 @@
 <?php
 
-function variablesColab($post_id = null)
-{
-    if ($post_id === null) {
-        global $post;
-        $post_id = $post->ID;
-    }
-
-    $current_user_id = get_current_user_id();
-    $colabPostOrigen = get_post_meta($post_id, 'colabPostOrigen', true);
-    $colabAutor = get_post_meta($post_id, 'colabAutor', true);
-    $colabColaborador = get_post_meta($post_id, 'colabColaborador', true);
-    $colabMensaje = get_post_meta($post_id, 'colabMensaje', true);
-    $colabFileUrl = get_post_meta($post_id, 'colabFileUrl', true);
-    $post_audio_lite = get_post_meta($post_id, 'post_audio_lite', true);
-    $participantes = get_post_meta($post_id, 'participantes', true);
-    $conversacion_id = get_post_meta($post_id, 'conversacion_id', true);
-
-    $imagenPost = get_the_post_thumbnail_url($post_id, 'full');
-    if (!$imagenPost) {
-        $imagenPost = 'https://i0.wp.com/2upra.com/wp-content/uploads/2024/09/1ndoryu_1725478496.webp?quality=40&strip=all';
-    }
-    $imagenPostOp = img($imagenPost, 40, 'all');
-
-    $postTitulo = get_the_title($post_id);
-
-    return [
-        'post_id' => $post_id,
-        'conversacion_id' => $conversacion_id,
-        'participantes' => $participantes,
-        'post_audio_lite' => $post_audio_lite,
-        'current_user_id' => $current_user_id,
-        'colabPostOrigen' => $colabPostOrigen,
-        'colabAutor' => $colabAutor,
-        'colabColaborador' => $colabColaborador,
-        'colabMensaje' => $colabMensaje,
-        'colabFileUrl' => $colabFileUrl,
-        'colabAutorName' => get_the_author_meta('display_name', $colabAutor),
-        'colabColaboradorName' => get_the_author_meta('display_name', $colabColaborador),
-        'colabColaboradorAvatar' => imagenPerfil($colabColaborador),
-        'colabAutorAvatar' => imagenPerfil($colabAutor),
-        'colabFecha' => get_the_date('', $post_id),
-        'colab_status' => get_post_status($post_id),
-        'imagenPostOp' => $imagenPostOp,
-        'postTitulo' => $postTitulo, // Añadir el título del post
-    ];
-}
+// Refactor(Exec): Función variablesColab() movida a app/Services/ColabService.php
 
 
 
