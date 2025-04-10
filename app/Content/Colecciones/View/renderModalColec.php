0d3b992dcd8f529e82f45c75c5385decc31e89b1
1ndoryu <andoryyu@gmail.com>
2025-04-10 03:27:33 +0200

    Refactor(Org): Mueve función modalCreacionColeccion() a app/View/Modals/CollectionModal.php

--- app/Content/Colecciones/View/renderModalColec.php
+++ app/Content/Colecciones/View/renderModalColec.php
@@ -10,35 +10,6 @@
 
 // Refactor(Org): Funcion obtenerListaColec() y su hook movidos a app/Services/CollectionService.php
 
-function modalCreacionColeccion()
-{
+// Refactor(Exec): Funcion modalCreacionColeccion() movida a app/View/Modals/CollectionModal.php
 
-    ob_start();
 ?>
-    <div class="modalColec crearColec modalCrearColec modal" id="modalCrearColec" style="display: none;">
-        <div class="colecciones formColec">
-            <h3>Crear colección</h3>
-            <div class="previewAreaArchivos previewColec" id="previewImagenColec">
-                <label>Agregar imagen (opcional)</label>
-            </div>
-            <input type="text" placeholder="Nombre de la colección" id="tituloColec">
-            <input type="text" placeholder="Descripción de la colección (opcional)" id="descripColec">
-
-            <div class="bloque flex-row"" id=" opcionesColec" style="display: flex">
-                <p>Opciones de post</p>
-                <div class="flex flex-row gap-2">
-                    <label class="custom-checkbox">
-                        <input type="checkbox" id="privadoColec" name="privadoColec" value="1">
-                        <span class="checkmark"></span>
-                        <? echo $GLOBALS['iconoPrivado']; ?>
-                    </label>
-                </div>
-            </div>
-            <div class="XJAAHB">
-                <button class="botonsecundario" id="btnVolverColec">Volver</button>
-                <button class="botonprincipal" id="btnCrearColec">Crear</button>
-            </div>
-        </div>
-    </div>
-<?
-}
--- app/View/Modals/CollectionModal.php
+++ app/View/Modals/CollectionModal.php
@@ -66,3 +66,37 @@ function modalColeccion()
     </div>
 <?
 }
+
+// Refactor(Exec): Funcion modalCreacionColeccion() movida desde app/Content/Colecciones/View/renderModalColec.php
+function modalCreacionColeccion()
+{
+
+    ob_start();
+?>
+    <div class="modalColec crearColec modalCrearColec modal" id="modalCrearColec" style="display: none;">
+        <div class="colecciones formColec">
+            <h3>Crear colección</h3>
+            <div class="previewAreaArchivos previewColec" id="previewImagenColec">
+                <label>Agregar imagen (opcional)</label>
+            </div>
+            <input type="text" placeholder="Nombre de la colección" id="tituloColec">
+            <input type="text" placeholder="Descripción de la colección (opcional)" id="descripColec">
+
+            <div class="bloque flex-row"" id=" opcionesColec" style="display: flex">
+                <p>Opciones de post</p>
+                <div class="flex flex-row gap-2">
+                    <label class="custom-checkbox">
+                        <input type="checkbox" id="privadoColec" name="privadoColec" value="1">
+                        <span class="checkmark"></span>
+                        <? echo $GLOBALS['iconoPrivado']; ?>
+                    </label>
+                </div>
+            </div>
+            <div class="XJAAHB">
+                <button class="botonsecundario" id="btnVolverColec">Volver</button>
+                <button class="botonprincipal" id="btnCrearColec">Crear</button>
+            </div>
+        </div>
+    </div>
+<?
+}
