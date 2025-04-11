f240e0a9452fc79e1de2ce4308c425afcaff41c6
1ndoryu <andoryyu@gmail.com>
2025-04-11 02:19:23 +0200

    Refactor(Org): Mueve función formNotasUL() de renderNota.php a NoteRenderer.php

--- app/Content/Notas/renderNota.php
+++ app/Content/Notas/renderNota.php
@@ -1,28 +1,7 @@
 <?
 
 // Función htmlNotas() movida a app/View/Renderers/NoteRenderer.php
-
-function formNotasUL()
-{
-    $filtro = 'notas';
-
-    ob_start();
-?>
-    <ul class="social-post-list clase-notas">
-        <li class="POST-<? echo esc_attr($filtro); ?> EDYQHV"
-            filtro="<? echo esc_attr($filtro); ?>">
-
-            <div class="contenidoNota agregarNuevaNota" id="agregarNuevaNota">
-                <p class="contenidoNotaP">Escribir una nueva nota</p>
-            </div>
-            <div class="botonesNotasGenerales">
-                <button><? echo $GLOBALS['borradorIcon']; ?></button>
-            </div>
-        </li>
-    </ul>
-<?
-    return ob_get_clean();
-}
+// Función formNotasUL() movida a app/View/Renderers/NoteRenderer.php
 
 function formNotas()
 {
