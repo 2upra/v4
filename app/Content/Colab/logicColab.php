b16b1accac4d8eb732614a95a47834c493c41e32
1ndoryu <andoryyu@gmail.com>
2025-04-11 02:17:37 +0200

    Refactor(Clean): Elimina archivo vacío app/Content/Colab/logicColab.php

--- app/Content/Colab/logicColab.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Refactor(Org): Funcion botonColab() movida a app/View/Helpers/UIHelper.php
-// Refactor(Org): Funcion empezarColab() y su hook movidos a app/Services/ColabService.php
-// Refactor(Org): Funcion actualizarEstadoColab() y su hook movidos a app/Services/Post/PostStatusService.php
-
-
-?>
