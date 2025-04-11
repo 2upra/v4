f08a0acb451fa69759911be4923479ad06eff122
1ndoryu <andoryyu@gmail.com>
2025-04-11 19:31:56 +0200

    Refactor(Org): Move add_meta_tags function and hook from TemplateDev.php to app/Setup/ThemeSetup.php

--- TemplateDev.php
+++ TemplateDev.php
@@ -4,18 +4,7 @@ Template Name: dev
 ya hay un h1 adentro de dev
 */
 
-// Asegúrate de que estas meta etiquetas estén en el header
-add_action('wp_head', 'add_meta_tags');
-function add_meta_tags() {
-    ?>
-    <meta name="description" content="<? echo esc_attr(get_the_excerpt()); ?>">
-    <meta name="keywords" content="<? echo esc_attr(get_post_meta(get_the_ID(), '_yoast_wpseo_focuskw', true)); ?>">
-    <meta property="og:title" content="<? echo esc_attr(get_the_title()); ?>">
-    <meta property="og:description" content="<? echo esc_attr(get_the_excerpt()); ?>">
-    <meta property="og:image" content="<? echo esc_url(get_the_post_thumbnail_url()); ?>">
-    <meta property="og:url" content="<? echo esc_url(get_permalink()); ?>">
-    <?
-}
+// Refactor(Org): Moved add_meta_tags function and hook to app/Setup/ThemeSetup.php
 
 get_header();
 $user_id = get_current_user_id();
