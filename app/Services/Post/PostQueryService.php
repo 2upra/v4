d9357e9293f4f7c2bfe7fd9eb2149119663ee646
1ndoryu <andoryyu@gmail.com>
2025-04-11 04:00:54 +0200

    Refactor(Org): Create PostQueryService to hold post query argument building logic

---
+++ app/Services/Post/PostQueryService.php
@@ -0,0 +1,22 @@
+<?php
+
+// Refactor(Org): Created PostQueryService.php
+// Contains functions related to building WP_Query arguments for retrieving posts
+// based on various criteria (filters, user type, search, etc.).
+
+// Example function structure (to be populated later):
+/*
+function build_query_args_for_filter($filter, $user_id, $paged) {
+    $args = [
+        'post_type' => 'social_post',
+        'posts_per_page' => 12,
+        'paged' => $paged,
+    ];
+
+    // Add filter-specific logic here...
+
+    return $args;
+}
+*/
+
+?>
