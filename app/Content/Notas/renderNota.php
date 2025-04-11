2a9383c661e3d4d98ce5e9b9f704aafdf45901ed
1ndoryu <andoryyu@gmail.com>
2025-04-11 02:43:48 +0200

    Refactor(Clean): Elimina archivo vacío app/Content/Notas/renderNota.php

--- app/Content/Notas/renderNota.php
+++
@@ -1,7 +0,0 @@
-<?
-
-// Función htmlNotas() movida a app/View/Renderers/NoteRenderer.php
-// Función formNotasUL() movida a app/View/Renderers/NoteRenderer.php
-// Función formNotas() movida a app/View/Components/Forms/NoteForm.php
-
-
