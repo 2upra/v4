eb8057e2252af395a1147a57178b36320cc6cfa7
1ndoryu <andoryyu@gmail.com>
2025-04-12 11:54:47 +0200

    Refactor(Clean): Elimina archivo app/Content/Colab/partColab.php que quedó vacío

--- app/Content/Colab/partColab.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Refactor(Exec): Función contenidoColab() movida a app/View/Renderers/ColabRenderer.php
-// Refactor(Exec): Función participantesColab() movida a app/View/Renderers/ColabRenderer.php
-
-
-// Refactor(Exec): Función opcionesColab() movida a app/View/Helpers/ColabHelper.php
-// Refactor(Exec): Función opcionesColabActivo() movida a app/View/Helpers/ColabHelper.php
