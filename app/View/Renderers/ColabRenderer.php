a735f1d73e0d3c8a0d8e3e030b977772fece0109
1ndoryu <andoryyu@gmail.com>
2025-04-12 06:25:26 +0200

    Refactor(Org): Mueve función contenidoColab() de partColab.php a ColabRenderer.php

--- app/View/Renderers/ColabRenderer.php
+++ app/View/Renderers/ColabRenderer.php
@@ -97,3 +97,48 @@ function colab()
 
 <? return ob_get_clean();
 }
+
+// Refactor(Exec): Función contenidoColab() movida desde app/Content/Colab/partColab.php
+function contenidoColab($var)
+{
+    $post_id = $var['post_id'];
+    $colabMensaje  = $var['colabMensaje'];
+    $post_audio_lite = $var['post_audio_lite'];
+    $colabFileUrl = $var['colabFileUrl'];
+
+    ob_start();
+?>
+    <div class="XZAKCB">
+
+        <div class="BCGWEY">
+            <span class="badge ver-contenido" data-post-id="<? echo esc_attr($post_id); ?>">Ver contenido</span>
+        </div>
+        <div class="colabfiles" id="colabfiles-<? echo esc_attr($post_id); ?>" style="display: none;">
+            <? if (!empty($post_audio_lite)) : ?>
+                <div class="DNPHZG">
+                    <? echo audioColab($post_id, $post_audio_lite); ?>
+                </div>
+            <? else : ?>
+                <div class="AIWZKN">
+                    <? if (!empty($colabFileUrl)) : ?>
+                        <? $file_name = basename($colabFileUrl); ?>
+                        <a href="<? echo esc_url($colabFileUrl); ?>" download class="file-download no-ajax">
+                            <div class="XQGSAN">
+                                <? echo $GLOBALS['fileGrande']; ?>
+                                <? echo esc_html($file_name); ?>
+                            </div>
+                        </a>
+                        <p class="textoMuyPequeno">
+                            El archivo ha sido analizado y no se encontraron virus. Sin embargo, si no confías en la persona que realizó la solicitud, no descargues archivos.
+                        </p>
+                        <p class="mensajeColab">Mensaje de solicitud: <? echo esc_html($colabMensaje); ?></p>
+                    <? else : ?>
+                        <p>No hay archivo adjunto.</p>
+                    <? endif; ?>
+                </div>
+            <? endif; ?>
+        </div>
+    </div>
+<?php
+    return ob_get_clean();
+}
