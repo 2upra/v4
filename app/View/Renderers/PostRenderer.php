7a2bc0fa19ad7889971df664363500a60de3d380
1ndoryu <andoryyu@gmail.com>
2025-04-12 03:42:19 +0200

    Refactor(Org): Mueve función renderMusicContent() de renderPost.php a PostRenderer.php

--- app/View/Renderers/PostRenderer.php
+++ app/View/Renderers/PostRenderer.php
@@ -227,6 +227,95 @@ function renderNonMusicContent($filtro, $post_id, $author_id, $author_avatar, $a
     <?
 }
 
+// Refactor(Exec): Mueve función renderMusicContent() desde app/Content/Posts/View/renderPost.php
+function renderMusicContent($filtro, $post_id, $author_name, $block, $es_suscriptor, $post_status, $audio_url)
+{
+    $thumbnail_url = get_the_post_thumbnail_url($post_id, 'full');
+    $optimized_thumbnail_url = img($thumbnail_url, 40, 'all');
+    $momento = get_post_meta($post_id, 'momento', true);
+    $esColeccion = get_post_meta($post_id, 'datosColeccion', true);
+    if (!$esColeccion) {
+        $esColeccion = get_post_meta($post_id, 'ultimaModificacion', true);
+    }
+    $permalink = get_permalink($post_id);
+
+?>
+    <? if (!empty($momento) || !empty($esColeccion)) : ?>
+        <a href="<? echo esc_url($permalink); ?>">
+        <? endif; ?>
+        <div class="post-content">
+            <div class="MFQOYC">
+                <? echo like($post_id); ?>
+                <? echo opcionesRola($post_id, $post_status, $audio_url); ?>
+            </div>
+            <div class="KLYJBY">
+                <? echo audioPost($post_id); ?>
+            </div>
+
+            <? if (!empty($momento) || !empty($esColeccion)) : ?>
+                <div class="contentMoment">
+                    <?
+                    $content = get_the_content();
+                    if (!empty($content)) {
+                        echo $content;
+                    } else {
+                        echo '<p>' . get_the_title() . '</p>';
+                    }
+                    ?>
+                </div>
+            <? else : ?>
+                <div class="LRKHLC">
+                    <div class="XOKALG">
+                        <?
+                        $rola_meta = get_post_meta($post_id, 'rola', true);
+                        $nombre_rola_html = ''; // Variable para almacenar el HTML de nombreRola
+
+                        if ($rola_meta === '1') {
+                            $nombre_rola = get_post_meta($post_id, 'nombreRola', true);
+                            if (empty($nombre_rola)) {
+                                $nombre_rola = get_post_meta($post_id, 'nombreRola1', true);
+                            }
+                            if (!empty($nombre_rola)) {
+                                $nombre_rola_html = '<p class="nameRola">' . esc_html($nombre_rola) . '</p>';
+                            }
+                        }
+                        $output = '<p>' . esc_html($author_name) . '</p>';
+                        $output .= '<p>-</p>';
+                        $output .= $nombre_rola_html;
+
+                        echo $output;
+                        ?>
+                    </div>
+                </div>
+            <? endif; ?>
+            <div class="CPQBEN" style="display: none;">
+                <div class="CPQBAU"><? echo $author_name; ?></div>
+                <div class="CPQBCO">
+                    <?
+                    $rola_meta = get_post_meta($post_id, 'rola', true);
+
+                    if ($rola_meta === '1') {
+                        $nombre_rola = get_post_meta($post_id, 'nombreRola', true);
+                        if (empty($nombre_rola)) {
+                            $nombre_rola = get_post_meta($post_id, 'nombreRola1', true);
+                        }
+                        if (!empty($nombre_rola)) {
+                            echo "<p>" . esc_html($nombre_rola) . "</p>";
+                        } else {
+                        }
+                    } else {
+                    }
+                    ?>
+                </div>
+                <img src="<?= esc_url($optimized_thumbnail_url); ?>" alt="">
+            </div>
+        </div>
+        <? if (!empty($momento) || !empty($esColeccion)) : ?>
+        </a>
+    <? endif; ?>
+<?
+}
+
 // Refactor(Org): Moved function postrolaresumen() from app/Perfiles/perfilmusic.php
 function postrolaresumen() {
     global $post;
