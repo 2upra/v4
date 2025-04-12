feab73d3f977d83718863beece9727c8ff4b8fd3
1ndoryu <andoryyu@gmail.com>
2025-04-12 03:01:50 +0200

    Refactor(Org): Move AJAX handler ajax_update_presentacion() from perfilmusic.php to PresentationEditor.php

--- app/Perfiles/perfilmusic.php
+++ app/Perfiles/perfilmusic.php
@@ -107,43 +107,7 @@ add_shortcode('custom_user_profile_music', 'custom_user_profile_shortcode_music'
 // Refactor(Org): Moved function presentacion_shortcode() and its hook to app/View/Components/Profile/PresentationEditor.php
 
 
-
-function ajax_update_presentacion() {
-    require_once(ABSPATH . 'wp-admin/includes/image.php');
-    require_once(ABSPATH . 'wp-admin/includes/file.php');
-    require_once(ABSPATH . 'wp-admin/includes/media.php');
-
-    $user_id = get_current_user_id();
-
-    $texto = isset($_POST['texto']) ? sanitize_text_field($_POST['texto']) : 'Texto predeterminado';
-    $imagen_url = isset($_POST['imagen']) ? esc_url_raw($_POST['imagen']) : ''; 
-    
-    if (isset($_FILES['newImage']) && $_FILES['newImage']['size'] > 0) {
-        $imagen_id = media_handle_upload('newImage', 0);
-        if (is_wp_error($imagen_id)) {
-            $imagen_url = 'URL de imagen de error';
-        } else {
-            $imagen_url = wp_get_attachment_url($imagen_id);
-        }
-    }
-
-    update_user_meta($user_id, 'presentacion_texto', $texto);
-    update_user_meta($user_id, 'presentacion_imagen', $imagen_url);
-
-    // Crear HTML para respuesta
-    $html = "<div>";
-    $html .= "<div class='imagen-container'>";
-    $html .= "<img src='{$imagen_url}' alt='Imagen de Presentación' id='presentacion-imagen'>";
-    $html .= "<p id='presentacion-texto'>{$texto}</p>";
-    $html .= "</div>";
-
-    echo $html;
-
-    wp_die();
-}
-
-add_action('wp_ajax_update_presentacion', 'ajax_update_presentacion');
-add_action('wp_ajax_nopriv_update_presentacion', 'ajax_update_presentacion');
+// Refactor(Exec): Moved function ajax_update_presentacion() and its hooks to app/View/Components/Profile/PresentationEditor.php
 
 
 // Refactor(Org): Moved function postrolaresumen() to app/View/Renderers/PostRenderer.php
