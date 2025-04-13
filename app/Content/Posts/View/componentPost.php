df02104a47a20030531b96f23c17e69815acff1c
1ndoryu <andoryyu@gmail.com>
2025-04-13 02:32:07 +0200

    Refactor(Org): Mueve función wave() de componentPost.php a AudioHelper.php

--- app/Content/Posts/View/componentPost.php
+++ app/Content/Posts/View/componentPost.php
@@ -30,75 +30,8 @@
 
 // Refactor(Org): Función fondoPost() movida a app/View/Helpers/PostHelper.php
 
-function wave($audio_url, $audio_id_lite, $postId)
-{
-    $wave = get_post_meta($postId, 'waveform_image_url', true);
+// Refactor(Org): Función wave() y generate_wave_html() movidas a app/View/Helpers/AudioHelper.php
 
-    // Contar la cantidad de audios disponibles
-    $audio_count = 0;
-    $audio_urls = array();
-
-    // Cargar la URL para post_audio_lite
-    $audio_url_lite = get_post_meta($postId, 'post_audio_lite', true);
-    if (!empty($audio_url_lite)) {
-        $audio_count++;
-        $audio_urls['post_audio_lite'] = $audio_url_lite;
-    }
-
-    // Cargar las URLs para post_audio_lite_2, post_audio_lite_3, ..., post_audio_lite_30
-    for ($i = 2; $i <= 30; $i++) {
-        $meta_key = 'post_audio_lite_' . $i;
-        $audio_url_multiple = get_post_meta($postId, $meta_key, true);
-
-        if (!empty($audio_url_multiple)) {
-            $audio_count++;
-            $audio_urls[$meta_key] = $audio_url_multiple;
-        }
-    }
-?>
-    <div class="waveforms-container-post" id="waveforms-container-<?php echo $postId; ?>" data-post-id="<?php echo esc_attr($postId); ?>">
-        <?php
-        // Mostrar los botones solo si hay más de un audio
-        if ($audio_count > 1) : ?>
-            <div class="botonesWave">
-                <button class="prevWave" data-post-id="<?php echo esc_attr($postId); ?>">Anterior</button>
-                <button class="nextWave" data-post-id="<?php echo esc_attr($postId); ?>">Siguiente</button>
-            </div>
-        <?php endif; ?>
-        <?php
-        // Generar el HTML para cada audio
-        $index = 0;
-        foreach ($audio_urls as $meta_key => $audio_url) {
-            generate_wave_html($audio_url, $audio_id_lite, $postId, $meta_key, $wave, $index);
-            $index++;
-        }
-        ?>
-    </div>
-<?php
-}
-
-function generate_wave_html($audio_url, $audio_id_lite, $postId, $meta_key, $wave, $index)
-{
-
-    $waveCargada = get_post_meta($postId, 'waveCargada_' . $meta_key, true); // Wave cargada para cada audio
-    $urlAudioSegura = audioUrlSegura($audio_url);
-    $unique_id = $postId . '-' . $meta_key; // ID único para cada waveform
-
-    if (is_wp_error($urlAudioSegura)) {
-        $urlAudioSegura = '';
-    }
-?>
-    <div id="waveform-<?php echo $unique_id; ?>"
-        class="waveform-container without-image"
-        postIDWave="<?php echo $unique_id; ?>"
-        data-wave-cargada="<?php echo $waveCargada ? 'true' : 'false'; ?>"
-        data-audio-url="<?php echo esc_url($urlAudioSegura); ?>">
-        <div class="waveform-background" style="background-image: url('<?php echo esc_url($wave); ?>');"></div>
-        <div class="waveform-message"></div>
-        <div class="waveform-loading" style="display: none;">Cargando...</div>
-    </div>
-<?php
-}
 
 // Refactor(Org): Función audioPost() movida a app/View/Helpers/AudioHelper.php
 
