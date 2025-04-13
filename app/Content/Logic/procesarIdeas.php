0a3d3681bb19ad81c7eaea87d0ad7e37144faa99
1ndoryu <andoryyu@gmail.com>
2025-04-13 04:18:58 +0200

    Refactor(Clean): Elimina archivo app/Content/Logic/procesarIdeas.php que quedó vacío

--- app/Content/Logic/procesarIdeas.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-// Refactor(Org): Función manejarIdea() movida a app/Services/IdeaService.php
-
-// Refactor(Org): Función procesarIdeas() movida a app/Services/IdeaService.php
-
-// Refactor(Org): Función movida a app/AlgoritmoPost/calcularPuntos.php
