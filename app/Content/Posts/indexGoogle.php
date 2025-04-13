553508600cf6fba7a6c145fd633d94dfce23f497
1ndoryu <andoryyu@gmail.com>
2025-04-13 03:31:59 +0200

    Refactor(Clean): Elimina archivo app/Content/Posts/indexGoogle.php que quedó vacío

--- app/Content/Posts/indexGoogle.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//Evitar que wp genere los titulos por defecto 
-remove_action('wp_head', '_wp_render_title_tag', 1);
-
-// Refactor(Org): Moved function regenerate_colecciones_sitemap() and its hook to SEOService.php
-
-?>
