f1801f32bb2ab0fa5a1e5d177bd9ba565f2a8f74
1ndoryu <andoryyu@gmail.com>
2025-04-13 03:01:49 +0200

    Refactor(Clean): Delete empty file app/Chat/Galle2v.php

--- app/Chat/Galle2v.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Refactor(Org): Función guardarMensaje() movida a app/Services/ChatService.php
-// Refactor(Org): Función procesarMensaje() movida a app/Services/ChatService.php
-
-// La función guardarMensaje() ha sido movida a app/Services/ChatService.php
-// La función procesarMensaje() ha sido movida a app/Services/ChatService.php
-
