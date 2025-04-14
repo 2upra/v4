c723ebcd941b44eb85e67d38123ac893fbef2f26
1ndoryu <andoryyu@gmail.com>
2025-04-14 02:43:15 +0200

    Refactor(Org): Mueve función modalComprarAcciones() de app/Finanza/ComprarAcciones/ a app/View/Components/Finanza/ComprarAcciones/

--- app/Finanza/ComprarAcciones/ModalComprar.php
+++ app/Finanza/ComprarAcciones/ModalComprar.php
@@ -1,23 +1,4 @@
 <?
+// Refactor(Exec): Moved function modalComprarAcciones to app/View/Components/Finanza/ComprarAcciones/ModalComprar.php
 
-function modalComprarAcciones()
-{
-
-    ob_start();
-    ?>
-
-        <div class="HMPGRM" id="modalinvertir">
-            <div id="contenidocomprar">
-                <p class="ETXLXB">Ingresa la cantidad a donar</p>
-                <input type="text" id="cantidadCompra" placeholder="$20">
-                <input type="hidden" id="cantidadReal">
-                <input type="hidden" id="userID" value="<? echo get_current_user_id(); ?>">
-                <p>"Al donar, parte de tu contribución se convierte en acciones de nuestra empresa a través de un fondo de inversión algorítmico que ajusta su valor automáticamente. Tu apoyo impulsa el proyecto y te hace parte de nuestro crecimiento con la autoridad de beneficiarte de nuestros frutos en el futuro"</p>
-                <div class="DZYSQD DZYSQF">
-                    <button class="DZYBQD cerrardonar">Volver</button>
-                    <button class="DZYBQD botonprincipal" id="botonComprar">Donar</button>
-                </div>
-            </div>
-        </div>
-    <? return ob_get_clean();
-}
+// This file is now empty after moving the function and can be deleted in a subsequent step.
--- app/View/Components/Finanza/ComprarAcciones/ModalComprar.php
+++ app/View/Components/Finanza/ComprarAcciones/ModalComprar.php
@@ -2,6 +2,27 @@
 // Archivo creado para el componente de vista del modal de compra de acciones.
 // Contendrá la función modalComprarAcciones() movida desde app/Finanza/ComprarAcciones/
 
-// TODO: Mover la función modalComprarAcciones() aquí.
+// Refactor(Exec): Moved function modalComprarAcciones from app/Finanza/ComprarAcciones/ModalComprar.php
+function modalComprarAcciones()
+{
+
+    ob_start();
+    ?>
+
+        <div class="HMPGRM" id="modalinvertir">
+            <div id="contenidocomprar">
+                <p class="ETXLXB">Ingresa la cantidad a donar</p>
+                <input type="text" id="cantidadCompra" placeholder="$20">
+                <input type="hidden" id="cantidadReal">
+                <input type="hidden" id="userID" value="<? echo get_current_user_id(); ?>">
+                <p>"Al donar, parte de tu contribución se convierte en acciones de nuestra empresa a través de un fondo de inversión algorítmico que ajusta su valor automáticamente. Tu apoyo impulsa el proyecto y te hace parte de nuestro crecimiento con la autoridad de beneficiarte de nuestros frutos en el futuro"</p>
+                <div class="DZYSQD DZYSQF">
+                    <button class="DZYBQD cerrardonar">Volver</button>
+                    <button class="DZYBQD botonprincipal" id="botonComprar">Donar</button>
+                </div>
+            </div>
+        </div>
+    <? return ob_get_clean();
+}
 
 ?>
