566095b2287eae22345244d9ac6e815ff64cf79b
1ndoryu <andoryyu@gmail.com>
2025-04-14 04:13:42 +0200

    Refactor(Clean): Elimina archivo app/AlgoritmoPost/interesesAlgoritmo.php que quedó vacío

--- app/AlgoritmoPost/interesesAlgoritmo.php
+++
@@ -1,6 +0,0 @@
-<?
-
-// Refactor(Org): Función obtenerLikesDelUsuario() movida a app/Services/LikeService.php
-
-// Refactor(Org): Funciones generarMetaDeIntereses y actualizarIntereses movidas a app/Services/UserService.php
-
