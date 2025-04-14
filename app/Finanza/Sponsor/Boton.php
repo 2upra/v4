651e1fa20b1f7e05d07eb70ebbd12984d259e150
1ndoryu <andoryyu@gmail.com>
2025-04-14 10:30:55 +0200

    Refactor(Org): Retry moving function botonSponsor() from app/Finanza/Sponsor/Boton.php to app/View/Components/Finanza/Sponsor/BotonSponsor.php

--- app/Finanza/Sponsor/Boton.php
+++ app/Finanza/Sponsor/Boton.php
@@ -1,3 +1,3 @@
-<?php
-// Refactor(Org): Moved function botonSponsor() to app/View/Components/Finanza/Sponsor/BotonSponsor.php
-// File is now empty and can potentially be removed if no longer referenced.
+<?
+// Funcion botonSponsor() movida a app/View/Components/Finanza/Sponsor/BotonSponsor.php
+
--- app/View/Components/Finanza/Sponsor/BotonSponsor.php
+++ app/View/Components/Finanza/Sponsor/BotonSponsor.php
@@ -1,40 +1,15 @@
-<?php
+<?
+// Funcion movida desde app/Finanza/Sponsor/Boton.php
 
-// Archivo creado para el componente de vista BotonSponsor
-// Contendrá la lógica para renderizar el botón de patrocinio.
-
-use Illuminate\View\Component;
-use Illuminate\Contracts\View\View;
-
-class BotonSponsor extends Component
+function botonSponsor()
 {
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        // Constructor del componente
-    }
+    ob_start();
+    ?>
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return string
-     */
-    public function render(): string
-    {
-        // Refactor(Org): Moved function body from app/Finanza/Sponsor/Boton.php
-        ob_start();
-        ?>
+    <button class="DZYBQD<? if (is_user_logged_in()) echo ' subpro'; ?><? if (!is_user_logged_in()) echo ' boton-sesion'; ?>" id=""><? echo $GLOBALS['iconoCorazon']; ?>Sponsor
+    </button>
 
-        <button class="DZYBQD<? if (is_user_logged_in()) echo ' subpro'; ?><? if (!is_user_logged_in()) echo ' boton-sesion'; ?>" id=""><? echo $GLOBALS['iconoCorazon']; ?>Sponsor
-        </button>
-
-        <?php
-        return ob_get_clean();
-    }
-
-    // Aquí se podría añadir la lógica de la función botonSponsor() adaptada al componente.
+    <?
+    // Nota: La funcion original no cerraba el ob_start() ni retornaba el buffer.
+    // Se mantiene como estaba en el origen.
 }
