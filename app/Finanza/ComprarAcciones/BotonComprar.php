66684352ed384f173cbcdd75c1502d5f6cd8837d
1ndoryu <andoryyu@gmail.com>
2025-04-14 02:49:03 +0200

    Refactor(Clean): Elimina archivo app/Finanza/ComprarAcciones/BotonComprar.php que quedó vacío

--- app/Finanza/ComprarAcciones/BotonComprar.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Refactor(Org): Moved function botonComprarAcciones to app/View/Components/Finanza/ComprarAcciones/BotonComprar.php
-
-// Este archivo ahora está vacío ya que la función fue movida.
-// Puede ser eliminado en un paso futuro si no se necesita más.
-
-// Acción de refactorización: No se realizó ninguna acción adicional, la función 'botonComprarAcciones' ya no estaba presente en este archivo.
-
-?>
