e84838bdd80ee670db844fd689b0d8e921bac849
1ndoryu <andoryyu@gmail.com>
2025-04-14 02:37:39 +0200

    Refactor(Org): Crea archivo app/View/Components/Finanza/ComprarAcciones/ModalComprar.php para componente de modal

---
+++ app/View/Components/Finanza/ComprarAcciones/ModalComprar.php
@@ -0,0 +1,7 @@
+<?php
+// Archivo creado para el componente de vista del modal de compra de acciones.
+// Contendrá la función modalComprarAcciones() movida desde app/Finanza/ComprarAcciones/
+
+// TODO: Mover la función modalComprarAcciones() aquí.
+
+?>
