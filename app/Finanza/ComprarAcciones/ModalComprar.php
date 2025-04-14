73331a602a0aa8b57f96ce69e00dfee7b5cd1b08
1ndoryu <andoryyu@gmail.com>
2025-04-14 02:55:03 +0200

    Refactor(Clean): Elimina archivo app/Finanza/ComprarAcciones/ModalComprar.php que quedó vacío

--- app/Finanza/ComprarAcciones/ModalComprar.php
+++
@@ -1,4 +0,0 @@
-<?
-// Refactor(Exec): Moved function modalComprarAcciones to app/View/Components/Finanza/ComprarAcciones/ModalComprar.php
-
-// This file is now empty after moving the function and can be deleted in a subsequent step.
