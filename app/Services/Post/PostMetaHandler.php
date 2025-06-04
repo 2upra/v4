0bb5d714a9557d692c32d25fbe545dd9464ff577
1ndoryu <andoryyu@gmail.com>
2025-06-04 12:49:43 +0200

    Tarea ID RF-PMH-002 (Crear un servicio PostMetaHandler) completada (Misión RefactorMultipleAudioPosts)

--- app/Services/Post/PostMetaHandler.php
+++ app/Services/Post/PostMetaHandler.php
@@ -2,23 +2,37 @@
 
 namespace App\Services\Post;
 
-// This class will be fully implemented in Task RF-PMH-002.
-// For now, it's a placeholder to satisfy dependency injection.
 class PostMetaHandler
 {
-    public function __construct()
-    {
-        // Constructor for PostMetaHandler
-    }
-
-    // Placeholder methods for future implementation
+    /**
+     * Copies a batch of specified post meta from a source post to a target post.
+     *
+     * @param int $sourcePostId The ID of the post to copy meta from.
+     * @param int $targetPostId The ID of the post to copy meta to.
+     * @param array $metaKeys An array of meta keys to copy.
+     * @return void
+     */
     public function copyMeta(int $sourcePostId, int $targetPostId, array $metaKeys): void
     {
-        // Logic to be implemented in RF-PMH-002
+        foreach ($metaKeys as $key) {
+            $value = get_post_meta($sourcePostId, $key, true);
+            if ($value !== '') { // Only copy if meta exists and is not empty
+                update_post_meta($targetPostId, $key, $value);
+            }
+        }
     }
 
+    /**
+     * Deletes a batch of specified post meta from a post.
+     *
+     * @param int $postId The ID of the post from which to delete meta.
+     * @param array $metaKeys An array of meta keys to delete.
+     * @return void
+     */
     public function deleteMetaBatch(int $postId, array $metaKeys): void
     {
-        // Logic to be implemented in RF-PMH-002
+        foreach ($metaKeys as $key) {
+            delete_post_meta($postId, $key);
+        }
     }
 }
