1e77acf81c463b4a882731096a3b03213c08b8dd
1ndoryu <andoryyu@gmail.com>
2025-06-08 04:27:32 -0400

    *

--- app/Misc/iconos.php
+++ app/Misc/iconos.php
@@ -6,6 +6,8 @@
 // stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
 function set_global_iconos()
 {
+    $GLOBALS['twobox'] = '<svg id="uuid-aaaa7a22-0b65-42e8-a69c-056d1307c32f" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25.2 25.2"><defs><style>.uuid-0180f0e2-a104-4795-812c-22cf17b52a9d {fill: #efefef; height="13" width="13"}</style></defs><rect class="uuid-0180f0e2-a104-4795-812c-22cf17b52a9d" y="12.6" width="12.6" height="12.6"/><rect class="uuid-0180f0e2-a104-4795-812c-22cf17b52a9d" x="12.6" width="12.6" height="12.6"/></svg>';
+
     $GLOBALS['iconoHabitoRe'] = '<svg data-testid="geist-icon" height="13" stroke-linejoin="round" style="color:currentColor" viewBox="0 0 16 16" width="13"><path fill-rule="evenodd" clip-rule="evenodd" d="M7.96452 2.5C11.0257 2.5 13.5 4.96643 13.5 8C13.5 11.0336 11.0257 13.5 7.96452 13.5C6.12055 13.5 4.48831 12.6051 3.48161 11.2273L3.03915 10.6217L1.828 11.5066L2.27046 12.1122C3.54872 13.8617 5.62368 15 7.96452 15C11.8461 15 15 11.87 15 8C15 4.13001 11.8461 1 7.96452 1C5.06835 1 2.57851 2.74164 1.5 5.23347V3.75V3H0V3.75V7.25C0 7.66421 0.335786 8 0.75 8H3.75H4.5V6.5H3.75H2.63724C3.29365 4.19393 5.42843 2.5 7.96452 2.5ZM8.75 5.25V4.5H7.25V5.25V7.8662C7.25 8.20056 7.4171 8.51279 7.6953 8.69825L9.08397 9.62404L9.70801 10.0401L10.5401 8.79199L9.91603 8.37596L8.75 7.59861V5.25Z" fill="currentColor"></path></svg>';
 
     $GLOBALS['minus'] = '<svg data-testid="geist-icon" height="16" stroke-linejoin="round" style="color:currentColor" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 7.25H2.75H13.25H14V8.75H13.25H2.75H2V7.25Z" fill="currentColor"></path></svg>';
