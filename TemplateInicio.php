69dac7113ac8e53f84008a601051b1a7fd749868
1ndoryu <andoryyu@gmail.com>
2025-12-15 05:38:56 -0400

    refactor: reemplaza la función `dev()` por el componente `InicioTabs` en la página de inicio para usuarios no logueados.

--- TemplateInicio.php
+++ TemplateInicio.php
@@ -4,7 +4,8 @@ Template Name: Inicio
 */
 
 // Función para obtener el idioma preferido del navegador
-function obtenerIdiomaDelNavegador() {
+function obtenerIdiomaDelNavegador()
+{
     if (!isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
         return 'en';
     }
@@ -55,7 +56,7 @@ $nologin_class = !is_user_logged_in() ? ' nologin' : '';
     <div id="content" class="<? echo esc_attr($nologin_class); ?>">
         <input type="hidden" id="pagina_actual" name="pagina_actual" value="<? echo esc_attr(get_the_title()); ?>">
         <? if (!is_user_logged_in()): ?>
-            <? echo dev(); ?>
+            <? echo \Kamples\Views\Components\Tabs\InicioTabs::render(); ?>
         <? else: ?>
             <? echo socialTabs(); ?>
         <? endif; ?>
